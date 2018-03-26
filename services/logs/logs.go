9855cf38ebc726e2ce597fa855b20fb70c992110
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2018-03-26 11:43:11 +0300

    PMM-1274 Cleanup logs service.

--- services/logs/logs.go
+++ services/logs/logs.go
@@ -27,82 +27,85 @@ import (
 	"strconv"
 )
 
-type log struct {
-	Path       string
-	Journalctl string
-}
-
+// File represents log file content.
 type File struct {
 	Name string
 	Data []byte
 	Err  error
 }
 
-var (
-	defaultLogs = []log{
-		// Managed by supervisord
-		{"/var/log/mysql.log", ""},
-		{"/var/log/consul.log", "consul"},
-		{"/var/log/cron.log", ""},
-		{"/var/log/qan-api.log", "percona-qan-api"},
-		{"/var/log/prometheus.log", "prometheus"},
-		{"/var/log/createdb.log", ""},
-		{"/var/log/orchestrator.log", "orchestrator"},
-		{"/var/log/dashboard-upgrade.log", ""},
-		{"/var/log/node_exporter.log", "node_exporter"},
-		{"/var/log/pmm-manage.log", "pmm-manage"},
-		{"/var/log/pmm-managed.log", ""},
-		{"/var/log/supervisor/supervisord.log", ""},
-		// Grafana and Nginx
-		{"/var/log/grafana/grafana.log", ""},
-		{"/var/log/nginx.log", "nginx"},
-		{"/var/log/nginx/access.log", ""},
-		{"/var/log/nginx/error.log", ""},
-		// AMI/OVF
-		{"/var/log/mysqld.log", "mysqld"},
-		{"/var/log/nginx/access.log", ""},
-		{"/var/log/nginx/error.log", ""},
-		{"", "crond"},
-	}
-)
+type log struct {
+	FilePath string
+	UnitName string
+}
+
+var defaultLogs = []log{
+	// Managed by supervisord
+	{"/var/log/consul.log", "consul"},
+	{"/var/log/createdb.log", ""},
+	{"/var/log/cron.log", ""},
+	{"/var/log/dashboard-upgrade.log", ""},
+	{"/var/log/mysql.log", ""},
+	{"/var/log/node_exporter.log", "node_exporter"},
+	{"/var/log/orchestrator.log", "orchestrator"},
+	{"/var/log/pmm-manage.log", "pmm-manage"},
+	{"/var/log/pmm-managed.log", "pmm-managed"},
+	{"/var/log/prometheus.log", "prometheus"},
+	{"/var/log/qan-api.log", "percona-qan-api"},
+	{"/var/log/supervisor/supervisord.log", ""},
+
+	// Grafana and Nginx
+	{"/var/log/grafana/grafana.log", ""},
+	{"/var/log/nginx.log", "nginx"},
+	{"/var/log/nginx/access.log", ""},
+	{"/var/log/nginx/error.log", ""},
+
+	// AMI/OVF
+	{"", "crond"},
+	{"/var/log/mysqld.log", "mysqld"},
+}
 
 // Logs is responsible for interactions with logs.
 type Logs struct {
-	n    int
-	logs []log
+	n              int
+	logs           []log
+	journalctlPath string
 }
 
 // New creates a new Logs service.
+// n is a number of last lines of log to read.
 func New(n int) *Logs {
+	journalctlPath, _ := exec.LookPath("journalctl")
 	return &Logs{
-		n:    n,
-		logs: defaultLogs,
+		n:              n,
+		logs:           defaultLogs,
+		journalctlPath: journalctlPath,
 	}
 }
 
 // Zip creates .zip archive with all logs.
 func (l *Logs) Zip(ctx context.Context, w io.Writer) error {
-	// Create a new zip archive.
 	zw := zip.NewWriter(w)
 
-	// Add logs to the archive.
 	for _, log := range l.logs {
-		name, content, err := l.tailN(ctx, &log, l.n)
+		name, content, err := l.readLog(ctx, &log)
 		if err != nil {
-			content = append(content, '\n')
+			if len(content) > 0 {
+				content = append(content, "\n\n"...)
+			}
 			content = append(content, []byte(err.Error())...)
 		}
+
 		f, err := zw.Create(name)
 		if err != nil {
 			return err
 		}
-		_, err = f.Write(content)
-		if err != nil {
+		if _, err = f.Write(content); err != nil {
 			return err
 		}
 	}
 
-	// Make sure to check the error on Close.
+	// make sure to check the error on Close
 	return zw.Close()
 }
 
@@ -112,46 +115,51 @@ func (l *Logs) Files(ctx context.Context) []File {
 
 	for _, log := range l.logs {
 		file := File{}
-		file.Name, file.Data, file.Err = l.tailN(ctx, &log, l.n)
+		file.Name, file.Data, file.Err = l.readLog(ctx, &log)
 		files = append(files, file)
 	}
 
 	return files
 }
 
-// tailN reads last n lines from defined Log configuration
-func (l *Logs) tailN(ctx context.Context, log *log, n int) (name string, data []byte, err error) {
-	if _, err := exec.LookPath("journalctl"); err == nil && log.Journalctl != "" {
-		data, err := journalctlN(ctx, log.Journalctl, n)
-		return log.Journalctl, data, err
-	} else if log.Path != "" {
-		data, err := tailN(ctx, log.Path, n)
-		return filepath.Base(log.Path), data, err
+// readLog reads last l.n lines from defined Log configuration.
+func (l *Logs) readLog(ctx context.Context, log *log) (name string, data []byte, err error) {
+	if log.UnitName != "" && l.journalctlPath != "" {
+		name = log.UnitName
+		data, err = l.journalctlN(ctx, log.UnitName)
+		return
 	}
 
-	return "", nil, fmt.Errorf("unable to get log: %v", log)
+	if log.FilePath != "" {
+		name = filepath.Base(log.FilePath)
+		data, err = l.tailN(ctx, log.FilePath)
+		return
+	}
+
+	err = fmt.Errorf("unable to get log: %v", log)
+	return
 }
 
-// journalctlN reads last n lines from systemd unit u.
-func journalctlN(ctx context.Context, u string, n int) ([]byte, error) {
-	cmd := exec.CommandContext(ctx, "journalctl", "-n", strconv.Itoa(n), "-u", u)
+// journalctlN reads last l.n lines from systemd unit u using `journalctl` command.
+func (l *Logs) journalctlN(ctx context.Context, u string) ([]byte, error) {
+	cmd := exec.CommandContext(ctx, l.journalctlPath, "-n", strconv.Itoa(l.n), "-u", u)
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 	b, err := cmd.Output()
 	if err != nil {
-		return b, fmt.Errorf("JournalctlN(%s, %d): %s: %s", u, n, err, stderr.String())
+		return b, fmt.Errorf("JournalctlN(%s, %d): %s: %s", u, l.n, err, stderr.String())
 	}
 	return b, nil
 }
 
-// tailN reads last n lines from log file at given path.
-func tailN(ctx context.Context, path string, n int) ([]byte, error) {
-	cmd := exec.CommandContext(ctx, "/usr/bin/tail", "-n", strconv.Itoa(n), path)
+// tailN reads last l.n lines from log file at given path using `tail` command.
+func (l *Logs) tailN(ctx context.Context, path string) ([]byte, error) {
+	cmd := exec.CommandContext(ctx, "/usr/bin/tail", "-n", strconv.Itoa(l.n), path)
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 	b, err := cmd.Output()
 	if err != nil {
-		return b, fmt.Errorf("TailN(%s, %d): %s: %s", path, n, err, stderr.String())
+		return b, fmt.Errorf("TailN(%s, %d): %s: %s", path, l.n, err, stderr.String())
 	}
 	return b, nil
 }
