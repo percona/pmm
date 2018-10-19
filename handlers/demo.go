43842b2ccd7f72d5935ef2e3297e3cdd1fa26d60
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2018-10-19 09:52:52 +0300

    Update license text to the modern rendering.

--- handlers/demo.go
+++ handlers/demo.go
@@ -2,8 +2,8 @@
 // Copyright (C) 2017 Percona LLC
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
@@ -12,7 +12,7 @@
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 package handlers
 
--- services/qan/utils_test.go
+++ services/qan/utils_test.go
@@ -2,8 +2,8 @@
 // Copyright (C) 2017 Percona LLC
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
@@ -12,7 +12,7 @@
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 package qan
 
@@ -24,13 +24,13 @@ import (
 
 func TestSanitizeDSN(t *testing.T) {
 	uris := map[string]string{
-		"admin:abc123@127.0.0.1:100":                                                          "admin:***@127.0.0.1:100",
-		"localhost:27017/":                                                                    "localhost:27017",
-		"localhost:27017?opt=5":                                                               "localhost:27017",
-		"localhost":                                                                           "localhost",
-		"admin:abc123@localhost:1,localhost:2":                                                "admin:***@localhost:1,localhost:2",
-		"root:qwertyUIOP)(*&^%$#@1@localhost":                                                 "root:***@localhost",
-		"root:qwerty:UIOP)(*&^%$#@1@localhost":                                                "root:***@localhost",
+		"admin:abc123@127.0.0.1:100":           "admin:***@127.0.0.1:100",
+		"localhost:27017/":                     "localhost:27017",
+		"localhost:27017?opt=5":                "localhost:27017",
+		"localhost":                            "localhost",
+		"admin:abc123@localhost:1,localhost:2": "admin:***@localhost:1,localhost:2",
+		"root:qwertyUIOP)(*&^%$#@1@localhost":  "root:***@localhost",
+		"root:qwerty:UIOP)(*&^%$#@1@localhost": "root:***@localhost",
 		"mysql57:secret_password@tcp(mysql57.ckpwzom1xccn.eu-west-1.rds.amazonaws.com:3306)/": "mysql57:***@tcp(mysql57.ckpwzom1xccn.eu-west-1.rds.amazonaws.com:3306)",
 	}
 	for uri, expected := range uris {
