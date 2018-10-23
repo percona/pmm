77ff633b91b7f373e86219e648faf87eae7f31a7
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2018-10-23 15:58:11 +0300

    PMM-3110 ClickHouse migration. Initial grpc server.

---
+++ db.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+
+	_ "github.com/kshvakov/clickhouse"
+
+	"github.com/Percona-Lab/qan-api/migrations"
+	"github.com/golang-migrate/migrate"
+	_ "github.com/golang-migrate/migrate/database/clickhouse"
+	"github.com/golang-migrate/migrate/source/go_bindata"
+	"github.com/jmoiron/sqlx"
+)
+
+// NewDB return updated db.
+func NewDB(dsn string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("clickhouse", dsn)
+	if err != nil {
+		log.Fatal("Connection: ", err)
+	}
+
+	if err := runMigrations(dsn); err != nil {
+		log.Fatal("Migrations: ", err)
+	}
+	log.Println("Migrations applied.")
+	return db, nil
+}
+
+func runMigrations(dsn string) error {
+	s := bindata.Resource(migrations.AssetNames(),
+		func(name string) ([]byte, error) {
+			return migrations.Asset(name)
+		})
+
+	d, err := bindata.WithInstance(s)
+	if err != nil {
+		return err
+	}
+	log.Println("dsn: ", dsn)
+	m, err := migrate.NewWithSourceInstance("go-bindata", d, dsn)
+	if err != nil {
+		return err
+	}
+
+	// run up to the latest migration
+	err = m.Up()
+	if err == migrate.ErrNoChange {
+		return nil
+	}
+	return err
+}
