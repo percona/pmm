5fc298b17e419697aecc4c1895f1eec1901ccc3d
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-01-15 22:59:41 +0300

    PMM-3172 Use map for SetStateRequest.

--- api/agent/agent.validator.pb.go
+++ api/agent/agent.validator.pb.go
@@ -44,13 +44,7 @@ func (this *PingResponse) Validate() error {
 	return nil
 }
 func (this *SetStateRequest) Validate() error {
-	for _, item := range this.AgentProcesses {
-		if item != nil {
-			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(item); err != nil {
-				return github_com_mwitkow_go_proto_validators.FieldError("AgentProcesses", err)
-			}
-		}
-	}
+	// Validation of proto3 map<> fields is unsupported.
 	return nil
 }
 func (this *SetStateRequest_AgentProcess) Validate() error {
