4563818d2b4e25bd3160376261a117a1dcae526d
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-02-05 15:44:16 +0300

    PMM-3479 More version parsing code.

--- version/release.go
+++ version/release.go
@@ -1,9 +1,6 @@
-// Package version provides exported variables that should be set during build to specify PMM version.
 package version
 
 import (
-	"fmt"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -34,34 +31,6 @@ var (
 	// Git branch name, e.g. "master".
 	Branch string
 )
-
-var versionRE = regexp.MustCompile(`^(\d+)\.(\d+)\.(\d+)(.*)$`)
-
-type ParsedVersion struct {
-	Major int
-	Minor int
-	Patch int
-	Rest  string
-}
-
-func Parse(version string) (ParsedVersion, error) {
-	m := versionRE.FindStringSubmatch(version)
-	if len(m) != 5 {
-		return ParsedVersion{}, fmt.Errorf("failed to parse %q", version)
-	}
-	pv := ParsedVersion{Rest: m[4]}
-	var err error
-	if pv.Major, err = strconv.Atoi(m[1]); err != nil {
-		return ParsedVersion{}, err
-	}
-	if pv.Minor, err = strconv.Atoi(m[2]); err != nil {
-		return ParsedVersion{}, err
-	}
-	if pv.Patch, err = strconv.Atoi(m[3]); err != nil {
-		return ParsedVersion{}, err
-	}
-	return pv, nil
-}
 
 func ShortInfo() string {
 	if ProjectName == "" {
