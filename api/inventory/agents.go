e0fef713bb697e9049735b57ef19cc51e6f26083
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2019-03-20 15:37:38 +0200

    PMM-3657 API for PostgreSQL - implement interfaces.

--- api/inventory/agents.go
+++ api/inventory/agents.go
@@ -14,6 +14,7 @@ func (*RDSExporter) agent()             {}
 func (*ExternalExporter) agent()        {}
 func (*MongoDBExporter) agent()         {}
 func (*QANMySQLPerfSchemaAgent) agent() {}
+func (*PostgresExporter) agent()        {}
 
 func (a *PMMAgent) ID() string                { return a.AgentId }
 func (a *NodeExporter) ID() string            { return a.AgentId }
@@ -22,3 +23,4 @@ func (a *RDSExporter) ID() string             { return a.AgentId }
 func (a *ExternalExporter) ID() string        { return a.AgentId }
 func (a *MongoDBExporter) ID() string         { return a.AgentId }
 func (a *QANMySQLPerfSchemaAgent) ID() string { return a.AgentId }
+func (a *PostgresExporter) ID() string        { return a.AgentId }
