7b62184b09edfa381e3d772c6cda01ec45854c6a
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-04-05 15:04:15 +0300

    PMM-3798 Try to detect Node address.

---
+++ nodeinfo/nodeinfo_test.go
@@ -0,0 +1,24 @@
+package nodeinfo
+
+import (
+	"net"
+	"runtime"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	info := Get()
+
+	if runtime.GOOS != info.Distro {
+		t.Errorf("expected %q distro, got %q", runtime.GOOS, info.Distro)
+	}
+
+	// all our test environments have IPv4 addresses
+	ip := net.ParseIP(info.PublicAddress)
+	if ip == nil {
+		t.Fatalf("failed to parse %q as IP address", info.PublicAddress)
+	}
+	if ip.To4() == nil {
+		t.Fatalf("failed to parse %q as IPv4 address", info.PublicAddress)
+	}
+}
