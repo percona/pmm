c797ea94d8f73b9b5a1d86cda036f903710cead9
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-04-05 15:04:15 +0300

    PMM-3798 Try to detect Node address.

--- nodeinfo/nodeinfo.go
+++ nodeinfo/nodeinfo.go
@@ -3,6 +3,7 @@ package nodeinfo
 
 import (
 	"io/ioutil"
+	"net"
 	"runtime"
 )
 
@@ -10,13 +11,17 @@ import (
 type NodeInfo struct {
 	Distro    string
 	MachineID string
+
+	// Public/external address that can be used for scraping by Prometheus.
+	PublicAddress string
 }
 
 // Get returns node information for current node.
 func Get() *NodeInfo {
 	return &NodeInfo{
-		Distro:    readDistro(),
-		MachineID: readMachineID(),
+		Distro:        readDistro(),
+		MachineID:     readMachineID(),
+		PublicAddress: readPublicAddress(),
 	}
 }
 
@@ -36,4 +41,38 @@ func readMachineID() string {
 		}
 	}
 	return ""
+}
+
+// TODO remove that completely once we have "zero port" feature
+func readPublicAddress() string {
+	var res string
+
+	ifaces, _ := net.Interfaces()
+	for _, iface := range ifaces {
+		// skip down and loopback interfaces
+		if iface.Flags&net.FlagUp == 0 {
+			continue
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		addrs, _ := iface.Addrs()
+		for _, addr := range addrs {
+			s := addr.String()
+			if ipnet, _ := addr.(*net.IPNet); ipnet != nil {
+				s = ipnet.IP.String()
+			}
+			if ip := net.ParseIP(s); ip != nil {
+				// prefer (return first) IPv4 address, but fallback to any IPv6
+				res = ip.String()
+				ip = ip.To4()
+				if ip != nil {
+					return ip.String()
+				}
+			}
+		}
+	}
+
+	return res
 }
