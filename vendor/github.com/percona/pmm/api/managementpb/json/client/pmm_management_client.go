f11fbef8ad0040261b8924a56110d7263b3c56aa
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-05-02 11:34:48 +0300

    Update API.

--- vendor/github.com/percona/pmm/api/managementpb/json/client/pmm_management_client.go
+++ vendor/github.com/percona/pmm/api/managementpb/json/client/pmm_management_client.go
@@ -11,8 +11,10 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
+	"github.com/percona/pmm/api/managementpb/json/client/mongo_db"
 	"github.com/percona/pmm/api/managementpb/json/client/my_sql"
 	"github.com/percona/pmm/api/managementpb/json/client/node"
+	"github.com/percona/pmm/api/managementpb/json/client/postgre_sql"
 	"github.com/percona/pmm/api/managementpb/json/client/service"
 )
 
@@ -59,9 +61,13 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *PMMManagem
 	cli := new(PMMManagement)
 	cli.Transport = transport
 
+	cli.MongoDB = mongo_db.New(transport, formats)
+
 	cli.MySQL = my_sql.New(transport, formats)
 
 	cli.Node = node.New(transport, formats)
+
+	cli.PostgreSQL = postgre_sql.New(transport, formats)
 
 	cli.Service = service.New(transport, formats)
 
@@ -109,9 +115,13 @@ func (cfg *TransportConfig) WithSchemes(schemes []string) *TransportConfig {
 
 // PMMManagement is a client for PMM management
 type PMMManagement struct {
+	MongoDB *mongo_db.Client
+
 	MySQL *my_sql.Client
 
 	Node *node.Client
+
+	PostgreSQL *postgre_sql.Client
 
 	Service *service.Client
 
@@ -122,9 +132,13 @@ type PMMManagement struct {
 func (c *PMMManagement) SetTransport(transport runtime.ClientTransport) {
 	c.Transport = transport
 
+	c.MongoDB.SetTransport(transport)
+
 	c.MySQL.SetTransport(transport)
 
 	c.Node.SetTransport(transport)
+
+	c.PostgreSQL.SetTransport(transport)
 
 	c.Service.SetTransport(transport)
 
