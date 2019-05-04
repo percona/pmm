7aebec8462a380210de687bdfc17262abcaef29f
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-05-04 14:23:16 +0300

    Expand interfaces.

--- api/agentpb/agent.go
+++ api/agentpb/agent.go
@@ -4,71 +4,114 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
-//go-sumtype:decl isServerMessage_Payload
 //go-sumtype:decl isAgentMessage_Payload
+//go-sumtype:decl isServerMessage_Payload
+//go-sumtype:decl AgentMessagePayload
+//go-sumtype:decl ServerMessagePayload
 
 // Workaround for https://github.com/golang/protobuf/issues/261.
 // Useful for helper functions.
 // TODO Refactor code to use
-// ServerRequestPayload, ServerResponsePayload, AgentRequestPayload, AgentResponsePayload instead.
+// AgentRequestPayload, AgentResponsePayload, ServerResponsePayload, ServerRequestPayload instead.
 type (
-	ServerMessagePayload = isServerMessage_Payload
 	AgentMessagePayload  = isAgentMessage_Payload
+	ServerMessagePayload = isServerMessage_Payload
 )
 
-//go-sumtype:decl ServerRequestPayload
-//go-sumtype:decl ServerResponsePayload
+// code below uses the same order as definitions in agent.proto
+
 //go-sumtype:decl AgentRequestPayload
 //go-sumtype:decl AgentResponsePayload
-
-type ServerRequestPayload interface {
-	server()
-	request()
-}
-
-type ServerResponsePayload interface {
-	server()
-	response()
-}
+//go-sumtype:decl ServerResponsePayload
+//go-sumtype:decl ServerRequestPayload
 
 type AgentRequestPayload interface {
-	agent()
-	request()
+	AgentMessageRequestPayload() isAgentMessage_Payload
+	sealed()
 }
 
 type AgentResponsePayload interface {
-	agent()
-	response()
+	AgentMessageResponsePayload() isAgentMessage_Payload
+	sealed()
 }
 
-// code below uses the same order as definitions in agent.proto (except ping/pong)
+type ServerResponsePayload interface {
+	ServerMessageResponsePayload() isServerMessage_Payload
+	sealed()
+}
 
-func (*Ping) agent()    {}
-func (*Ping) server()   {}
-func (*Ping) request()  {}
-func (*Pong) agent()    {}
-func (*Pong) server()   {}
-func (*Pong) response() {}
+type ServerRequestPayload interface {
+	ServerMessageRequestPayload() isServerMessage_Payload
+	sealed()
+}
 
 // AgentMessage request payloads
-func (*StateChangedRequest) agent()   {}
-func (*StateChangedRequest) request() {}
-func (*QANCollectRequest) agent()     {}
-func (*QANCollectRequest) request()   {}
+func (m *Ping) AgentMessageRequestPayload() isAgentMessage_Payload {
+	return &AgentMessage_Ping{Ping: m}
+}
+func (m *StateChangedRequest) AgentMessageRequestPayload() isAgentMessage_Payload {
+	return &AgentMessage_StateChanged{StateChanged: m}
+}
+func (m *QANCollectRequest) AgentMessageRequestPayload() isAgentMessage_Payload {
+	return &AgentMessage_QanCollect{QanCollect: m}
+}
 
 // AgentMessage response payloads
-func (*SetStateResponse) agent()    {}
-func (*SetStateResponse) response() {}
+func (m *Pong) AgentMessageResponsePayload() isAgentMessage_Payload {
+	return &AgentMessage_Pong{Pong: m}
+}
+func (m *SetStateResponse) AgentMessageResponsePayload() isAgentMessage_Payload {
+	return &AgentMessage_SetState{SetState: m}
+}
 
 // ServerMessage response payloads
-func (*StateChangedResponse) server()   {}
-func (*StateChangedResponse) response() {}
-func (*QANCollectResponse) server()     {}
-func (*QANCollectResponse) response()   {}
+func (m *Pong) ServerMessageResponsePayload() isServerMessage_Payload {
+	return &ServerMessage_Pong{Pong: m}
+}
+func (m *StateChangedResponse) ServerMessageResponsePayload() isServerMessage_Payload {
+	return &ServerMessage_StateChanged{StateChanged: m}
+}
+func (m *QANCollectResponse) ServerMessageResponsePayload() isServerMessage_Payload {
+	return &ServerMessage_QanCollect{QanCollect: m}
+}
 
 // ServerMessage request payloads
-func (*SetStateRequest) server()  {}
-func (*SetStateRequest) request() {}
+func (m *Ping) ServerMessageRequestPayload() isServerMessage_Payload {
+	return &ServerMessage_Ping{Ping: m}
+}
+func (m *SetStateRequest) ServerMessageRequestPayload() isServerMessage_Payload {
+	return &ServerMessage_SetState{SetState: m}
+}
+
+func (*Ping) sealed()                   {}
+func (m *StateChangedRequest) sealed()  {}
+func (m *QANCollectRequest) sealed()    {}
+func (*Pong) sealed()                   {}
+func (m *SetStateResponse) sealed()     {}
+func (m *StateChangedResponse) sealed() {}
+func (m *QANCollectResponse) sealed()   {}
+func (m *SetStateRequest) sealed()      {}
+
+// check interfaces
+var (
+	// AgentMessage request payloads
+	_ AgentRequestPayload = (*Ping)(nil)
+	_ AgentRequestPayload = (*StateChangedRequest)(nil)
+	_ AgentRequestPayload = (*QANCollectRequest)(nil)
+
+	// AgentMessage response payloads
+	_ AgentResponsePayload = (*Pong)(nil)
+	_ AgentResponsePayload = (*SetStateResponse)(nil)
+
+	// ServerMessage response payloads
+	_ ServerResponsePayload = (*Pong)(nil)
+	_ ServerResponsePayload = (*StateChangedResponse)(nil)
+	_ ServerResponsePayload = (*QANCollectResponse)(nil)
+
+	// ServerMessage request payloads
+	_ ServerRequestPayload = (*Ping)(nil)
+	_ ServerRequestPayload = (*SetStateRequest)(nil)
+)
 
 //go-sumtype:decl AgentParams
 
