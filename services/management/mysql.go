d845755fb128e10be5e68c35e161e8104d118357
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2019-05-07 16:57:19 +0300

    PMM-3986 Add Custom labels to Management API. (#184)

--- services/management/mysql.go
+++ services/management/mysql.go
@@ -49,10 +49,11 @@ func (s *MySQLService) Add(ctx context.Context, req *managementpb.AddMySQLReques
 
 	if e := s.db.InTransaction(func(tx *reform.TX) error {
 		service, err := models.AddNewService(tx.Querier, models.MySQLServiceType, &models.AddDBMSServiceParams{
-			ServiceName: req.ServiceName,
-			NodeID:      req.NodeId,
-			Address:     pointer.ToStringOrNil(req.Address),
-			Port:        pointer.ToUint16OrNil(uint16(req.Port)),
+			ServiceName:  req.ServiceName,
+			NodeID:       req.NodeId,
+			Address:      pointer.ToStringOrNil(req.Address),
+			Port:         pointer.ToUint16OrNil(uint16(req.Port)),
+			CustomLabels: req.CustomLabels,
 		})
 
 		if err != nil {
