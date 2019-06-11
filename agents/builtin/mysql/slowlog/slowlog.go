a79806ce0522a5281bc7f0394a654fc42c9fb9ee
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-06-11 15:17:01 +0300

    PMM-3694 Metrics for errors and warning. (#72)

--- agents/builtin/mysql/slowlog/slowlog.go
+++ agents/builtin/mysql/slowlog/slowlog.go
@@ -289,20 +289,22 @@ func makeBuckets(agentID string, res event.Result, periodStart time.Time, period
 
 	for _, v := range res.Class {
 		mb := &qanpb.MetricsBucket{
-			Queryid:             v.Id,
-			Fingerprint:         v.Fingerprint,
-			Database:            "",
-			Schema:              v.Db,
-			Username:            v.User,
-			ClientHost:          v.Host,
-			AgentId:             agentID,
-			AgentType:           inventorypb.AgentType_QAN_MYSQL_SLOWLOG_AGENT,
-			PeriodStartUnixSecs: uint32(periodStart.Unix()),
-			PeriodLengthSecs:    periodLengthSecs,
-			Example:             v.Example.Query,
-			ExampleFormat:       qanpb.ExampleFormat_EXAMPLE,
-			ExampleType:         qanpb.ExampleType_RANDOM,
-			NumQueries:          float32(v.TotalQueries),
+			Queryid:              v.Id,
+			Fingerprint:          v.Fingerprint,
+			Database:             "",
+			Schema:               v.Db,
+			Username:             v.User,
+			ClientHost:           v.Host,
+			AgentId:              agentID,
+			AgentType:            inventorypb.AgentType_QAN_MYSQL_SLOWLOG_AGENT,
+			PeriodStartUnixSecs:  uint32(periodStart.Unix()),
+			PeriodLengthSecs:     periodLengthSecs,
+			Example:              v.Example.Query,
+			ExampleFormat:        qanpb.ExampleFormat_EXAMPLE,
+			ExampleType:          qanpb.ExampleType_RANDOM,
+			NumQueries:           float32(v.TotalQueries),
+			Errors:               errListsToMap(v.ErrorsCode, v.ErrorsCount),
+			NumQueriesWithErrors: v.NumQueriesWithErrors,
 		}
 
 		// If key has suffix _time or _wait than field is TimeMetrics.
@@ -544,4 +546,12 @@ func makeBuckets(agentID string, res event.Result, periodStart time.Time, period
 // Changes returns channel that should be read until it is closed.
 func (s *SlowLog) Changes() <-chan Change {
 	return s.changes
+}
+
+func errListsToMap(k, v []uint64) map[uint64]uint64 {
+	m := map[uint64]uint64{}
+	for i, e := range k {
+		m[e] = v[i]
+	}
+	return m
 }
