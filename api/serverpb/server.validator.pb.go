e7247599fcf58c59bcfae885b9594554f86cf434
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-06-25 13:14:43 +0300

    PMM-4246 Add APIs for server settings.

--- api/serverpb/server.validator.pb.go
+++ api/serverpb/server.validator.pb.go
@@ -6,6 +6,7 @@ package serverpb
 import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	_ "github.com/golang/protobuf/ptypes/duration"
 	_ "github.com/golang/protobuf/ptypes/timestamp"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger/options"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
@@ -25,6 +26,59 @@ func (this *VersionResponse) Validate() error {
 	if this.Timestamp != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Timestamp); err != nil {
 			return github_com_mwitkow_go_proto_validators.FieldError("Timestamp", err)
+		}
+	}
+	return nil
+}
+func (this *MetricsResolutions) Validate() error {
+	if this.Hr != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Hr); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Hr", err)
+		}
+	}
+	if this.Mr != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Mr); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Mr", err)
+		}
+	}
+	if this.Lr != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Lr); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Lr", err)
+		}
+	}
+	return nil
+}
+func (this *Settings) Validate() error {
+	if this.MetricsResolutions != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.MetricsResolutions); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("MetricsResolutions", err)
+		}
+	}
+	return nil
+}
+func (this *GetSettingsRequest) Validate() error {
+	return nil
+}
+func (this *GetSettingsResponse) Validate() error {
+	if this.Settings != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Settings); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Settings", err)
+		}
+	}
+	return nil
+}
+func (this *ChangeSettingsRequest) Validate() error {
+	if this.MetricsResolutions != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.MetricsResolutions); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("MetricsResolutions", err)
+		}
+	}
+	return nil
+}
+func (this *ChangeSettingsResponse) Validate() error {
+	if this.Settings != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Settings); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("Settings", err)
 		}
 	}
 	return nil
