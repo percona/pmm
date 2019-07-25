defc17f23ccd9e4b7f756daf6b1a337cdcfa171e
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-07-25 14:43:26 +0300

    PMM-4351 Remove unused fields.

--- api/agentpb/collector.validator.pb.go
+++ api/agentpb/collector.validator.pb.go
@@ -41,7 +41,6 @@ func (this *MetricsBucket) Validate() error {
 }
 func (this *MetricsBucket_Common) Validate() error {
 	// Validation of proto3 map<> fields is unsupported.
-	// Validation of proto3 map<> fields is unsupported.
 	return nil
 }
 func (this *MetricsBucket_MySQL) Validate() error {
