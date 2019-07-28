ddc33bfe8a73a1b9890186969f8efd0f0033d149
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-07-28 16:54:25 +0300

    PMM-4379 WIP

--- version/update.go
+++ version/update.go
@@ -6,10 +6,10 @@ import (
 
 // UpdateCheckResult represents `pmm-update -check` result.
 type UpdateCheckResult struct {
-	InstalledVersion string     `json:"installed_version"`
-	InstalledTime    *time.Time `json:"installed_time"`
+	InstalledRPMVersion string     `json:"installed_rpm_version"`
+	InstalledTime       *time.Time `json:"installed_time"`
 
-	LatestVersion string     `json:"latest_version"`
-	LatestTime    *time.Time `json:"latest_time"`
-	LatestRepo    string     `json:"latest_repo"`
+	LatestRPMVersion string     `json:"latest_rpm_version"`
+	LatestTime       *time.Time `json:"latest_time"`
+	LatestRepo       string     `json:"latest_repo"`
 }
