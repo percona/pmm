30d49f5346ae4196c074669ca0451550dc0158b9
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-08-08 17:12:03 +0300

    Install Go from tarball.

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -12,8 +12,13 @@ sed -i '/nodocs/d' /etc/yum.conf
 # reinstall with man pages
 yum reinstall -y yum rpm
 
-yum install -y golang ansible-lint mc tmux psmisc which bash-completion bash-completion-extras man man-pages
+yum install -y ansible-lint mc tmux psmisc which bash-completion bash-completion-extras man man-pages
 
+# install the same verison as used by PMM build process
+curl https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz -o /tmp/golang.tar.gz
+tar -C /usr/local -xzf /tmp/golang.tar.gz
+update-alternatives --install "/usr/bin/go" "go" "/usr/local/go/bin/go" 0
+update-alternatives --set go /usr/local/go/bin/go
 go env
 
 go get golang.org/x/tools/cmd/gopls \
