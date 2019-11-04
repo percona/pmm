c4d31c63a8973d785d538abd32906bed76930f31
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-11-04 15:48:25 +0300

    PMM-4895 Fix nginx config after update. (#70)

--- pkg/yum/yum_test.go
+++ pkg/yum/yum_test.go
@@ -66,7 +66,7 @@ func TestCheck(t *testing.T) {
 	if updateAvailable {
 		t.Log("Assuming pmm-update update is available.")
 		assert.True(t, res.UpdateAvailable, "update should be available")
-		assert.True(t, strings.HasPrefix(res.LatestNewsURL, "https://per.co.na/pmm/2."), "%s", res.LatestNewsURL)
+		assert.True(t, strings.HasPrefix(res.LatestNewsURL, "https://per.co.na/pmm/2."), "latest_news_url = %q", res.LatestNewsURL)
 		assert.NotEqual(t, res.Installed.Version, res.Latest.Version, "versions should not be the same")
 		assert.NotEqual(t, res.Installed.FullVersion, res.Latest.FullVersion, "versions should not be the same")
 		assert.NotEqual(t, *res.Installed.BuildTime, *res.Latest.BuildTime, "build times should not be the same (%s)", *res.Installed.BuildTime)
