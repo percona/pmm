e15e998bbd3b9906fa11c925f89eb17905e96506
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-11-20 17:03:04 +0300

    Fix test. (#74)

--- pkg/yum/yum_test.go
+++ pkg/yum/yum_test.go
@@ -27,6 +27,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var gaReleaseDate = time.Date(2019, 9, 18, 0, 0, 0, 0, time.UTC) //nolint
+
 func TestInstalled(t *testing.T) {
 	res, err := Installed(context.Background(), "pmm-update")
 	require.NoError(t, err)
@@ -34,7 +36,7 @@ func TestInstalled(t *testing.T) {
 	assert.True(t, strings.HasPrefix(res.Installed.Version, "2."), "%s", res.Installed.Version)
 	assert.True(t, strings.HasPrefix(res.Installed.FullVersion, "2."), "%s", res.Installed.FullVersion)
 	require.NotEmpty(t, res.Installed.BuildTime)
-	assert.True(t, time.Since(*res.Installed.BuildTime) < 60*24*time.Hour, "InstalledTime = %s", res.Installed.BuildTime)
+	assert.True(t, res.Installed.BuildTime.After(gaReleaseDate), "Installed.BuildTime = %s", res.Installed.BuildTime)
 	assert.Equal(t, "local", res.Installed.Repo)
 }
 
@@ -45,13 +47,13 @@ func TestCheck(t *testing.T) {
 	assert.True(t, strings.HasPrefix(res.Installed.Version, "2."), "%s", res.Installed.Version)
 	assert.True(t, strings.HasPrefix(res.Installed.FullVersion, "2."), "%s", res.Installed.FullVersion)
 	require.NotEmpty(t, res.Installed.BuildTime)
-	assert.True(t, time.Since(*res.Installed.BuildTime) < 60*24*time.Hour, "InstalledTime = %s", res.Installed.BuildTime)
+	assert.True(t, res.Installed.BuildTime.After(gaReleaseDate), "Installed.BuildTime = %s", res.Installed.BuildTime)
 	assert.Equal(t, "local", res.Installed.Repo)
 
 	assert.True(t, strings.HasPrefix(res.Latest.Version, "2."), "%s", res.Latest.Version)
 	assert.True(t, strings.HasPrefix(res.Latest.FullVersion, "2."), "%s", res.Latest.FullVersion)
 	require.NotEmpty(t, res.Latest.BuildTime)
-	assert.True(t, time.Since(*res.Latest.BuildTime) < 60*24*time.Hour, "LatestTime = %s", res.Latest.BuildTime)
+	assert.True(t, res.Latest.BuildTime.After(gaReleaseDate), "Latest.BuildTime = %s", res.Latest.BuildTime)
 	assert.NotEmpty(t, res.Latest.Repo)
 
 	// We assume that the latest percona/pmm-server:2 and perconalab/pmm-server:dev-latest images
