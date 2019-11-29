580ae663b97e932ff755642dcb194b48d4465518
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-11-29 09:13:09 +0300

    PMM-4949 RDS Management API. (#466)

--- api/managementpb/json/client/pmm_management_client.go
+++ api/managementpb/json/client/pmm_management_client.go
@@ -12,12 +12,12 @@ import (
 	strfmt "github.com/go-openapi/strfmt"
 
 	"github.com/percona/pmm/api/managementpb/json/client/actions"
-	"github.com/percona/pmm/api/managementpb/json/client/discovery"
 	"github.com/percona/pmm/api/managementpb/json/client/mongo_db"
 	"github.com/percona/pmm/api/managementpb/json/client/my_sql"
 	"github.com/percona/pmm/api/managementpb/json/client/node"
 	"github.com/percona/pmm/api/managementpb/json/client/postgre_sql"
 	"github.com/percona/pmm/api/managementpb/json/client/proxy_sql"
+	"github.com/percona/pmm/api/managementpb/json/client/rds"
 	"github.com/percona/pmm/api/managementpb/json/client/service"
 )
 
@@ -66,8 +66,6 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *PMMManagem
 
 	cli.Actions = actions.New(transport, formats)
 
-	cli.Discovery = discovery.New(transport, formats)
-
 	cli.MongoDB = mongo_db.New(transport, formats)
 
 	cli.MySQL = my_sql.New(transport, formats)
@@ -77,6 +75,8 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *PMMManagem
 	cli.PostgreSQL = postgre_sql.New(transport, formats)
 
 	cli.ProxySQL = proxy_sql.New(transport, formats)
+
+	cli.RDS = rds.New(transport, formats)
 
 	cli.Service = service.New(transport, formats)
 
@@ -126,8 +126,6 @@ func (cfg *TransportConfig) WithSchemes(schemes []string) *TransportConfig {
 type PMMManagement struct {
 	Actions *actions.Client
 
-	Discovery *discovery.Client
-
 	MongoDB *mongo_db.Client
 
 	MySQL *my_sql.Client
@@ -137,6 +135,8 @@ type PMMManagement struct {
 	PostgreSQL *postgre_sql.Client
 
 	ProxySQL *proxy_sql.Client
+
+	RDS *rds.Client
 
 	Service *service.Client
 
@@ -149,8 +149,6 @@ func (c *PMMManagement) SetTransport(transport runtime.ClientTransport) {
 
 	c.Actions.SetTransport(transport)
 
-	c.Discovery.SetTransport(transport)
-
 	c.MongoDB.SetTransport(transport)
 
 	c.MySQL.SetTransport(transport)
@@ -160,6 +158,8 @@ func (c *PMMManagement) SetTransport(transport runtime.ClientTransport) {
 	c.PostgreSQL.SetTransport(transport)
 
 	c.ProxySQL.SetTransport(transport)
+
+	c.RDS.SetTransport(transport)
 
 	c.Service.SetTransport(transport)
 
