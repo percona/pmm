164cc1ae9e9148e8377501ec557441d42ab9b39d
Nurlan Moldomurov <bupychuk1989@gmail.com>
2019-12-11 13:25:46 +0300

    PMM-4927 Fix test failures. (#140)

--- agents/mysql/perfschema/perfschema_test.go
+++ agents/mysql/perfschema/perfschema_test.go
@@ -234,6 +234,7 @@ func TestPerfSchema(t *testing.T) {
 	require.NoError(t, err)
 	tests.LogTable(t, structs)
 
+	var rowsExamined float32
 	mySQLVersion, mySQLVendor := tests.MySQLVersion(t, sqlDB)
 	var digests map[string]string // digest_text/fingerprint to digest/query_id
 	switch fmt.Sprintf("%s-%s", mySQLVersion, mySQLVendor) {
@@ -248,6 +249,13 @@ func TestPerfSchema(t *testing.T) {
 			"SELECT * FROM `city`": "05292e6e5fb868ce2864918d5e934cb3",
 		}
 
+	case "8.0-oracle":
+		digests = map[string]string{
+			"SELECT `sleep` (?)":   "0b1b1c39d4ee2dda7df2a532d0a23406d86bd34e2cd7f22e3f7e9dedadff9b69",
+			"SELECT * FROM `city`": "950bdc225cf73c9096ba499351ed4376f4526abad3d8ceabc168b6b28cfc9eab",
+		}
+		rowsExamined = 1
+
 	case "5.6-percona":
 		digests = map[string]string{
 			"SELECT `sleep` (?)":   "d8dc769e3126abd5578679f520bad1a5",
@@ -259,7 +267,7 @@ func TestPerfSchema(t *testing.T) {
 			"SELECT * FROM `city`": "9c799bdb2460f79b3423b77cd10403da",
 		}
 
-	case "8.0-oracle", "8.0-percona": // Percona switched to upstream's implementation
+	case "8.0-percona":
 		digests = map[string]string{
 			"SELECT `sleep` (?)":   "0b1b1c39d4ee2dda7df2a532d0a23406d86bd34e2cd7f22e3f7e9dedadff9b69",
 			"SELECT * FROM `city`": "950bdc225cf73c9096ba499351ed4376f4526abad3d8ceabc168b6b28cfc9eab",
@@ -322,8 +330,10 @@ func TestPerfSchema(t *testing.T) {
 				MQueryTimeSum:       actual.Common.MQueryTimeSum,
 			},
 			Mysql: &agentpb.MetricsBucket_MySQL{
-				MRowsSentCnt: 1,
-				MRowsSentSum: 1,
+				MRowsSentCnt:     1,
+				MRowsSentSum:     1,
+				MRowsExaminedCnt: rowsExamined,
+				MRowsExaminedSum: rowsExamined,
 			},
 		}
 		expected.Common.Queryid = digests[expected.Common.Fingerprint]
