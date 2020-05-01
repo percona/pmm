7b2bc5d24503bf8fde0c4b8a8246d3f7e805bea1
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-05-01 21:02:03 +0300

    Revert "Revert "PMM-5777 Inventory add external exporter endpoint. (#516)""
    This reverts commit bd0fb8d2aac255d4fd2164a09d43d6f55e1ac2e4.

--- api/inventorypb/agents.go
+++ api/inventorypb/agents.go
@@ -20,3 +20,4 @@ func (*QANMySQLSlowlogAgent) sealedAgent()           {}
 func (*QANMongoDBProfilerAgent) sealedAgent()        {}
 func (*QANPostgreSQLPgStatementsAgent) sealedAgent() {}
 func (*RDSExporter) sealedAgent()                    {}
+func (*ExternalExporter) sealedAgent()               {}
