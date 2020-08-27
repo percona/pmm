97f894a5153cfe36f1e6a2e39df9bfd8ddde01a5
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2020-08-27 17:29:07 +0300

    PMM-6539 Move backup APIs to own package for now

--- api/managementpb/json/client/pmm_management_client.go
+++ api/managementpb/json/client/pmm_management_client.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/percona/pmm/api/managementpb/json/client/actions"
 	"github.com/percona/pmm/api/managementpb/json/client/annotation"
-	"github.com/percona/pmm/api/managementpb/json/client/backups"
 	"github.com/percona/pmm/api/managementpb/json/client/external"
 	"github.com/percona/pmm/api/managementpb/json/client/mongo_db"
 	"github.com/percona/pmm/api/managementpb/json/client/my_sql"
@@ -68,7 +67,6 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *PMMManagem
 	cli.Transport = transport
 	cli.Actions = actions.New(transport, formats)
 	cli.Annotation = annotation.New(transport, formats)
-	cli.Backups = backups.New(transport, formats)
 	cli.External = external.New(transport, formats)
 	cli.MongoDB = mongo_db.New(transport, formats)
 	cli.MySQL = my_sql.New(transport, formats)
@@ -126,8 +124,6 @@ type PMMManagement struct {
 
 	Annotation annotation.ClientService
 
-	Backups backups.ClientService
-
 	External external.ClientService
 
 	MongoDB mongo_db.ClientService
@@ -154,7 +150,6 @@ func (c *PMMManagement) SetTransport(transport runtime.ClientTransport) {
 	c.Transport = transport
 	c.Actions.SetTransport(transport)
 	c.Annotation.SetTransport(transport)
-	c.Backups.SetTransport(transport)
 	c.External.SetTransport(transport)
 	c.MongoDB.SetTransport(transport)
 	c.MySQL.SetTransport(transport)
