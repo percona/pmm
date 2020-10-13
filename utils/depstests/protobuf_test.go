7f03c5fa34c502086f28e78b6e59a91486141219
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2020-10-13 12:48:19 +0300

    PMM-6760 Fix protobuf dependency (#483)
    * PMM-6760 Fix protobuf dependency
    
    * PMM-6760 Fix
    
    * PMM-6760 Add nolint

---
+++ utils/depstests/protobuf_test.go
@@ -0,0 +1,22 @@
+package depstests
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/jsonpb" //nolint:staticcheck
+	"github.com/golang/protobuf/ptypes" //nolint:staticcheck
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDuration(t *testing.T) {
+	// https://github.com/golang/protobuf/issues/883
+	// https://github.com/golang/protobuf/issues/1219
+	// https://jira.percona.com/browse/PMM-6760
+
+	var m jsonpb.Marshaler
+	s, err := m.MarshalToString(ptypes.DurationProto(-time.Nanosecond))
+	require.NoError(t, err)
+	assert.Equal(t, `"-0.000000001s"`, s)
+}
