e7cb1c4a8e4d1df6a2c75a6de88e66d499d92ed2
Artem Gavrilov <artem.gavrilov@percona.com>
2020-10-20 18:04:23 +0200

    SAAS-377 Disable checks by name (#588)
    * SAAS-377 Add API for checks disabling
    
    * SAAS-377 Make checks API consistent with other PMM APIs
    
    * SAAS-377 Eliminate types collision in swagger generated code
    
    * SAAS-377 Renaming
    
    * SAAS-377 Fix typo
    
    * SAAS-377 Renaming
    
    * SAAS-377 Refactoring
    
    * SAAS-377 Renaming
    
    * SAAS-377 Minor tweaks
    
    Co-authored-by: Alexey Palazhchenko <alexey.palazhchenko@percona.com>

--- api/managementpb/json/client/security_checks/get_security_check_results_responses.go
+++ api/managementpb/json/client/security_checks/get_security_check_results_responses.go
@@ -119,42 +119,6 @@ func (o *GetSecurityCheckResultsDefault) readResponse(response runtime.ClientRes
 	return nil
 }
 
-/*DetailsItems0 details items0
-swagger:model DetailsItems0
-*/
-type DetailsItems0 struct {
-
-	// type url
-	TypeURL string `json:"type_url,omitempty"`
-
-	// value
-	// Format: byte
-	Value strfmt.Base64 `json:"value,omitempty"`
-}
-
-// Validate validates this details items0
-func (o *DetailsItems0) Validate(formats strfmt.Registry) error {
-	return nil
-}
-
-// MarshalBinary interface implementation
-func (o *DetailsItems0) MarshalBinary() ([]byte, error) {
-	if o == nil {
-		return nil, nil
-	}
-	return swag.WriteJSON(o)
-}
-
-// UnmarshalBinary interface implementation
-func (o *DetailsItems0) UnmarshalBinary(b []byte) error {
-	var res DetailsItems0
-	if err := swag.ReadJSON(b, &res); err != nil {
-		return err
-	}
-	*o = res
-	return nil
-}
-
 /*GetSecurityCheckResultsDefaultBody get security check results default body
 swagger:model GetSecurityCheckResultsDefaultBody
 */
