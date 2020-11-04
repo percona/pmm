bd813553d91fd9627161e583891ae9a63eadb5ca
Andrii Skomorokhov <askomorokhov@users.noreply.github.com>
2020-11-04 15:54:10 +0200

    PMM-6050 Add external group (#606)
    * PMM-6050 Add external group
    
    * PMM-6050 Make external group required.
    
    * PMM-6050 Make external group optional.

--- api/inventorypb/json/client/services/get_service_responses.go
+++ api/inventorypb/json/client/services/get_service_responses.go
@@ -408,6 +408,9 @@ type GetServiceOKBodyExternal struct {
 
 	// Custom user-assigned labels.
 	CustomLabels map[string]string `json:"custom_labels,omitempty"`
+
+	// Group name of external service.
+	Group string `json:"group,omitempty"`
 }
 
 // Validate validates this get service OK body external
