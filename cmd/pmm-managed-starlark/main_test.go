3ec2b45c3ce71bf8bc8deb767622e80c7b410fc3
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2020-11-04 18:39:00 +0300

    Fix fluky test (#509)

--- cmd/pmm-managed-starlark/main_test.go
+++ cmd/pmm-managed-starlark/main_test.go
@@ -32,7 +32,11 @@ import (
 
 const (
 	invalidStarlarkScriptStderr = "Error running starlark script: thread invalid starlark script: failed to execute function check_context: function check_context accepts no arguments (2 given)"
-	memoryConsumingScriptStderr = "fatal error: runtime: out of memory"
+
+	// Possible errors:
+	// fatal error: runtime: out of memory
+	// fatal error: out of memory allocating heap arena metadata
+	memoryConsumingScriptStderr = "out of memory"
 )
 
 var validQueryActionResult = []map[string]interface{}{
