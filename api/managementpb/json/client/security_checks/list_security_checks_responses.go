0cc1f21473562a38cb084c70d73cf845231f1df8
Palash Nigam <palash.nigam@percona.com>
2020-11-16 18:11:30 +0530

    SAAS-413 Add summary field

--- api/managementpb/json/client/security_checks/list_security_checks_responses.go
+++ api/managementpb/json/client/security_checks/list_security_checks_responses.go
@@ -128,6 +128,9 @@ type ChecksItems0 struct {
 	// disabled
 	Disabled bool `json:"disabled,omitempty"`
 
+	// summary
+	Summary string `json:"summary,omitempty"`
+
 	// description
 	Description string `json:"description,omitempty"`
 }
