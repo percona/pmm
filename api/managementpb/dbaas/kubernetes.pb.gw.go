e6d5029c4ca4845ae08771799f40d33b28e31fa4
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-12-17 09:56:37 +0100

    PMM-6944 New endpoint for get kutebernetes cluster.

--- api/managementpb/dbaas/kubernetes.pb.gw.go
+++ api/managementpb/dbaas/kubernetes.pb.gw.go
@@ -135,6 +135,40 @@ func local_request_Kubernetes_UnregisterKubernetesCluster_0(ctx context.Context,
 
 }
 
+func request_Kubernetes_GetKubernetesCluster_0(ctx context.Context, marshaler runtime.Marshaler, client KubernetesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetKubernetesClusterRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetKubernetesCluster(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Kubernetes_GetKubernetesCluster_0(ctx context.Context, marshaler runtime.Marshaler, server KubernetesServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetKubernetesClusterRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetKubernetesCluster(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterKubernetesHandlerServer registers the http handlers for service Kubernetes to "mux".
 // UnaryRPC     :call KubernetesServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -207,6 +241,29 @@ func RegisterKubernetesHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		}
 
 		forward_Kubernetes_UnregisterKubernetesCluster_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_Kubernetes_GetKubernetesCluster_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Kubernetes_GetKubernetesCluster_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Kubernetes_GetKubernetesCluster_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -311,6 +368,26 @@ func RegisterKubernetesHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("POST", pattern_Kubernetes_GetKubernetesCluster_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Kubernetes_GetKubernetesCluster_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Kubernetes_GetKubernetesCluster_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -320,6 +397,8 @@ var (
 	pattern_Kubernetes_RegisterKubernetesCluster_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"v1", "management", "DBaaS", "Kubernetes", "Register"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Kubernetes_UnregisterKubernetesCluster_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"v1", "management", "DBaaS", "Kubernetes", "Unregister"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Kubernetes_GetKubernetesCluster_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"v1", "management", "DBaaS", "Kubernetes", "Get"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -328,4 +407,6 @@ var (
 	forward_Kubernetes_RegisterKubernetesCluster_0 = runtime.ForwardResponseMessage
 
 	forward_Kubernetes_UnregisterKubernetesCluster_0 = runtime.ForwardResponseMessage
+
+	forward_Kubernetes_GetKubernetesCluster_0 = runtime.ForwardResponseMessage
 )
