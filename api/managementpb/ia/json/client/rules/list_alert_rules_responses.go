8010029b43f75066ce2451fe4c99d643ae7d0894
Dávid Mikuš <dasiodev@gmail.com>
2021-01-28 10:54:29 +0100

    Revert "PMM-7257 Added expr field to rule (#657)" (#665)
    This reverts commit fa07a3c7f118abfea81e4e5e87ab0bb41a72d253.

--- api/managementpb/ia/json/client/rules/list_alert_rules_responses.go
+++ api/managementpb/ia/json/client/rules/list_alert_rules_responses.go
@@ -297,9 +297,6 @@ type RulesItems0 struct {
 	// Format: date-time
 	CreatedAt strfmt.DateTime `json:"created_at,omitempty"`
 
-	// Template expression with rule parameters.
-	Expr string `json:"expr,omitempty"`
-
 	// template
 	Template *RulesItems0Template `json:"template,omitempty"`
 }
