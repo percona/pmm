3ff6c74c8c6ae74752ced33a09cf10d9b490af1c
Jaroslav Musil <49949480+yareach@users.noreply.github.com>
2021-02-22 19:36:11 +0100

    PMM-5784 pt-pg-summary. (#614)
    * PMM-5784 Created structures for pt-pg-summary.
    
    * PMM-6982 IA settings API (#620)
    
    * PMM-6982 Add comments
    
    * PMM-6982 IA settings API
    
    * PMM-6982 Add initialism
    
    * PMM-6982 Fix typo
    
    * Add summary to Rule (#631)
    
    * PMM-5784 Updated.
    
    * PMM-6982 Add API for togging IA (#636)
    
    * PMM-7108 Fix reform version (#638)
    
    * PMM-6827 DB cluster monitoring. (#625)
    
    * PMM-6826 Add disk size to create API.
    
    * PMM-6826 Use bytes instead of megabytes.
    
    * PMM-6826 Make disk size required field.
    
    * PMM-6827 Add public address to Settings API
    
    * PMM-6827 Add ability to remove public address.
    
    * PMM-6827 rename public address to pmm public address.
    
    * PMM-6827 rename public address to pmm public address.
    
    * PMM-6827 Fix merge conflicts.
    
    * PMM-6827 Fix merge conflicts.
    
    Co-authored-by: Alexey Palazhchenko <alexey.palazhchenko@percona.com>
    
    * PMM-6983 Add force for unregister k8s (#633)
    
    * PMM-6959 Update API descriptors. (#632)
    
    * PMM-6983 Add bool fields into unregister k8s.
    
    * PMM-6983 Revert api descriptors.
    
    * PMM-6983 Fix.
    
    * PMM-6983 Add comment to force option.
    
    * PMM-6983 Change comment.
    
    Co-authored-by: Nurlan Moldomurov <bupychuk1989@gmail.com>
    
    * PMM-5784 Removed pt-mysql-summary references.
    
    * PMM-5784 Renamed address to host, and PgSQL to Pg.
    
    * PMM-5784 Added missing type.
    
    Co-authored-by: Jaroslav Musil <jaroslav.musil.sde@gmail.com>
    Co-authored-by: Alexey Palazhchenko <alexey.palazhchenko@percona.com>
    Co-authored-by: Nurlan Moldomurov <bupychuk1989@gmail.com>
    Co-authored-by: JiriCtvrtka <62988319+JiriCtvrtka@users.noreply.github.com>
    Co-authored-by: Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>

--- api/managementpb/json/client/actions/actions_client.go
+++ api/managementpb/json/client/actions/actions_client.go
@@ -45,6 +45,8 @@ type ClientService interface {
 
 	StartPTMongoDBSummaryAction(params *StartPTMongoDBSummaryActionParams) (*StartPTMongoDBSummaryActionOK, error)
 
+	StartPTPgSummaryAction(params *StartPTPgSummaryActionParams) (*StartPTPgSummaryActionOK, error)
+
 	StartPTSummaryAction(params *StartPTSummaryActionParams) (*StartPTSummaryActionOK, error)
 
 	StartPostgreSQLShowCreateTableAction(params *StartPostgreSQLShowCreateTableActionParams) (*StartPostgreSQLShowCreateTableActionOK, error)
@@ -381,6 +383,39 @@ func (a *Client) StartPTMongoDBSummaryAction(params *StartPTMongoDBSummaryAction
 	}
 	// unexpected success response
 	unexpectedSuccess := result.(*StartPTMongoDBSummaryActionDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+  StartPTPgSummaryAction starts PT pg summary action starts pt pg summary action
+*/
+func (a *Client) StartPTPgSummaryAction(params *StartPTPgSummaryActionParams) (*StartPTPgSummaryActionOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewStartPTPgSummaryActionParams()
+	}
+
+	result, err := a.transport.Submit(&runtime.ClientOperation{
+		ID:                 "StartPTPgSummaryAction",
+		Method:             "POST",
+		PathPattern:        "/v1/management/Actions/StartPTPgSummary",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http", "https"},
+		Params:             params,
+		Reader:             &StartPTPgSummaryActionReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*StartPTPgSummaryActionOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*StartPTPgSummaryActionDefault)
 	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
