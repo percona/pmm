150711e81f833e5f9d9726806abf5c26bf21c661
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-03-18 13:15:08 +0100

    PMM-7676 Fix.

--- api/inventorypb/json/client/agents/add_azure_database_exporter_responses.go
+++ api/inventorypb/json/client/agents/add_azure_database_exporter_responses.go
@@ -143,7 +143,7 @@ type AddAzureDatabaseExporterBody struct {
 	AzureDatabaseSubscriptionID string `json:"azure_database_subscription_id,omitempty"`
 
 	// Azure resource type (mysql, maria, postgres)
-	AzureResourceType string `json:"azure_resource_type,omitempty"`
+	AzureDatabaseResourceType string `json:"azure_database_resource_type,omitempty"`
 
 	// Custom user-assigned labels.
 	CustomLabels map[string]string `json:"custom_labels,omitempty"`
