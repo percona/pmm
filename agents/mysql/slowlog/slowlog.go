1241080361e3f2276f7ca16995abf0b003aba088
Palash Nigam <npalash25@gmail.com>
2021-05-19 13:17:08 +0530

    PMM-7104 dont rotate binlogs (#263)

--- agents/mysql/slowlog/slowlog.go
+++ agents/mysql/slowlog/slowlog.go
@@ -268,7 +268,7 @@ func (s *SlowLog) rotateSlowLog(ctx context.Context, slowLogPath string) error {
 		return errors.Wrap(err, "cannot rename old slowlog file")
 	}
 
-	_, err = db.ExecContext(ctx, fmt.Sprintf("FLUSH LOGS /* %s */", queryTag)) //nolint:gosec
+	_, err = db.ExecContext(ctx, "FLUSH NO_WRITE_TO_BINLOG SLOW LOGS") //nolint:gosec
 	if err != nil {
 		return errors.Wrap(err, "cannot flush logs")
 	}
