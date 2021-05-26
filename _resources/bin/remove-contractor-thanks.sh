8156d43ce33096021662c27eff071ae51252db73
PaulJacobs-percona <63049452+PaulJacobs-percona@users.noreply.github.com>
2021-05-26 12:39:09 +0300

    RM-924 RN 2.18.0 (#500)
    * RM-924 Release Notes 2.18.0
    
    * RM-924 Release Notes 2.18.0
    
    * RM-924 Release Notes 2.18.0 Highlights
    
    * RM-924 Release Notes 2.18.0 Remove contractor thanks
    
    * RM-924 Release Notes 2.18.0
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Roma Novikov <roma.novikov@percona.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Roma Novikov <roma.novikov@percona.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Roma Novikov <roma.novikov@percona.com>
    
    * Update docs/release-notes/2.18.0.md
    
    Co-authored-by: Roma Novikov <roma.novikov@percona.com>
    
    Co-authored-by: Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
    Co-authored-by: Roma Novikov <roma.novikov@percona.com>

---
+++ _resources/bin/remove-contractor-thanks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Percona contractors who create tickets don't need to be thanked in the same
+# way as community contributors are. Jirabot can't easily find out whether a
+# contributor is a Percona contractor or not. This script deletes them by name.
+# USAGE: _resources/bin/remove-contractor-thanks.sh docs/release-notes/2.X.0.md
+
+PATTERN="(Thanks to %s for reporting this issue)"
+NAMES=(
+    "Beata Handzelova"
+    "Vojtech Koval"
+    "David Mikus"
+    )
+
+FILE=$1
+
+for n in "${NAMES[@]}"; do
+    PATT=$(printf "s/${PATTERN}//\n" "${n}")
+    echo "Removing thanks for $n"
+    sed -i '' -e "${PATT}" $FILE
+done
