1d7e0f8cd16b40377e4bd5abc4e36a7ef6dacd7f
Dávid Mikuš <dasiodev@gmail.com>
2021-07-28 16:03:53 +0200

    PMM-5016 Enforce relationships between agents and services/nodes (#800)
    * PMM-5016 enforce relationships between agents and services/nodes
    
    * PMM-5016 Refactor from switch to map
    
    * PMM-5016 uncessary newline
    
    * PMM-5016 unknown agents/nodes returns false
    
    * PMM-5016 Add proxy sql exporter
    
    * PMM-5016 azure and rds
    
    Co-authored-by: Maksym <4347218+oter@users.noreply.github.com>

--- models/agent_helpers.go
+++ models/agent_helpers.go
@@ -657,6 +657,84 @@ type CreateAgentParams struct {
 	DisableCollectors              []string
 }
 
+func compatibleNodeAndAgent(nodeType NodeType, agentType AgentType) bool {
+	const allowAll = "allow_all"
+	allow := map[NodeType]AgentType{
+		GenericNodeType:             allowAll,
+		ContainerNodeType:           allowAll,
+		RemoteNodeType:              ExternalExporterType,
+		RemoteRDSNodeType:           RDSExporterType,
+		RemoteAzureDatabaseNodeType: AzureDatabaseExporterType,
+	}
+
+	allowed, ok := allow[nodeType]
+	if !ok {
+		return false
+	}
+
+	if allowed == allowAll {
+		return true
+	}
+
+	return allowed == agentType
+}
+
+func compatibleServiceAndAgent(serviceType ServiceType, agentType AgentType) bool {
+	allow := map[AgentType][]ServiceType{
+		MySQLdExporterType: {
+			MySQLServiceType,
+		},
+		QANMySQLSlowlogAgentType: {
+			MySQLServiceType,
+		},
+		QANMySQLPerfSchemaAgentType: {
+			MySQLServiceType,
+		},
+		MongoDBExporterType: {
+			MongoDBServiceType,
+		},
+		QANMongoDBProfilerAgentType: {
+			MongoDBServiceType,
+		},
+		PostgresExporterType: {
+			PostgreSQLServiceType,
+		},
+		ProxySQLExporterType: {
+			ProxySQLServiceType,
+		},
+		AzureDatabaseExporterType: {
+			PostgreSQLServiceType,
+			MySQLServiceType,
+		},
+		RDSExporterType: {
+			PostgreSQLServiceType,
+			MySQLServiceType,
+		},
+		QANPostgreSQLPgStatMonitorAgentType: {
+			PostgreSQLServiceType,
+		},
+		QANPostgreSQLPgStatementsAgentType: {
+			PostgreSQLServiceType,
+		},
+		ExternalExporterType: {
+			ExternalServiceType,
+		},
+	}
+
+	allowed, ok := allow[agentType]
+	if !ok {
+		return false
+	}
+
+	for _, svcType := range allowed {
+		if svcType == serviceType {
+			return true
+		}
+	}
+
+	return false
+}
+
 // CreateAgent creates Agent with given type.
 func CreateAgent(q *reform.Querier, agentType AgentType, params *CreateAgentParams) (*Agent, error) {
 	id := "/agent_id/" + uuid.New().String()
@@ -678,13 +756,24 @@ func CreateAgent(q *reform.Querier, agentType AgentType, params *CreateAgentPara
 	}
 
 	if params.NodeID != "" {
-		if _, err := FindNodeByID(q, params.NodeID); err != nil {
+		node, err := FindNodeByID(q, params.NodeID)
+		if err != nil {
 			return nil, err
 		}
+
+		if !compatibleNodeAndAgent(node.NodeType, agentType) {
+			return nil, status.Errorf(codes.FailedPrecondition, "invalid combination of node type %s and agent type %s", node.NodeType, agentType)
+		}
 	}
+
 	if params.ServiceID != "" {
-		if _, err := FindServiceByID(q, params.ServiceID); err != nil {
+		svc, err := FindServiceByID(q, params.ServiceID)
+		if err != nil {
 			return nil, err
+		}
+
+		if !compatibleServiceAndAgent(svc.ServiceType, agentType) {
+			return nil, status.Errorf(codes.FailedPrecondition, "invalid combination of service type %s and agent type %s", svc.ServiceType, agentType)
 		}
 	}
 
