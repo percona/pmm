b8eb4d5f38056dcb8c9df2b47900822bfb65655b
Carlos Salguero <carlos.salguero@percona.com>
2021-12-02 15:50:56 -0300

    PMM-8770 Added safe collectors params (#799)
    * PMM-8770 Added safe collectors params
    
    * PMM-8770 Added collstats fields for inventory
    
    * PMM-8770 Added collstats fields for inventory
    
    * PMM-8770 Updated Add mongo messages
    
    Updated add mongo messages with new fields to set the databases names
    and collection names to scrape metrics.
    Also set limits for databases and collections
    
    * PMM-8770 Updated definitions
    
    * PMM-8770 Converted param to array
    
    * Update api/managementpb/mongodb.proto
    
    Co-authored-by: Artem Gavrilov <artem.gavrilov@percona.com>
    
    * Fixed index numbers
    
    * Fixed index numbers
    
    Co-authored-by: Denys Kondratenko <denisok@users.noreply.github.com>
    Co-authored-by: Artem Gavrilov <artem.gavrilov@percona.com>

--- api/inventorypb/json/client/agents/add_mongo_db_exporter_responses.go
+++ api/inventorypb/json/client/agents/add_mongo_db_exporter_responses.go
@@ -173,6 +173,13 @@ type AddMongoDBExporterBody struct {
 
 	// Custom password for exporter endpoint /metrics.
 	AgentPassword string `json:"agent_password,omitempty"`
+
+	// List of colletions to get stats from. Can use *
+	StatsCollections []string `json:"stats_collections"`
+
+	// Collections limit. Only get Databases and collection stats if the total number of collections in the server
+	// is less than this value. 0: no limit
+	CollectionsLimit int32 `json:"collections_limit,omitempty"`
 }
 
 // Validate validates this add mongo DB exporter body
