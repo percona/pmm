22188887bf75e7f5562fd5ceb734305a358196b3
Carlos Salguero <carlos.salguero@percona.com>
2022-04-12 15:50:44 -0300

    Removed PXC message requirement

--- api/managementpb/dbaas/pxc_clusters.validator.pb.go
+++ api/managementpb/dbaas/pxc_clusters.validator.pb.go
@@ -10,7 +10,6 @@ import (
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	math "math"
-	regexp "regexp"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -80,18 +79,9 @@ func (this *GetPXCClusterCredentialsResponse) Validate() error {
 	}
 	return nil
 }
-
-var _regex_CreatePXCClusterRequest_Name = regexp.MustCompile(`^[a-z]([-a-z0-9]*[a-z0-9])?$`)
-
 func (this *CreatePXCClusterRequest) Validate() error {
 	if this.KubernetesClusterName == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("KubernetesClusterName", fmt.Errorf(`value '%v' must not be an empty string`, this.KubernetesClusterName))
-	}
-	if !_regex_CreatePXCClusterRequest_Name.MatchString(this.Name) {
-		return github_com_mwitkow_go_proto_validators.FieldError("Name", fmt.Errorf(`value '%v' must be a string conforming to regex "^[a-z]([-a-z0-9]*[a-z0-9])?$"`, this.Name))
-	}
-	if nil == this.Params {
-		return github_com_mwitkow_go_proto_validators.FieldError("Params", fmt.Errorf("message must exist"))
 	}
 	if this.Params != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Params); err != nil {
