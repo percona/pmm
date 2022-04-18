f0ec0724b1f0517b8fbcf58872f1984bd3c318eb
Carlos Salguero <carlos.salguero@percona.com>
2022-04-18 08:41:08 -0300

    PMM-9704 Removed validation for PSMDB creation

--- api/managementpb/dbaas/psmdb_clusters.validator.pb.go
+++ api/managementpb/dbaas/psmdb_clusters.validator.pb.go
@@ -10,7 +10,6 @@ import (
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	math "math"
-	regexp "regexp"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -19,12 +18,6 @@ var _ = fmt.Errorf
 var _ = math.Inf
 
 func (this *PSMDBClusterParams) Validate() error {
-	if !(this.ClusterSize > 0) {
-		return github_com_mwitkow_go_proto_validators.FieldError("ClusterSize", fmt.Errorf(`value '%v' must be greater than '0'`, this.ClusterSize))
-	}
-	if nil == this.Replicaset {
-		return github_com_mwitkow_go_proto_validators.FieldError("Replicaset", fmt.Errorf("message must exist"))
-	}
 	if this.Replicaset != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Replicaset); err != nil {
 			return github_com_mwitkow_go_proto_validators.FieldError("Replicaset", err)
@@ -37,9 +30,6 @@ func (this *PSMDBClusterParams_ReplicaSet) Validate() error {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.ComputeResources); err != nil {
 			return github_com_mwitkow_go_proto_validators.FieldError("ComputeResources", err)
 		}
-	}
-	if !(this.DiskSize > 0) {
-		return github_com_mwitkow_go_proto_validators.FieldError("DiskSize", fmt.Errorf(`value '%v' must be greater than '0'`, this.DiskSize))
 	}
 	return nil
 }
@@ -63,18 +53,9 @@ func (this *GetPSMDBClusterCredentialsResponse) Validate() error {
 func (this *GetPSMDBClusterCredentialsResponse_PSMDBCredentials) Validate() error {
 	return nil
 }
-
-var _regex_CreatePSMDBClusterRequest_Name = regexp.MustCompile(`^[a-z]([-a-z0-9]*[a-z0-9])?$`)
-
 func (this *CreatePSMDBClusterRequest) Validate() error {
 	if this.KubernetesClusterName == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("KubernetesClusterName", fmt.Errorf(`value '%v' must not be an empty string`, this.KubernetesClusterName))
-	}
-	if !_regex_CreatePSMDBClusterRequest_Name.MatchString(this.Name) {
-		return github_com_mwitkow_go_proto_validators.FieldError("Name", fmt.Errorf(`value '%v' must be a string conforming to regex "^[a-z]([-a-z0-9]*[a-z0-9])?$"`, this.Name))
-	}
-	if nil == this.Params {
-		return github_com_mwitkow_go_proto_validators.FieldError("Params", fmt.Errorf("message must exist"))
 	}
 	if this.Params != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.Params); err != nil {
