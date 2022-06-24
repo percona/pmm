b086c1e3f9e598b94855767c7422f9a60952d0f7
Dmytro Liakhov <dim@sikk.me>
2022-06-24 15:16:54 +0300

    PMM-5005: Show up connected time in status response

--- admin/commands/status.go
+++ admin/commands/status.go
@@ -41,6 +41,7 @@ PMM Client:
 	Connected        : {{ .PMMAgentStatus.Connected }}{{ if .PMMAgentStatus.Connected }}
 	Time drift       : {{ .PMMAgentStatus.ServerClockDrift }}
 	Latency          : {{ .PMMAgentStatus.ServerLatency }}{{ end }}
+	Up connected time: {{ .PMMAgentStatus.UpConnectedTime }}
 	pmm-admin version: {{ .PMMVersion }}
 	pmm-agent version: {{ .PMMAgentStatus.AgentVersion }}
 Agents:
