2c1dd61a9c7534fee58e5fb44b7f1127b5ede51a
Michal Kralik <michal.kralik@percona.com>
2022-06-24 17:42:05 +0200

    PMM-7462 Use separate function for conditions

--- managed/services/server/server.go
+++ managed/services/server/server.go
@@ -550,30 +550,19 @@ func (s *Server) validateChangeSettingsRequest(ctx context.Context, req *serverp
 		return status.Error(codes.FailedPrecondition, "DBaaS is enabled via ENABLE_DBAAS or via deprecated PERCONA_TEST_DBAAS environment variable.")
 	}
 
-	if metricsRes.GetHr().AsDuration() != 0 &&
-		s.envSettings.MetricsResolutions.HR != 0 &&
-		metricsRes.GetHr().AsDuration() != s.envSettings.MetricsResolutions.HR {
-
+	if !canUpdateDurationSetting(metricsRes.GetHr().AsDuration(), s.envSettings.MetricsResolutions.HR) {
 		return status.Error(codes.FailedPrecondition, "High resolution for metrics is set via METRICS_RESOLUTION_HR (or METRICS_RESOLUTION) environment variable.")
 	}
 
-	if metricsRes.GetMr().AsDuration() != 0 &&
-		s.envSettings.MetricsResolutions.MR != 0 &&
-		metricsRes.GetMr().AsDuration() != s.envSettings.MetricsResolutions.MR {
-
+	if !canUpdateDurationSetting(metricsRes.GetMr().AsDuration(), s.envSettings.MetricsResolutions.MR) {
 		return status.Error(codes.FailedPrecondition, "Medium resolution for metrics is set via METRICS_RESOLUTION_MR environment variable.")
 	}
-	if metricsRes.GetLr().AsDuration() != 0 &&
-		s.envSettings.MetricsResolutions.LR != 0 &&
-		metricsRes.GetLr().AsDuration() != s.envSettings.MetricsResolutions.LR {
 
+	if !canUpdateDurationSetting(metricsRes.GetLr().AsDuration(), s.envSettings.MetricsResolutions.LR) {
 		return status.Error(codes.FailedPrecondition, "Low resolution for metrics is set via METRICS_RESOLUTION_LR environment variable.")
 	}
 
-	if req.DataRetention.AsDuration() != 0 &&
-		s.envSettings.DataRetention != 0 &&
-		req.DataRetention.AsDuration() != s.envSettings.DataRetention {
-
+	if !canUpdateDurationSetting(req.DataRetention.AsDuration(), s.envSettings.DataRetention) {
 		return status.Error(codes.FailedPrecondition, "Data retention for queries is set via DATA_RETENTION environment variable.")
 	}
 
@@ -908,7 +897,15 @@ func isAgentsStateUpdateNeeded(mr *serverpb.MetricsResolutions) bool {
 	return true
 }
 
-// check interfaces
+// check interfaces.
 var (
 	_ serverpb.ServerServer = (*Server)(nil)
 )
+
+func canUpdateDurationSetting(setting, envSetting time.Duration) bool {
+	if setting != 0 && envSetting != 0 && setting != envSetting {
+		return false
+	}
+
+	return true
+}
