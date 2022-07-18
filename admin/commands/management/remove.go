3cc21f5a7e3aadea2c08515a45d73c259c911f8b
Andrew Minkin <andrew.minkin@percona.com>
2022-07-18 19:21:04 +0600

    PMM-10095 Make linter happy

--- admin/commands/management/remove.go
+++ admin/commands/management/remove.go
@@ -82,7 +82,8 @@ func (cmd *removeMySQLCommand) Run() (commands.Result, error) {
 		case len(servicesRes.Payload.External) == 1:
 			cmd.ServiceID = servicesRes.Payload.External[0].ServiceID
 		}
-		if cmd.ServiceID == "" { //nolint:golint
+		if cmd.ServiceID == "" {
+			//nolint:revive,golint
 			return nil, errors.New(`We could not find a service associated with the local node. Please provide "Service ID" or "Service name".`)
 		}
 	}
