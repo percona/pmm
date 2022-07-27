de57fb811571a15ca3b53cab42ac9657d538a5e9
Artem Gavrilov <artem.gavrilov@percona.com>
2022-07-27 19:33:37 +0200

    PMM-7 Update tests

--- agent/actions/mysql_show_table_status_action_test.go
+++ agent/actions/mysql_show_table_status_action_test.go
@@ -68,7 +68,7 @@ func TestShowTableStatus(t *testing.T) {
 				nil, "", "",
 			}, actual[1])
 
-		case "5.7":
+		case "5.7", "8.0":
 			assert.Equal(t, []interface{}{
 				"Name", "Engine", "Version", "Row_format", "Rows", "Avg_row_length", "Data_length", "Max_data_length",
 				"Index_length", "Data_free", "Auto_increment", "Create_time", "Update_time", "Check_time", "Collation",
@@ -77,19 +77,6 @@ func TestShowTableStatus(t *testing.T) {
 			actual[1][11] = createTime
 			assert.Equal(t, []interface{}{
 				"city", "InnoDB", 10.0, "Dynamic", 4117.0, 95.0, 393216.0, 0.0,
-				81920.0, 0.0, 4080.0, "2019-06-10 12:04:29", nil, nil, "latin1_swedish_ci",
-				nil, "", "",
-			}, actual[1])
-
-		case "8.0":
-			assert.Equal(t, []interface{}{
-				"Name", "Engine", "Version", "Row_format", "Rows", "Avg_row_length", "Data_length", "Max_data_length",
-				"Index_length", "Data_free", "Auto_increment", "Create_time", "Update_time", "Check_time", "Collation",
-				"Checksum", "Create_options", "Comment",
-			}, actual[0])
-			actual[1][11] = createTime
-			assert.Equal(t, []interface{}{
-				"city", "InnoDB", 10.0, "Dynamic", 4046.0, 97.0, 393216.0, 0.0,
 				81920.0, 0.0, 4080.0, "2019-06-10 12:04:29", nil, nil, "latin1_swedish_ci",
 				nil, "", "",
 			}, actual[1])
