cc74e1f50f9e6c4f20b5ef3e5bb44424279b165a
Nikita Beletskii <nikita.beletskii@percona.com>
2022-07-28 15:39:57 +0000

    PMM-10036 migration to monorepo (final)

---
+++ build/scripts/create-tags.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python2
+
+from __future__ import print_function, unicode_literals
+import os, subprocess, time
+
+REPOS = [
+    "sources/grafana/src/github.com/grafana/grafana",
+    "sources/grafana-dashboards",
+    "sources/pmm-server",
+    "sources/pmm-update/src/github.com/percona/pmm-update",
+    "sources/pmm/src/github.com/percona/pmm",
+    "sources/qan-api2/src/github.com/percona/qan-api2",
+    ".",
+]
+
+tty = subprocess.check_output("tty", shell=True).strip()
+env = os.environ.copy()
+env["GPG_TTY"] = tty
+
+with open("./VERSION", "r") as f:
+    version = f.read().strip()
+
+print(tty, version)
+
+subprocess.check_call("git submodule update", shell=True)
+
+for repo in REPOS:
+    print("==>", repo)
+
+    tag = "v" + version
+    cmd = "git tag --message='Version {version}.' --sign {tag}".format(version=version, tag=tag)
+    print(">", cmd)
+    subprocess.check_call(cmd, shell=True, cwd=repo, env=env)
+
+    cmd = "git push origin {tag}".format(tag=tag)
+    print(">", cmd)
+    subprocess.check_call(cmd, shell=True, cwd=repo)
+
+subprocess.check_call("git submodule status", shell=True)
