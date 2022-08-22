ea3b7236ab2e9977d2e5fbc1727a0f2acfcdc449
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-08-22 16:48:23 +0300

    PMM-9503 remove prometheus config (#1130)
    * PMM-9503 remove prometheus config
    
    * PMM-9503 fix bogus dates
    
    * PMM-9503 fix a few typos
    
    * PMM-9503 remove duplicate configs

--- managed/services/supervisord/supervisord.go
+++ managed/services/supervisord/supervisord.go
@@ -593,22 +593,6 @@ stdout_logfile_backups = 3
 redirect_stderr = true
 {{end}}
 
-{{define "prometheus"}}
-[program:prometheus]
-command = /bin/echo Prometheus is substituted by VictoriaMetrics
-user = pmm
-autorestart = false
-autostart = false
-startretries = 10
-startsecs = 1
-stopsignal = TERM
-stopwaitsecs = 300
-stdout_logfile = /srv/logs/prometheus.log
-stdout_logfile_maxbytes = 10MB
-stdout_logfile_backups = 3
-redirect_stderr = true
-{{end}}
-
 {{define "victoriametrics"}}
 [program:victoriametrics]
 priority = 7
