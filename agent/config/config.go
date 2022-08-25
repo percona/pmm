b1a341a6aa4dc0ca873f77c72eaf15703e094daa
Michal Kralik <michal.kralik@percona.com>
2022-08-25 17:48:32 +0200

    Review dog

--- agent/config/config.go
+++ agent/config/config.go
@@ -242,14 +242,16 @@ func setDefaults(cfg *Config, l *logrus.Entry) {
 }
 
 func configureNetworkDefaults(cfg *Config) {
-	if cfg.ListenSocket == "" && cfg.ListenAddress == "" && cfg.ListenPort == 0 {
-		cfg.ListenSocket = "/usr/local/percona/pmm2/pmm-agent.sock"
-	}
-	if cfg.ListenSocket == "" && cfg.ListenPort != 0 && cfg.ListenAddress == "" {
-		cfg.ListenAddress = "127.0.0.1"
-	}
-	if cfg.ListenSocket == "" && cfg.ListenPort == 0 && cfg.ListenAddress != "" {
-		cfg.ListenPort = 7777
+	if cfg.ListenSocket == "" {
+		if cfg.ListenAddress == "" && cfg.ListenPort == 0 {
+			cfg.ListenSocket = "/usr/local/percona/pmm2/pmm-agent.sock"
+		}
+		if cfg.ListenAddress == "" && cfg.ListenPort != 0 {
+			cfg.ListenAddress = "127.0.0.1"
+		}
+		if cfg.ListenAddress != "" && cfg.ListenPort == 0 {
+			cfg.ListenPort = 7777
+		}
 	}
 	if cfg.Ports.Min == 0 {
 		cfg.Ports.Min = 42000 // for minimal compatibility with PMM Client 1.x firewall rules and documentation
