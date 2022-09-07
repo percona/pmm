81cfc42a497017956835e590af02e35c5b3df8e1
Artem Gavrilov <artem.gavrilov@percona.com>
2022-09-07 18:15:13 +0200

    PMM-10452 Disable IA alert rules

--- managed/models/database.go
+++ managed/models/database.go
@@ -730,6 +730,7 @@ var databaseSchema = [][]string{
 	66: {
 		`UPDATE settings SET settings = settings #- '{ia, enabled}';`,
 		`UPDATE settings SET settings = settings - 'ia' || jsonb_build_object('alerting', settings->'ia');`,
+		`UPDATE ia_rules SET disabled = TRUE`,
 	},
 }
 
