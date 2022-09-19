a827e19abd3c802160ac0dab5c82d32ed8d6712e
Artem Gavrilov <artem.gavrilov@percona.com>
2022-09-20 01:37:48 +0200

    Intermediate progress

---
+++ migration.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+
+import requests
+
+
+def prepare_labels(rule):
+    if "labels" not in rule:
+        return
+
+    labels = rule["labels"]
+    labels.update({
+        "percona_alerting": "1",
+        "severity": rule["severity"].lstrip("SEVERITY_").lower(),
+        "template_name": rule["template_name"]
+    })
+    return labels
+
+
+def prepare_annotations(rule):
+    annotations = rule["annotations"]
+    annotations.update({"rule": rule["name"]})
+
+    return annotations
+
+
+def convert_rule(rule):
+    return {
+        "grafana_alert": {
+            "title": rule["name"],
+            "condition": "A",
+            "noDataState": "OK",
+            "execErrState": "Alerting",
+            "data": [
+                {
+                    "refId": "A",
+                    "datasourceUid": datasourceUID,
+                    "relativeTimeRange": {"from": 600, "to": 0},
+                    "model": {
+                        "expr": rule["expr"],
+                        "refId": "A",
+                        "instant": True,
+                    },
+                },
+            ]
+        },
+        "for": rule["for"],
+        "annotations": prepare_annotations(rule),
+        "labels": prepare_labels(rule),
+    }
+
+
+parser = argparse.ArgumentParser(description="Migration script for Integrated Alerting alert rules")
+parser.add_argument("-u", "--user", required=True, help="PMM user login")
+parser.add_argument("-p", "--password", required=True, help="PMM user password")
+parser.add_argument("-s", "--server-url", default="http://localhost/", help="PMM server URL (default: %(default)s)")
+parser.add_argument("-i", "--insecure", action="store_false", help="skip TLS certificates verification")
+parser.add_argument("-f", "--folder", default="Experimental",
+                    help="folder in Grafana where to put migrated alert rules (default: %(default)s)")
+parser.add_argument("-g", "--group", default="migrated",
+                    help="alert group name for migrated alert rules (default: %(default)s)")
+
+config = vars(parser.parse_args())
+
+auth = (config["user"], config["password"])
+
+# Create alert group for migrated rules
+groupURL = '{server_url}/graph/api/ruler/grafana/api/v1/rules/{folder}/{group}'.format(**config)
+groupReq = requests.get(groupURL, auth=auth, verify=config["insecure"])
+alertRulesGroup = json.loads(groupReq.text)
+print(alertRulesGroup)
+if "interval" not in alertRulesGroup:
+    alertRulesGroup["interval"] = "1m"
+
+# print(alertRulesGroup)
+# exit(1)
+
+
+# Get Metrics datasource UID
+datasourceURL = '{server_url}/graph/api/datasources/1'.format(**config)
+datasourceReq = requests.get(datasourceURL, auth=auth, verify=config["insecure"])
+datasource = json.loads(datasourceReq.text)
+datasourceUID = datasource["uid"]
+
+# Get existing Integrated Alerting alert rules
+iaRulesURL = '{server_url}/v1/management/ia/Rules/List'.format(**config)
+iaRulesReq = requests.post(iaRulesURL, auth=auth, verify=config["insecure"])
+iaRules = json.loads(iaRulesReq.text)
+if "rules" not in iaRules:
+    print("There are no rules to migrate")
+    exit(1)
+
+# Convert IA rules and add them to alert group
+for rule in iaRules["rules"]:
+    alertRulesGroup["rules"].append(convert_rule(rule))
+
+print(alertRulesGroup)
+# Update alert group
+rulesURL = '{server_url}/graph/api/ruler/grafana/api/v1/rules/{folder}'.format(**config)
+resp = requests.post(rulesURL, None, alertRulesGroup, auth=auth, verify=config["insecure"])
+print(resp.status_code)
