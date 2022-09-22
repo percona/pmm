63f65c64657ba72cd235b4f3641fa445160e0d26
Carlos Salguero <carlos.salguero@percona.com>
2022-09-22 12:31:58 -0300

    PMM-10640 Fixed tests and added a new one.

--- managed/services/management/dbaas/components_service_test.go
+++ managed/services/management/dbaas/components_service_test.go
@@ -369,7 +369,7 @@ func TestComponentServiceMatrix(t *testing.T) {
 
 	t.Run("All", func(t *testing.T) {
 		cs := &ComponentsService{}
-		m := cs.matrix(input, nil, nil)
+		m := cs.matrix(input, nil, nil, nil)
 
 		expected := map[string]*dbaasv1beta1.Component{
 			"5.7.26-31.37":   {ImagePath: "percona/percona-xtradb-cluster:5.7.26-31.37", ImageHash: "9d43d8e435e4aca5c694f726cc736667cb938158635c5f01a0e9412905f1327f", Status: "available", Critical: false},
@@ -395,10 +395,40 @@ func TestComponentServiceMatrix(t *testing.T) {
 		m := cs.matrix(input, nil, &models.Component{
 			DisabledVersions: []string{"8.0.20-11.2", "8.0.20-11.1"},
 			DefaultVersion:   "8.0.19-10.1",
-		})
+		}, nil)
 
 		expected := map[string]*dbaasv1beta1.Component{
 			"5.7.26-31.37":   {ImagePath: "percona/percona-xtradb-cluster:5.7.26-31.37", ImageHash: "9d43d8e435e4aca5c694f726cc736667cb938158635c5f01a0e9412905f1327f", Status: "available", Critical: false},
+			"5.7.27-31.39":   {ImagePath: "percona/percona-xtradb-cluster:5.7.27-31.39", ImageHash: "7d8eb4d2031c32c6e96451655f359d8e5e8e047dc95bada9a28c41c158876c26", Status: "available", Critical: false},
+			"5.7.28-31.41.2": {ImagePath: "percona/percona-xtradb-cluster:5.7.28-31.41.2", ImageHash: "fccd6525aaeedb5e436e9534e2a63aebcf743c043526dd05dba8519ebddc8b30", Status: "available", Critical: true},
+			"5.7.29-31.43":   {ImagePath: "percona/percona-xtradb-cluster:5.7.29-31.43", ImageHash: "85fb479de073770280ae601cf3ec22dc5c8cca4c8b0dc893b09503767338e6f9", Status: "available", Critical: false},
+			"5.7.30-31.43":   {ImagePath: "percona/percona-xtradb-cluster:5.7.30-31.43", ImageHash: "b03a060e9261b37288a2153c78f86dcfc53367c36e1bcdcae046dd2d0b0721af", Status: "available", Critical: false},
+			"5.7.31-31.45":   {ImagePath: "percona/percona-xtradb-cluster:5.7.31-31.45", ImageHash: "3852cef43cc0c6aa791463ba6279e59dcdac3a4fb1a5616c745c1b3c68041dc2", Status: "available", Critical: false},
+			"5.7.31-31.45.2": {ImagePath: "percona/percona-xtradb-cluster:5.7.31-31.45.2", ImageHash: "0decf85c7c7afacc438f5fe355dc8320ea7ffc7018ca2cb6bda3ac0c526ae172", Status: "available", Critical: false},
+			"5.7.32-31.47":   {ImagePath: "percona/percona-xtradb-cluster:5.7.32-31.47", ImageHash: "7b095019ad354c336494248d6080685022e2ed46e3b53fc103b25cd12c95952b", Status: "recommended", Critical: false},
+			"8.0.19-10.1":    {ImagePath: "percona/percona-xtradb-cluster:8.0.19-10.1", ImageHash: "1058ae8eded735ebdf664807aad7187942fc9a1170b3fd0369574cb61206b63a", Status: "available", Critical: false, Default: true},
+			"8.0.20-11.1":    {ImagePath: "percona/percona-xtradb-cluster:8.0.20-11.1", ImageHash: "54b1b2f5153b78b05d651034d4603a13e685cbb9b45bfa09a39864fa3f169349", Status: "available", Critical: false, Disabled: true},
+			"8.0.20-11.2":    {ImagePath: "percona/percona-xtradb-cluster:8.0.20-11.2", ImageHash: "feda5612db18da824e971891d6084465aa9cdc9918c18001cd95ba30916da78b", Status: "available", Critical: false, Disabled: true},
+			"8.0.21-12.1":    {ImagePath: "percona/percona-xtradb-cluster:8.0.21-12.1", ImageHash: "d95cf39a58f09759408a00b519fe0d0b19c1b28332ece94349dd5e9cdbda017e", Status: "recommended", Critical: false},
+		}
+
+		assert.Equal(t, expected, m)
+	})
+
+	t.Run("Globally Disabled Versions", func(t *testing.T) {
+		cs := &ComponentsService{}
+
+		gdv := []*goversion.Version{
+			goversion.Must(goversion.NewVersion("5.7.26-31.37")),
+		}
+		m := cs.matrix(input, nil, &models.Component{
+			DisabledVersions: []string{"8.0.20-11.2", "8.0.20-11.1"},
+			DefaultVersion:   "8.0.19-10.1",
+		}, gdv)
+
+		expected := map[string]*dbaasv1beta1.Component{
+			// This must not be in the result because it was globally disabled.
+			// "5.7.26-31.37":   {ImagePath: "percona/percona-xtradb-cluster:5.7.26-31.37", ImageHash: "9d43d8e435e4aca5c694f726cc736667cb938158635c5f01a0e9412905f1327f", Status: "available", Critical: false},
 			"5.7.27-31.39":   {ImagePath: "percona/percona-xtradb-cluster:5.7.27-31.39", ImageHash: "7d8eb4d2031c32c6e96451655f359d8e5e8e047dc95bada9a28c41c158876c26", Status: "available", Critical: false},
 			"5.7.28-31.41.2": {ImagePath: "percona/percona-xtradb-cluster:5.7.28-31.41.2", ImageHash: "fccd6525aaeedb5e436e9534e2a63aebcf743c043526dd05dba8519ebddc8b30", Status: "available", Critical: true},
 			"5.7.29-31.43":   {ImagePath: "percona/percona-xtradb-cluster:5.7.29-31.43", ImageHash: "85fb479de073770280ae601cf3ec22dc5c8cca4c8b0dc893b09503767338e6f9", Status: "available", Critical: false},
@@ -423,7 +453,7 @@ func TestComponentServiceMatrix(t *testing.T) {
 		m := cs.matrix(input, minimumSupportedVersion, &models.Component{
 			DisabledVersions: []string{"8.0.21-12.1", "8.0.20-11.1"},
 			DefaultVersion:   "8.0.20-11.2",
-		})
+		}, nil)
 
 		expected := map[string]*dbaasv1beta1.Component{
 			"8.0.19-10.1": {ImagePath: "percona/percona-xtradb-cluster:8.0.19-10.1", ImageHash: "1058ae8eded735ebdf664807aad7187942fc9a1170b3fd0369574cb61206b63a", Status: "available", Critical: false},
@@ -437,7 +467,7 @@ func TestComponentServiceMatrix(t *testing.T) {
 
 	t.Run("EmptyMatrix", func(t *testing.T) {
 		cs := &ComponentsService{}
-		m := cs.matrix(make(map[string]componentVersion), nil, nil)
+		m := cs.matrix(make(map[string]componentVersion), nil, nil, nil)
 		assert.Equal(t, make(map[string]*dbaasv1beta1.Component), m)
 	})
 }
