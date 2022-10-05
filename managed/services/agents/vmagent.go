3e3423ac0b427b6cb0807157225baa72d5b0c8ab
Anton Bystrov <anton.bystrov@simbirsoft.com>
2022-10-05 15:48:31 +0300

    Remove flag

--- managed/services/agents/vmagent.go
+++ managed/services/agents/vmagent.go
@@ -35,7 +35,6 @@ func vmAgentConfig(scrapeCfg string) *agentpb.SetStateRequest_AgentProcess {
 		"-httpListenAddr=127.0.0.1:{{.listen_port}}",
 		// needed for login/password at client side.
 		"-envflag.enable=true",
-		"-promscrape.noStaleMarkers=true",
 		"-minStalenessInterval=1s",
 	}
 
