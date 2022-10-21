5fd3873d11f80dbce7ad0d9bb57b37a40eaeb525
michael.okoko <michael.okoko@percona.com>
2022-10-21 11:11:58 +0100

    fix tests

--- managed/services/backup/pitr_timerange_service_test.go
+++ managed/services/backup/pitr_timerange_service_test.go
@@ -155,17 +155,17 @@ func TestListPITRTimelines(t *testing.T) {
 		assert.Nil(t, timelines)
 	})
 
-	t.Run("skips artifacts with file stat errors", func(t *testing.T) {
+	t.Run("skips artifacts with deletion markers", func(t *testing.T) {
 		mockedStorage := &mockPitrLocationClient{}
 		listedFiles := []minio.FileInfo{
 			{
-				Name: "rs0/20220829/20220829115611-1.20220829120544-10.oplog.s2",
-				Size: 1024,
+				Name:           "rs0/20220829/20220829115611-1.20220829120544-10.oplog.s2",
+				Size:           1024,
+				IsDeleteMarker: true,
 			},
 		}
 
 		mockedStorage.On("List", ctx, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(listedFiles, nil)
-		mockedStorage.On("FileStat", ctx, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(minio.FileInfo{}, errors.New("file stat error"))
 
 		ss := NewPITRTimerangeService(mockedStorage)
 		timelines, err := ss.getPITROplogs(ctx, location, "")
