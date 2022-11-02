6635b86b36f0e236bb4eb414630d0c1b6e0e456d
Diogo Recharte <diogo.recharte@percona.com>
2022-11-02 09:49:07 +0000

    PMM-10889: Add tab completion for pmm-admin / pmm

--- admin/cmd/bootstrap.go
+++ admin/cmd/bootstrap.go
@@ -24,6 +24,7 @@ import (
 	"strings"
 
 	"github.com/alecthomas/kong"
+	kongcompletion "github.com/jotaen/kong-completion"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 
@@ -41,15 +42,22 @@ import (
 // Bootstrap is used to initialize the application.
 func Bootstrap(opts any) {
 	var kongCtx *kong.Context
+	var kongParser *kong.Kong
 	var parsedOpts any
 
 	switch o := opts.(type) {
 	case cli.PMMAdminCommands:
-		kongCtx = kong.Parse(&o, getDefaultKongOptions("pmm-admin")...)
+		kongParser = kong.Must(&o, getDefaultKongOptions("pmm-admin")...)
 		parsedOpts = &o
 	case cli.PMMCommands:
-		kongCtx = kong.Parse(&o, getDefaultKongOptions("pmm")...)
+		kongParser = kong.Must(&o, getDefaultKongOptions("pmm")...)
 		parsedOpts = &o
+	}
+
+	kongcompletion.Register(kongParser)
+	kongCtx, err := kongParser.Parse(os.Args[1:])
+	if err != nil {
+		logrus.Panic("Failed to parse CLI arguments")
 	}
 
 	f, ok := parsedOpts.(cli.GlobalFlagsGetter)
@@ -62,7 +70,7 @@ func Bootstrap(opts any) {
 	configureLogger(globalFlags)
 	finishBootstrap(globalFlags)
 
-	err := kongCtx.Run(globalFlags)
+	err = kongCtx.Run(globalFlags)
 	processFinalError(err, bool(globalFlags.JSON))
 }
 
