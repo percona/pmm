ffe7ad045940a7a67f010360fca5cb6b321c150f
Alexey Mukas <ritbl@protonmail.com>
2022-11-04 08:45:44 +0100

    add tests and minor improvements

--- managed/services/telemetry/transform.go
+++ managed/services/telemetry/transform.go
@@ -9,26 +9,48 @@ import (
 type itemsType []map[string]any
 
 func transformToJSON(config *Config, metrics []*pmmv1.ServerMetric_Metric) ([]*pmmv1.ServerMetric_Metric, error) {
+	if metrics == nil {
+		return nil, nil
+	}
+
+	if config.Transform == nil {
+		return nil, errors.Errorf("no transformation config is set")
+	}
+
 	if config.Transform.Type != JSONTransformType {
 		return nil, errors.Errorf("not supported transformation type [%s], it must be [%s]", config.Transform.Type, JSONTransformType)
 	}
+
+	if len(config.Data) == 0 || config.Data[0].MetricName == "" {
+		return nil, errors.Errorf("invalid metrics config")
+	}
+
+	firstMetric := config.Data[0].MetricName
 
 	result := map[string]itemsType{}
 
 	var items []map[string]any
 	var next map[string]any
 	for _, metric := range metrics {
-		isFirstMetric := metric.Key == config.Data[0].MetricName
+		isFirstMetric := metric.Key == firstMetric
 		if isFirstMetric {
 			if next != nil {
 				items = append(items, next)
 			}
 			next = map[string]any{}
 		}
+		if next == nil {
+			return nil, errors.Errorf("invalid metrics sequence: no match with first metric")
+		}
 		next[metric.Key] = metric.Value
 	}
 	if next != nil {
 		items = append(items, next)
+	}
+
+	// nothing to process
+	if items == nil {
+		return metrics, nil
 	}
 
 	result["v"] = items
---
+++ managed/services/telemetry/transform_test.go
@@ -0,0 +1,128 @@
+package telemetry
+
+import (
+	"fmt"
+	pmmv1 "github.com/percona-platform/saas/gen/telemetry/events/pmm"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_transformToJSON(t *testing.T) {
+	type args struct {
+		config  *Config
+		metrics []*pmmv1.ServerMetric_Metric
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []*pmmv1.ServerMetric_Metric
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "nil metrics",
+			args: args{
+				config:  config(),
+				metrics: nil,
+			},
+			want:    nil,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "empty metrics",
+			args: args{
+				config:  config(),
+				metrics: []*pmmv1.ServerMetric_Metric{},
+			},
+			want:    []*pmmv1.ServerMetric_Metric{},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "no Transform in config",
+			args: args{
+				config:  config().noTransform(),
+				metrics: []*pmmv1.ServerMetric_Metric{},
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "no Metrics config",
+			args: args{
+				config:  config().noFirstMetricConfig(),
+				metrics: []*pmmv1.ServerMetric_Metric{},
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "no Metric Name config",
+			args: args{
+				config:  config().noFirstMetricNameConfig(),
+				metrics: []*pmmv1.ServerMetric_Metric{},
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "invalid seq",
+			args: args{
+				config: config(),
+				metrics: []*pmmv1.ServerMetric_Metric{
+					{Key: "", Value: "v1"}, // no match with first metric
+				},
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "happy path",
+			args: args{
+				config: config(),
+				metrics: []*pmmv1.ServerMetric_Metric{
+					{Key: config().Data[0].MetricName, Value: "v1"},
+					{Key: config().Data[0].MetricName, Value: "v2"},
+				},
+			},
+			want: []*pmmv1.ServerMetric_Metric{
+				{Key: config().Transform.Metric, Value: "{\"v\":[{\"my-metric\":\"v1\"},{\"my-metric\":\"v2\"}]}"},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := transformToJSON(tt.args.config, tt.args.metrics)
+			if !tt.wantErr(t, err, fmt.Sprintf("transformToJSON(%v, %v)", tt.args.config, tt.args.metrics)) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "transformToJSON(%v, %v)", tt.args.config, tt.args.metrics)
+		})
+	}
+}
+
+func config() *Config {
+	return &Config{
+		Transform: &ConfigTransform{
+			Metric: "metric",
+			Type:   JSONTransformType,
+		},
+		Data: []ConfigData{
+			{MetricName: "my-metric", Label: "label"},
+		},
+	}
+}
+
+func (c *Config) noTransform() *Config {
+	c.Transform = nil
+	return c
+}
+
+func (c *Config) noFirstMetricConfig() *Config {
+	c.Data = nil
+	return c
+}
+
+func (c *Config) noFirstMetricNameConfig() *Config {
+	c.Data[0].MetricName = ""
+	return c
+}
