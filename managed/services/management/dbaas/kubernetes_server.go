f570d78b8a629c3f04583cd26841816fb7bd59ff
Carlos Salguero <carlos.salguero@percona.com>
2022-11-15 09:03:56 -0300

    Removed dead code

--- managed/services/management/dbaas/kubernetes_server.go
+++ managed/services/management/dbaas/kubernetes_server.go
@@ -263,127 +263,6 @@ func replaceAWSAuthIfPresent(kubeconfig string, keyID, key string) (string, erro
 	return string(c), err
 }
 
-// RegisterKubernetesCluster registers an existing Kubernetes cluster in PMM.
-func (k kubernetesServer) RegisterKubernetesClusterOLM(ctx context.Context, req *dbaasv1beta1.RegisterKubernetesClusterRequest) (*dbaasv1beta1.RegisterKubernetesClusterResponse, error) {
-	var err error
-	req.KubeAuth.Kubeconfig, err = replaceAWSAuthIfPresent(req.KubeAuth.Kubeconfig, req.AwsAccessKeyId, req.AwsSecretAccessKey)
-	if err != nil {
-		if errors.Is(err, errKubeconfigIsEmpty) {
-			return nil, status.Error(codes.InvalidArgument, "Kubeconfig can't be empty")
-		} else if errors.Is(err, errMissingRequiredKubeconfigEnvVar) {
-			return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("Failed to transform kubeconfig to work with aws-iam-authenticator: %s", err))
-		}
-		k.l.Errorf("Replacing `aws` with `aim-authenticator` failed: %s", err)
-		return nil, status.Error(codes.Internal, "Internal server error")
-	}
-	var clusterInfo *dbaascontrollerv1beta1.CheckKubernetesClusterConnectionResponse
-	err = k.db.InTransaction(func(t *reform.TX) error {
-		var e error
-		clusterInfo, e = k.dbaasClient.CheckKubernetesClusterConnection(ctx, req.KubeAuth.Kubeconfig)
-		if e != nil {
-			return e
-		}
-
-		_, err := models.CreateKubernetesCluster(t.Querier, &models.CreateKubernetesClusterParams{
-			KubernetesClusterName: req.KubernetesClusterName,
-			KubeConfig:            req.KubeAuth.Kubeconfig,
-		})
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	ch := make(chan struct{})
-	go func() {
-		ctx := context.TODO()
-		defer close(ch)
-
-		if err := installOLMOperator(ctx, k.dbaasClient, req.KubeAuth.Kubeconfig, ""); err != nil {
-			k.l.Errorf("Cannot install the OLM operator: %s", err)
-		}
-	}()
-
-	pmmVersion, err := goversion.NewVersion(pmmversion.PMMVersion)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	pxcOperatorVersion, psmdbOperatorVersion, err := k.versionService.LatestOperatorVersion(ctx, pmmVersion.Core().String())
-	if err != nil {
-		return nil, err
-	}
-
-	if psmdbOperatorVersion != nil && (clusterInfo.Operators == nil || clusterInfo.Operators.PsmdbOperatorVersion == "") {
-		go func() {
-			<-ch // wait until OLM operator is installed.
-
-			ctx := context.TODO()
-			err := installOperator(ctx, k.dbaasClient, req.KubeAuth.Kubeconfig, "psmdb-operator-ns", "percona-server-mongodb-operator",
-				"operatorgroup", "stable", "percona-server-mongodb-operator.v1.10.0")
-			if err != nil {
-				k.l.Errorf("cannot install PSMDB operator: %s", err)
-				return
-			}
-			if err := approveInstallPlan(ctx, k.dbaasClient, req.KubeAuth.Kubeconfig, "percona-server-mongodb-operator"); err != nil {
-				k.l.Errorf("cannot approve the PSMDB operator installation: %s", err)
-			}
-		}()
-	}
-
-	if pxcOperatorVersion != nil && (clusterInfo.Operators == nil || clusterInfo.Operators.PxcOperatorVersion == "") {
-		go func() {
-			<-ch // wait until OLM operator is installed.
-
-			ctx := context.TODO()
-			err := installOperator(ctx, k.dbaasClient, req.KubeAuth.Kubeconfig, "pxc-operator-ns", "percona-xtradb-cluster-operator", "pxc-operator-group", "stable", "")
-			if err != nil {
-				k.l.Errorf("cannot install PSMDB operator: %s", err)
-				return
-			}
-			if err := approveInstallPlan(ctx, k.dbaasClient, req.KubeAuth.Kubeconfig, "percona-xtradb-cluster-operator"); err != nil {
-				k.l.Errorf("cannot approve the PXC operator installation: %s", err)
-			}
-		}()
-	}
-
-	settings, err := models.GetSettings(k.db.Querier)
-	if err != nil {
-		return nil, err
-	}
-	if settings.PMMPublicAddress != "" {
-		var apiKeyID int64
-		var apiKey string
-		apiKeyName := fmt.Sprintf("pmm-vmagent-%s-%d", req.KubernetesClusterName, rand.Int63())
-		apiKeyID, apiKey, err = k.grafanaClient.CreateAdminAPIKey(ctx, apiKeyName)
-		if err != nil {
-			return nil, err
-		}
-		pmmParams := &dbaascontrollerv1beta1.PMMParams{
-			PublicAddress: fmt.Sprintf("https://%s", settings.PMMPublicAddress),
-			Login:         "api_key",
-			Password:      apiKey,
-		}
-
-		_, err := k.dbaasClient.StartMonitoring(ctx, &dbaascontrollerv1beta1.StartMonitoringRequest{
-			KubeAuth: &dbaascontrollerv1beta1.KubeAuth{
-				Kubeconfig: req.KubeAuth.Kubeconfig,
-			},
-			Pmm: pmmParams,
-		})
-		if err != nil {
-			e := k.grafanaClient.DeleteAPIKeyByID(ctx, apiKeyID)
-			if e != nil {
-				k.l.Warnf("couldn't delete created API Key %v: %s", apiKeyID, e)
-			}
-			k.l.Warnf("couldn't start monitoring of the kubernetes cluster: %s", err)
-			return nil, status.Errorf(codes.Internal, "couldn't start monitoring of the kubernetes cluster: %s", err.Error())
-		}
-	}
-
-	return &dbaasv1beta1.RegisterKubernetesClusterResponse{}, nil
-}
-
 func installOLMOperator(ctx context.Context, client dbaasClient, kubeconfig, version string) error {
 	installOLMOperatorReq := &dbaascontrollerv1beta1.InstallOLMOperatorRequest{
 		KubeAuth: &dbaascontrollerv1beta1.KubeAuth{
@@ -394,29 +273,6 @@ func installOLMOperator(ctx context.Context, client dbaasClient, kubeconfig, ver
 
 	if _, err := client.InstallOLMOperator(ctx, installOLMOperatorReq); err != nil {
 		return errors.Wrap(err, "cannot install OLM operator")
-	}
-
-	return nil
-}
-
-func installOperator(ctx context.Context, client dbaasClient, kubeconfig, subscriptionNamespace, subscriptionName, group, channel, startingCSV string) error {
-	catalosSourceNamespace := "olm"
-
-	_, err := client.InstallOperator(ctx, &dbaascontrollerv1beta1.InstallOperatorRequest{
-		KubeAuth: &dbaascontrollerv1beta1.KubeAuth{
-			Kubeconfig: kubeconfig,
-		},
-		Namespace:              subscriptionNamespace,
-		Name:                   subscriptionName,
-		OperatorGroup:          group,
-		CatalogSource:          "operatorhubio-catalog",
-		CatalogSourceNamespace: catalosSourceNamespace,
-		Channel:                "stable",
-		InstallPlanApproval:    "Manual",
-		StartingCsv:            startingCSV,
-	})
-	if err != nil {
-		return err
 	}
 
 	return nil
@@ -520,7 +376,7 @@ func (k kubernetesServer) RegisterKubernetesCluster(ctx context.Context, req *db
 		}
 
 		if pxcOperatorVersion != nil && (clusterInfo.Operators == nil || clusterInfo.Operators.PxcOperatorVersion == "") {
-			if err := k.installOperator(ctx, "percona-xtradb-cluster-operator", "default", "", "stable", req.KubeAuth.Kubeconfig); err != nil {
+			if err := k.installOperator(ctx, "percona-xtradb-cluster-operator", "my-pxc-operator", "", "stable", req.KubeAuth.Kubeconfig); err != nil {
 				k.l.Errorf("cannot instal PXC operator in the new cluster: %s", err)
 			}
 			if err := approveInstallPlan(ctx, k.dbaasClient, req.KubeAuth.Kubeconfig, "percona-xtradb-cluster-operator"); err != nil {
@@ -529,7 +385,7 @@ func (k kubernetesServer) RegisterKubernetesCluster(ctx context.Context, req *db
 		}
 
 		if psmdbOperatorVersion != nil && (clusterInfo.Operators == nil || clusterInfo.Operators.PsmdbOperatorVersion == "") {
-			if err := k.installOperator(ctx, "percona-server-mongodb-operator", "default", "percona-server-mongodb-operator.v1.11.0", "stable", req.KubeAuth.Kubeconfig); err != nil {
+			if err := k.installOperator(ctx, "percona-server-mongodb-operator", "my-psmdb-operator", "percona-server-mongodb-operator.v1.11.0", "stable", req.KubeAuth.Kubeconfig); err != nil {
 				k.l.Errorf("cannot install PSMDB operator in the new cluster: %s", err)
 			}
 			if err := approveInstallPlan(ctx, k.dbaasClient, req.KubeAuth.Kubeconfig, "percona-server-mongodb-operator"); err != nil {
@@ -595,7 +451,7 @@ func (k kubernetesServer) installOperator(ctx context.Context, name, namespace, 
 		KubeAuth: &dbaascontrollerv1beta1.KubeAuth{
 			Kubeconfig: kubeConfig,
 		},
-		Namespace:              "my-" + name,
+		Namespace:              namespace,
 		Name:                   name,
 		OperatorGroup:          "og-" + name,
 		CatalogSource:          catalogSource,
