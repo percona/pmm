ed1531aff25a417d7a1850845b190ac60ca4227c
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2022-11-17 23:19:39 +0200

    PMM-10948 fix hints

--- managed/utils/interceptors/grpc_extension.go
+++ managed/utils/interceptors/grpc_extension.go
@@ -1,3 +1,19 @@
+// Copyright (C) 2017 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// Package interceptors contains gRPC wrappers for logging and Prometheus metrics.
 package interceptors
 
 import (
@@ -8,13 +24,14 @@ import (
 
 type grpcCallerOrigin struct{}
 
-type CallerOrigin string
+type callerOrigin string
 
 const (
-	InternalCallerOrigin = CallerOrigin("internal")
-	ExternalCallerOrigin = CallerOrigin("external")
+	internalCallerOrigin = callerOrigin("internal")
+	externalCallerOrigin = callerOrigin("external")
 )
 
+// GRPCMetricsExtension for extra labels in /debug/metrics
 type GRPCMetricsExtension struct {
 	grpc_prometheus.DefaultExtension
 }
@@ -24,34 +41,35 @@ func (e *GRPCMetricsExtension) ServerStreamMsgReceivedCounterCustomLabels() []st
 }
 
 func (e *GRPCMetricsExtension) ServerStreamMsgReceivedCounterValues(ctx context.Context) []string {
-	return []string{GetCallerOriginStr(ctx)}
+	return []string{getCallerOriginStr(ctx)}
 }
 
 var _ grpc_prometheus.ServerExtension = &GRPCMetricsExtension{}
 
+// SetCallerOrigin returns derived context with metric.
 func SetCallerOrigin(ctx context.Context, method string) context.Context {
 	return context.WithValue(ctx, grpcCallerOrigin{}, callerOriginFromRequest(ctx, method))
 }
 
-func GetCallerOriginStr(ctx context.Context) string {
+func getCallerOriginStr(ctx context.Context) string {
 	v := ctx.Value(grpcCallerOrigin{})
 	if v == nil {
 		return ""
 	}
-	return string(v.(CallerOrigin))
+	return string(v.(callerOrigin))
 }
 
-func callerOriginFromRequest(ctx context.Context, method string) CallerOrigin {
+func callerOriginFromRequest(ctx context.Context, method string) callerOrigin {
 	if method == "/server.Server/Readiness" || method == "/agent.Agent/Connect" {
-		return InternalCallerOrigin
+		return internalCallerOrigin
 	}
 
 	headers, _ := metadata.FromIncomingContext(ctx)
 
 	// if referer is present - the caller is an external one
 	if len(headers.Get("grpcgateway-referer")) == 0 {
-		return ExternalCallerOrigin
+		return externalCallerOrigin
 	}
 
-	return InternalCallerOrigin
+	return internalCallerOrigin
 }
