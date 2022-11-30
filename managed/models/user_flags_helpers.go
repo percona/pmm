bf476b911d06f295db33813e7f2b0be10acf0f7f
Michal Kralik <michal.kralik@percona.com>
2022-11-30 13:40:24 +0100

    PMM-10964: support for managing roles (#1367)
    * PMM-10964: support for managing roles

--- managed/models/user_flags_helpers.go
+++ managed/models/user_flags_helpers.go
@@ -16,6 +16,8 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/pkg/errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -34,6 +36,32 @@ type UpdateUserParams struct {
 	AlertingTour bool
 }
 
+// GetOrCreateUser returns user and optionally creates it, if not in database yet.
+func GetOrCreateUser(q *reform.Querier, userID int) (*UserDetails, error) {
+	userInfo, err := FindUser(q, userID)
+	if errors.Is(err, ErrNotFound) {
+		// User entry missing; create entry
+		params := CreateUserParams{
+			UserID: userID,
+		}
+		userInfo, err = CreateUser(q, &params)
+
+		// Handling race-condition
+		if errors.Is(err, ErrUserAlreadyExists) {
+			userInfo, err = FindUser(q, userID)
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userInfo, nil
+}
+
+// ErrUserAlreadyExists is returned when a user already exists in db.
+var ErrUserAlreadyExists = fmt.Errorf("UserAlreadyExists")
+
 // CreateUser create a new user with given parameters.
 func CreateUser(q *reform.Querier, params *CreateUserParams) (*UserDetails, error) {
 	if params.UserID <= 0 {
@@ -45,7 +73,7 @@ func CreateUser(q *reform.Querier, params *CreateUserParams) (*UserDetails, erro
 	err := q.Reload(row)
 	switch err {
 	case nil:
-		return nil, status.Errorf(codes.AlreadyExists, "User with ID %d already exists", params.UserID)
+		return nil, ErrUserAlreadyExists
 	case reform.ErrNoRows:
 		break
 	default:
