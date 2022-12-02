b3232b7c4d9ee968a32b0c3945c44d8f81bb942d
Rishat Ishbulatov <rishat.ishbulatov@simbirsoft.com>
2022-12-02 12:54:54 +0300

    PMM-10325 proto regen with register node responce model ref

--- api/managementpb/json/client/node/register_node_responses.go
+++ api/managementpb/json/client/node/register_node_responses.go
@@ -460,6 +460,9 @@ RegisterNodeOKBody register node OK body
 swagger:model RegisterNodeOKBody
 */
 type RegisterNodeOKBody struct {
+	// Token represents token for vmagent auth config.
+	Token string `json:"token,omitempty"`
+
 	// container node
 	ContainerNode *RegisterNodeOKBodyContainerNode `json:"container_node,omitempty"`
 
@@ -766,12 +769,6 @@ swagger:model RegisterNodeOKBodyPMMAgent
 type RegisterNodeOKBodyPMMAgent struct {
 	// Unique randomly generated instance identifier.
 	AgentID string `json:"agent_id,omitempty"`
-
-	// Login represent login for vmagent auth config.
-	Login string `json:"login,omitempty"`
-
-	// Password represent password used in vmagent auth config.
-	Password string `json:"password,omitempty"`
 
 	// Node identifier where this instance runs.
 	RunsOnNodeID string `json:"runs_on_node_id,omitempty"`
