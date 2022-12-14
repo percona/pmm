82eae8f5e503d43a5b3236a2fba6bbe68dd15ebc
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-12-15 02:12:54 +0300

    PMM-6352 update create-tags.py

--- build/scripts/create-tags.py
+++ build/scripts/create-tags.py
@@ -4,9 +4,21 @@ from __future__ import print_function, unicode_literals
 import os, subprocess, time
 
 REPOS = [
+    "sources/pmm/src/github.com/percona/pmm",
+    "sources/dbaas-controller/src/github.com/percona-platform/dbaas-controller",
+    "sources/pmm-dump",
+    "sources/pmm-qa/src/github.com/percona/pmm-qa",
+    "sources/pmm-ui-tests/src/github.com/percona/pmm-ui-tests",
     "sources/grafana/src/github.com/grafana/grafana",
     "sources/grafana-dashboards",
-    "sources/pmm/src/github.com/percona/pmm",
+    "sources/node_exporter/src/github.com/prometheus/node_exporter",
+    "sources/mysqld_exporter/src/github.com/percona/mysqld_exporter",
+    "sources/mongodb_exporter/src/github.com/percona/mongodb_exporter",
+    "sources/postgres_exporter/src/github.com/percona/postgres_exporter",
+    "sources/clickhouse_exporter/src/github.com/Percona-Lab/clickhouse_exporter",
+    "sources/proxysql_exporter/src/github.com/percona/proxysql_exporter",
+    "sources/rds_exporter/src/github.com/percona/rds_exporter",
+    "sources/azure_metrics_exporter/src/github.com/percona/azure_metrics_exporter",
     ".",
 ]
 
