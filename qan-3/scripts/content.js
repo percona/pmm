980d6bc72eed8e50fbd1c9242e034d7d7cd7bb22
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2022-12-22 11:29:10 +0100

    Comment out.

--- qan-3/scripts/content.js
+++ qan-3/scripts/content.js
@@ -1,12 +1,12 @@
 function callback() {
     alert("script loaded successfully.");
-    document.querySelector("body").setAttribute("style", "background-color: white");
+    //document.querySelector("body").setAttribute("style", "background-color: white");
 }
 
-function openDetails(e) {
-    if (!e) {
-        console.log("Not valid element");
-    }
+// function openDetails(e) {
+//     if (!e) {
+//         console.log("Not valid element");
+//     }
 
-    let query = e.querySelector("*[role='cell']:first-child:first-child");
-}
+//     let query = e.querySelector("*[role='cell']:first-child:first-child");
+// }
