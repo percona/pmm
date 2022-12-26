41cec88ebd0944a83866c8efcf661f40b3b9da7d
Artem Gavrilov <artem.gavrilov@percona.com>
2022-12-26 18:34:35 +0200

    PMM-7 Fix linters CI configuration; enable gosec, depguard, stylecheck and fix their warnings (#1531)
    * PMM-7 Fix linters CI configuration
    
    * PMM-7 Update golangci-lint
    
    * PMM-7 Fix linter warnings
    
    * PMM-7 Check that reviewdog fails CI
    
    * Revert "PMM-7 Check that reviewdog fails CI"
    
    This reverts commit 8d4e87d729efed8d76777e174d107ea2992a0d23.
    
    * PMM-7 Check that reviewdog fails CI
    
    * PMM-7 Enable gosec and fix warnigs
    
    * PMM-7 Enable depguard and fix warnings
    
    * PMM-7 Enable stylecheck and fix all warnings
    
    * Revert "PMM-7 Check that reviewdog fails CI"
    
    This reverts commit a867d32cd53ca26fce66417fa2a5b1ad8e064278.

--- managed/services/telemetry/datasource_grafana_sqlitedb.go
+++ managed/services/telemetry/datasource_grafana_sqlitedb.go
@@ -22,7 +22,7 @@ import (
 	"io"
 	"os"
 
-	// Events, errors and driver for for grafana sqlite database.
+	// Events, errors and driver for grafana sqlite database.
 	pmmv1 "github.com/percona-platform/saas/gen/telemetry/events/pmm"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
