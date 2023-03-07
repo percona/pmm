1b3bb1983d495ab049e6517ce043c953ac0315b9
Andrew Minkin <andrew.minkin@percona.com>
2023-03-07 18:30:41 +0600

    Fixed tests

--- managed/services/management/dbaas/db_cluster_service_test.go
+++ managed/services/management/dbaas/db_cluster_service_test.go
@@ -114,6 +114,7 @@ func TestDBClusterService(t *testing.T) {
 	grafanaClient.On("CreateAdminAPIKey", mock.Anything, mock.Anything).Return(int64(123456), "api-key", nil)
 
 	dbaasClient.On("StartMonitoring", mock.Anything, mock.Anything).Return(&controllerv1beta1.StartMonitoringResponse{}, nil)
+	kubeClient.On("GetServerVersion").Return(nil, nil)
 	clients := map[string]kubernetesClient{
 		dbKubernetesClusterNameTest: kubeClient,
 	}
