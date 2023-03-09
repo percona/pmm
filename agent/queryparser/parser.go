6e01b86fb6d4964c6cf307421c6ff3450338ffd3
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2023-03-09 12:42:15 +0100

    PMM-2852 MySQL comments parser.

--- agent/queryparser/parser.go
+++ agent/queryparser/parser.go
@@ -16,6 +16,9 @@
 package queryparser
 
 import (
+	"regexp"
+	"strings"
+
 	"github.com/pkg/errors"
 	"vitess.io/vitess/go/vt/proto/query"
 	"vitess.io/vitess/go/vt/sqlparser"
@@ -38,4 +41,47 @@ func MySQL(q string) (string, uint32, error) {
 	bindVars := sqlparser.GetBindvars(normalizedQuery)
 
 	return parsedQuery.Query, uint32(len(bindVars)), nil
+}
+
+// MySQLComments parse query and return its comments. Can parse multi comments.
+// Multi comments support should be dropped in future MySQL versions.
+// Doc: https://dev.mysql.com/doc/refman/8.0/en/comments.html
+func MySQLComments(q string) ([]string, error) {
+	// sqlparser.ExtractMysqlComment(q) doesnt work properly
+	// input: SELECT * FROM people /*! bla bla */ WHERE name = 'john'
+	// output: ECT * FROM people /*! bla bla */ WHERE name = 'joh
+	r, err := regexp.Compile("(?s)\\/\\*(.*?) \\*\\/")
+	if err != nil {
+		return nil, err
+	}
+
+	// comments using comment as a key to avoid duplicates
+	comments := make(map[string]bool)
+	for _, v := range r.FindAllStringSubmatch(strings.Replace(q, "\n", "", -1), -1) {
+		if len(v) < 2 {
+			continue
+		}
+
+		// replace all mutations of multiline comment
+		// /*! and /*+
+		replacer := strings.NewReplacer("!", "", "+", "")
+		comments[replacer.Replace(v[1])[1:]] = true
+	}
+
+	hashIndex := strings.Index(q, "#")
+	if hashIndex > -1 {
+		comments[q[(hashIndex+2):]] = true
+	}
+
+	dashIndex := strings.Index(q, "--")
+	if dashIndex > -1 {
+		comments[q[(dashIndex+3):]] = true
+	}
+
+	var res []string
+	for k, _ := range comments {
+		res = append(res, k)
+	}
+
+	return res, nil
 }
---
+++ agent/queryparser/parser_test.go
@@ -0,0 +1,87 @@
+// Copyright 2019 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package queryparser provides functionality for queries parsing.
+package queryparser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type testCase struct {
+	Name     string
+	Query    string
+	Comments []string
+}
+
+func TestMySQLComments(t *testing.T) {
+	testCases := []testCase{
+		{
+			Name:     "Dash comment",
+			Query:    "SELECT * FROM people -- dash comment",
+			Comments: []string{"dash comment"},
+		},
+		{
+			Name:     "Hash comment",
+			Query:    "SELECT * FROM people # hash comment",
+			Comments: []string{"hash comment"},
+		},
+		{
+			Name:     "Multiline comment",
+			Query:    "SELECT * FROM people /* multiline comment */",
+			Comments: []string{"multiline comment"},
+		},
+		{
+			Name: "Multiline comment with new line",
+			Query: "SELECT * FROM people /* multiline comment " +
+				"with new line" +
+				" */",
+			Comments: []string{"multiline comment with new line"},
+		},
+		{
+			Name: "Special multiline comment case with new line",
+			Query: "SELECT * FROM people /*! " +
+				"special multiline comment case " +
+				"with new line" +
+				" */",
+			Comments: []string{"special multiline comment case with new line"},
+		},
+		{
+			Name: "Second special multiline comment case with new line",
+			Query: "SELECT * FROM people /*+" +
+				" second special multiline comment case " +
+				"with new line" +
+				" */",
+			Comments: []string{"second special multiline comment case with new line"},
+		},
+		{
+			Name: "Multicomment case with new line",
+			Query: "SELECT * FROM people /*+" +
+				" multicomment case " +
+				"with new line" +
+				" */ WHERE name = 'John' # John selection",
+			Comments: []string{"multicomment case with new line", "John selection"},
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.Name, func(t *testing.T) {
+			comments, err := MySQLComments(c.Query)
+			require.NoError(t, err)
+			require.Equal(t, c.Comments, comments)
+		})
+	}
+}
