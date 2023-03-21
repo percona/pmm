eb2cc1c2d23e1f6dbaf79b51c4fc6e75209d7f04
Denis Stoyanov <stoyanov.gr@gmail.com>
2023-03-21 14:53:51 +0200

    PMM-5081: Prometheus metrics for Inventory data (fix license header)

--- managed/services/inventory/inventory_test.go
+++ managed/services/inventory/inventory_test.go
@@ -1,3 +1,18 @@
+// Copyright (C) 2017 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 package inventory
 
 import (
@@ -42,7 +57,7 @@ func TestInventory(t *testing.T) {
 	}
 
 	t.Run("Check real metrics", func(t *testing.T) {
-		_, err := http.NewRequest(http.MethodGet, "/debug/metrics", nil)
+		_, err := http.NewRequest(http.MethodGet, "http://127.0.0.1:7773/debug/metrics", nil)
 
 		require.NoError(t, err)
 		rw := httptest.NewRecorder()
