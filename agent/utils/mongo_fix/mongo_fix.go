5ddd22e31b8eb59493f76d6e7e41c10c9a59ff48
Alexey Mukas <ritbl@protonmail.com>
2023-03-24 11:13:25 +0100

    refactor

--- agent/utils/mongo_fix/mongo_fix.go
+++ agent/utils/mongo_fix/mongo_fix.go
@@ -36,16 +36,8 @@ func ClientOptionsForDSN(dsn string) (*options.ClientOptions, error) { //nolint:
 	username := parsedDsn.User.Username()
 	password, _ := parsedDsn.User.Password()
 	if username != "" || password != "" {
-		clientOptions = clientOptions.SetAuth(
-			options.Credential{
-				AuthMechanism:           clientOptions.Auth.AuthMechanism,
-				AuthMechanismProperties: clientOptions.Auth.AuthMechanismProperties,
-				AuthSource:              clientOptions.Auth.AuthSource,
-				Username:                username,
-				Password:                password,
-				PasswordSet:             clientOptions.Auth.PasswordSet,
-			},
-		)
+		clientOptions.Auth.Username = username
+		clientOptions.Auth.Password = password
 	}
 
 	return clientOptions, nil
