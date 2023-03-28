2244f162644846bdd1d99710d25e649bc41cc711
Alex Tymchuk <alexander.tymchuk@percona.com>
2023-03-28 09:10:03 +0000

    PMM-11684 address staticcheck warnings

--- managed/services/management/service.go
+++ managed/services/management/service.go
@@ -220,7 +220,7 @@ func (s *ServiceService) ListServices(ctx context.Context, req *managementpb.Lis
 		return nil, errTX
 	}
 
-	nodeMap := map[string]*models.Node{}
+	nodeMap := make(map[string]*models.Node, len(nodes))
 	for _, node := range nodes {
 		nodeMap[node.NodeID] = node
 	}
