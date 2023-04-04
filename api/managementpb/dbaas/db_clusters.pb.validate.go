f04e4c980443fbc6b404ae5eeaebda2985d56cfa
Diogo Recharte <diogo.recharte@percona.com>
2023-04-04 12:55:35 +0100

    PMM-11788 autogen get db cluster endpoint

--- api/managementpb/dbaas/db_clusters.pb.validate.go
+++ api/managementpb/dbaas/db_clusters.pb.validate.go
@@ -1564,6 +1564,35 @@ func (m *GetDBClusterResponse) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetPostgresqlCluster()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, GetDBClusterResponseValidationError{
+					field:  "PostgresqlCluster",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, GetDBClusterResponseValidationError{
+					field:  "PostgresqlCluster",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetPostgresqlCluster()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return GetDBClusterResponseValidationError{
+				field:  "PostgresqlCluster",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return GetDBClusterResponseMultiError(errors)
 	}
