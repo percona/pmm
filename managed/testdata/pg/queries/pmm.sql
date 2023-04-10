5f946683a439d8ea8745431decd51e670dc915c3
Rishat Ishbulatov <rishat.ishbulatov@simbirsoft.com>
2023-04-10 17:58:26 +0300

    PMM-10913 grafana with external postgres with ssl

--- managed/testdata/pg/queries/pmm.sql
+++ managed/testdata/pg/queries/pmm.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE "pmm-managed";
-CREATE USER "pmm-managed" LOGIN PASSWORD 'pmm-managed';
+CREATE USER "pmm-managed" WITH PASSWORD 'pmm-managed';
 GRANT ALL PRIVILEGES ON DATABASE "pmm-managed" TO "pmm-managed";
 \c pmm-managed;
 CREATE EXTENSION pg_stat_statements SCHEMA public;
