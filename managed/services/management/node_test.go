22860c7aee803699d095456ead60c10e3946e973
Alex Tymchuk <alexander.tymchuk@percona.com>
2023-05-01 11:55:46 +0000

    PMM-11973 add node tests

--- managed/services/management/node_test.go
+++ managed/services/management/node_test.go
@@ -20,6 +20,7 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -30,6 +31,7 @@ import (
 
 	"github.com/percona/pmm/api/inventorypb"
 	"github.com/percona/pmm/api/managementpb"
+	nodev1beta1 "github.com/percona/pmm/api/managementpb/node"
 	"github.com/percona/pmm/managed/models"
 	"github.com/percona/pmm/managed/utils/logger"
 	"github.com/percona/pmm/managed/utils/testdb"
@@ -37,29 +39,29 @@ import (
 )
 
 func TestNodeService(t *testing.T) {
-	setup := func(t *testing.T) (ctx context.Context, s *NodeService, teardown func(t *testing.T)) {
-		t.Helper()
-
-		ctx = logger.Set(context.Background(), t.Name())
-		uuid.SetRand(&tests.IDReader{})
-
-		sqlDB := testdb.Open(t, models.SetupFixtures, nil)
-		db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
-
-		teardown = func(t *testing.T) {
-			uuid.SetRand(nil)
-
-			require.NoError(t, sqlDB.Close())
-		}
-		var apiKeyProvider mockApiKeyProvider
-		apiKeyProvider.Test(t)
-		apiKeyProvider.On("CreateAdminAPIKey", ctx, mock.AnythingOfType("string")).Return(int64(0), "test-token", nil)
-		s = NewNodeService(db, &apiKeyProvider)
-
-		return
-	}
-
 	t.Run("Register", func(t *testing.T) {
+		setup := func(t *testing.T) (ctx context.Context, s *NodeService, teardown func(t *testing.T)) {
+			t.Helper()
+
+			ctx = logger.Set(context.Background(), t.Name())
+			uuid.SetRand(&tests.IDReader{})
+
+			sqlDB := testdb.Open(t, models.SetupFixtures, nil)
+			db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
+
+			teardown = func(t *testing.T) {
+				uuid.SetRand(nil)
+
+				require.NoError(t, sqlDB.Close())
+			}
+			var apiKeyProvider mockApiKeyProvider
+			apiKeyProvider.Test(t)
+			apiKeyProvider.On("CreateAdminAPIKey", ctx, mock.AnythingOfType("string")).Return(int64(0), "test-token", nil)
+			s = NewNodeService(db, &apiKeyProvider)
+
+			return
+		}
+
 		t.Run("New", func(t *testing.T) {
 			ctx, s, teardown := setup(t)
 			defer teardown(t)
@@ -147,5 +149,265 @@ func TestNodeService(t *testing.T) {
 				assert.NoError(t, err)
 			})
 		})
+	})
+
+	t.Run("ListNodes", func(t *testing.T) {
+		setup := func(t *testing.T) (ctx context.Context, s *MgmtNodeService, teardown func(t *testing.T)) {
+			t.Helper()
+
+			ctx = logger.Set(context.Background(), t.Name())
+			uuid.SetRand(&tests.IDReader{})
+
+			sqlDB := testdb.Open(t, models.SetupFixtures, nil)
+			db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
+
+			ar := &mockAgentsRegistry{}
+			ar.Test(t)
+
+			vmClient := &mockVictoriaMetricsClient{}
+			vmClient.Test(t)
+
+			s = NewMgmtNodeService(db, ar, vmClient)
+
+			teardown = func(t *testing.T) {
+				uuid.SetRand(nil)
+
+				require.NoError(t, sqlDB.Close())
+				ar.AssertExpectations(t)
+			}
+
+			return
+		}
+
+		const (
+			nodeExporterID      = "/agent_id/00000000-0000-4000-8000-000000000001"
+			postgresqlServiceId = "/service_id/00000000-0000-4000-8000-000000000002"
+		)
+
+		t.Run("should list all nodes unfiltered", func(t *testing.T) {
+			ctx, s, teardown := setup(t)
+			defer teardown(t)
+
+			metric := model.Vector{
+				&model.Sample{
+					Metric: model.Metric{
+						"__name__": "up",
+						"node_id":  "pmm-server",
+					},
+					Timestamp: 1,
+					Value:     1,
+				},
+			}
+
+			s.vmClient.(*mockVictoriaMetricsClient).On("Query", ctx, mock.Anything, mock.Anything).Return(metric, nil, nil).Times(2)
+			s.r.(*mockAgentsRegistry).On("IsConnected", models.PMMServerAgentID).Return(true).Once()
+			s.r.(*mockAgentsRegistry).On("IsConnected", nodeExporterID).Return(true).Once()
+			res, err := s.ListNodes(ctx, &nodev1beta1.ListNodeRequest{})
+
+			agents := []*nodev1beta1.UniversalNode_Agent{
+				{
+					AgentId:     nodeExporterID,
+					AgentType:   "node_exporter",
+					Status:      "UNKNOWN",
+					IsConnected: true,
+				},
+				{
+					AgentId:     models.PMMServerAgentID,
+					AgentType:   "pmm-agent",
+					Status:      "",
+					IsConnected: true,
+				},
+			}
+
+			services := []*nodev1beta1.UniversalNode_Service{
+				{
+					ServiceId:   postgresqlServiceId,
+					ServiceType: "postgresql",
+					ServiceName: "pmm-server-postgresql",
+				},
+			}
+
+			expected := &nodev1beta1.ListNodeResponse{
+				Nodes: []*nodev1beta1.UniversalNode{
+					{
+						NodeId:        "pmm-server",
+						NodeType:      "generic",
+						NodeName:      "pmm-server",
+						MachineId:     "",
+						Distro:        "",
+						NodeModel:     "",
+						ContainerId:   "",
+						ContainerName: "",
+						Address:       "127.0.0.1",
+						Region:        "",
+						Az:            "",
+						CustomLabels:  nil,
+						CreatedAt:     nil,
+						UpdatedAt:     nil,
+						Agents:        agents,
+						Services:      services,
+						Status:        nodev1beta1.UniversalNode_UP,
+					},
+				},
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, expected, res)
+		})
+
+		t.Run("should output an empty list of nodes when filter condition is not satisfied", func(t *testing.T) {
+			ctx, s, teardown := setup(t)
+			defer teardown(t)
+
+			s.vmClient.(*mockVictoriaMetricsClient).On("Query", ctx, mock.Anything, mock.Anything).Return(model.Vector{}, nil, nil).Times(2)
+			s.r.(*mockAgentsRegistry).On("IsConnected", models.PMMServerAgentID).Return(true).Once()
+			s.r.(*mockAgentsRegistry).On("IsConnected", nodeExporterID).Return(true).Once()
+
+			res, err := s.ListNodes(ctx, &nodev1beta1.ListNodeRequest{
+				NodeType: inventorypb.NodeType_REMOTE_NODE,
+			})
+
+			assert.NoError(t, err)
+			assert.Empty(t, res.Nodes)
+		})
+
+		t.Run("should output a list of nodes when filter condition is satisfied", func(t *testing.T) {
+			ctx, s, teardown := setup(t)
+			defer teardown(t)
+
+			metric := model.Vector{
+				&model.Sample{
+					Metric: model.Metric{
+						"__name__": "up",
+						"node_id":  "pmm-server",
+					},
+					Timestamp: 1,
+					Value:     1,
+				},
+			}
+			s.vmClient.(*mockVictoriaMetricsClient).On("Query", ctx, mock.Anything, mock.Anything).Return(metric, nil, nil).Times(2)
+			s.r.(*mockAgentsRegistry).On("IsConnected", models.PMMServerAgentID).Return(true).Once()
+			s.r.(*mockAgentsRegistry).On("IsConnected", nodeExporterID).Return(true).Once()
+
+			res, err := s.ListNodes(ctx, &nodev1beta1.ListNodeRequest{
+				NodeType: inventorypb.NodeType_GENERIC_NODE,
+			})
+
+			expected := &nodev1beta1.ListNodeResponse{
+				Nodes: []*nodev1beta1.UniversalNode{
+					{
+						NodeId:        "pmm-server",
+						NodeType:      "generic",
+						NodeName:      "pmm-server",
+						MachineId:     "",
+						Distro:        "",
+						NodeModel:     "",
+						ContainerId:   "",
+						ContainerName: "",
+						Address:       "127.0.0.1",
+						Region:        "",
+						Az:            "",
+						CustomLabels:  nil,
+						CreatedAt:     nil,
+						UpdatedAt:     nil,
+						Agents: []*nodev1beta1.UniversalNode_Agent{
+							{
+								AgentId:     nodeExporterID,
+								AgentType:   "node_exporter",
+								Status:      "UNKNOWN",
+								IsConnected: true,
+							},
+							{
+								AgentId:     models.PMMServerAgentID,
+								AgentType:   "pmm-agent",
+								Status:      "",
+								IsConnected: true,
+							},
+						},
+						Services: []*nodev1beta1.UniversalNode_Service{
+							{
+								ServiceId:   postgresqlServiceId,
+								ServiceType: "postgresql",
+								ServiceName: "pmm-server-postgresql",
+							},
+						},
+						Status: nodev1beta1.UniversalNode_UP,
+					},
+				},
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, expected, res)
+		})
+
+		t.Run("should output a list of nodes when filter condition is satisfied", func(t *testing.T) {
+			ctx, s, teardown := setup(t)
+			defer teardown(t)
+
+			metric := model.Vector{
+				&model.Sample{
+					Metric: model.Metric{
+						"__name__": "up",
+						"node_id":  "pmm-server",
+					},
+					Timestamp: 1,
+					Value:     1,
+				},
+			}
+			s.vmClient.(*mockVictoriaMetricsClient).On("Query", ctx, mock.Anything, mock.Anything).Return(metric, nil, nil).Times(2)
+			s.r.(*mockAgentsRegistry).On("IsConnected", models.PMMServerAgentID).Return(true).Once()
+			s.r.(*mockAgentsRegistry).On("IsConnected", nodeExporterID).Return(true).Once()
+
+			res, err := s.ListNodes(ctx, &nodev1beta1.ListNodeRequest{
+				NodeType: inventorypb.NodeType_GENERIC_NODE,
+			})
+
+			expected := &nodev1beta1.ListNodeResponse{
+				Nodes: []*nodev1beta1.UniversalNode{
+					{
+						NodeId:        "pmm-server",
+						NodeType:      "generic",
+						NodeName:      "pmm-server",
+						MachineId:     "",
+						Distro:        "",
+						NodeModel:     "",
+						ContainerId:   "",
+						ContainerName: "",
+						Address:       "127.0.0.1",
+						Region:        "",
+						Az:            "",
+						CustomLabels:  nil,
+						CreatedAt:     nil,
+						UpdatedAt:     nil,
+						Agents: []*nodev1beta1.UniversalNode_Agent{
+							{
+								AgentId:     nodeExporterID,
+								AgentType:   "node_exporter",
+								Status:      "UNKNOWN",
+								IsConnected: true,
+							},
+							{
+								AgentId:     models.PMMServerAgentID,
+								AgentType:   "pmm-agent",
+								Status:      "",
+								IsConnected: true,
+							},
+						},
+						Services: []*nodev1beta1.UniversalNode_Service{
+							{
+								ServiceId:   postgresqlServiceId,
+								ServiceType: "postgresql",
+								ServiceName: "pmm-server-postgresql",
+							},
+						},
+						Status: nodev1beta1.UniversalNode_UP,
+					},
+				},
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, expected, res)
+		})
+
 	})
 }
