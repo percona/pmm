f82acf8744e02dc5076abb70533b9ee0ce531313
Vadym Yarosh <vadym.yarosh@percona.com>
2023-05-02 12:25:23 +0200

    PMM-11742 Added requested changes

--- cli-tests/support/pmm-bin.setup.ts
+++ cli-tests/support/pmm-bin.setup.ts
@@ -1,18 +1,15 @@
 import { test as setup } from '@playwright/test';
-import * as cli from '@helpers/cliHelper';
 
 const oldImage = 'percona/pmm-server:2.32.0';
 const newImage = 'percona/pmm-server:2.33.0';
 
 setup.describe.configure({ mode: 'parallel' });
-setup(`pull ${newImage}`, async () => {
-  process.env.server_image = newImage;
-  const output = await cli.exec(`docker pull ${newImage}`);
-  await output.assertSuccess();
-});
 
-setup(`pull ${oldImage}`, async () => {
+/**
+ * Extension point un-hardcode versions using environment variables
+ * TODO: add detection of latest released and RC versions and previous release
+ */
+setup('Set default env.VARs', async () => {
+  process.env.server_image = newImage;
   process.env.server_image_old = oldImage;
-  const output = await cli.exec(`docker pull ${oldImage}`);
-  await output.assertSuccess();
 });
