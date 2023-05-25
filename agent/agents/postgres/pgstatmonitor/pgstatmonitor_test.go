af36c0835ca8fbf53f1b4f4fb3cedb60105485f4
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2023-05-25 17:49:01 +0200

    PMM-2852 PGSS PGSM

--- agent/agents/postgres/pgstatmonitor/pgstatmonitor_test.go
+++ agent/agents/postgres/pgstatmonitor/pgstatmonitor_test.go
@@ -96,15 +96,15 @@ func TestVersion(t *testing.T) {
 }
 
 func TestPGStatMonitorSchema(t *testing.T) {
-	t.Skip("Skip it until the sandbox supports pg_stat_monitor by default. The current PostgreSQL image is the official, not the one from PerconaLab")
+	//t.Skip("Skip it until the sandbox supports pg_stat_monitor by default. The current PostgreSQL image is the official, not the one from PerconaLab")
 	sqlDB := tests.OpenTestPostgreSQL(t)
 	defer sqlDB.Close() //nolint:errcheck
 	db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
 
 	engineVersion := tests.PostgreSQLVersion(t, sqlDB)
-	if !supportedVersion(engineVersion) || !extensionExists(db) {
-		t.Skip()
-	}
+	// if !supportedVersion(engineVersion) || !extensionExists(db) {
+	// 	t.Skip()
+	// }
 
 	_, err := db.Exec("CREATE EXTENSION IF NOT EXISTS pg_stat_monitor SCHEMA public")
 	assert.NoError(t, err)
@@ -144,7 +144,7 @@ func TestPGStatMonitorSchema(t *testing.T) {
 		"$281, $282, $283, $284, $285, $286, $287, $288, $289, $290, $291, $292, $293, $294, $295, $296, $297, $298, $299, $300, " +
 		"$301, $302, $303, $304, $305, $306, $307, $308, $309, $310, $311, $312, $313, $314, $315, $316, $317, $318, $319, $320, " +
 		"$321, $322, $323, $324, $325, $326, $327, $328, $329, $330, $331, $332, $333, $334, $335, $336, $337, $338, $339, $340, " +
-		"$341, $342, $343, $344, $345,  ..."
+		"$341, $342, $343, $3 ..."
 
 	var digests map[string]string
 	switch engineVersion {
