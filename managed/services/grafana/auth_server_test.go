de4e280fe746cef7833fb00f873c42196df3a629
Nurlan Moldomurov <bupychuk1989@gmail.com>
2023-05-31 18:29:42 +0300

    PMM-12182 fix (#2201)
    * PMM-12182 Fix the bug.
    
    * PMM-12182 Fix linter.
    
    * PMM-12182 Improve error message.
    
    * Apply suggestions from code review
    
    Co-authored-by: Artem Gavrilov <artem.gavrilov@percona.com>
    
    ---------
    
    Co-authored-by: Artem Gavrilov <artem.gavrilov@percona.com>

--- managed/services/grafana/auth_server_test.go
+++ managed/services/grafana/auth_server_test.go
@@ -49,6 +49,7 @@ func TestNextPrefix(t *testing.T) {
 		{"./", "/", "/"},
 		{"hax0r", "/", "/"},
 		{"", "/"},
+		{"/v1/AWSInstanceCheck/..%2finventory/Services/List'"},
 	} {
 		t.Run(paths[0], func(t *testing.T) {
 			for i, path := range paths[:len(paths)-1] {
@@ -220,6 +221,9 @@ func TestAuthServerAuthenticate(t *testing.T) {
 		"/v1/AWSInstanceCheck":                             none,
 		"/v1/Platform/Connect":                             admin,
 
+		"/v1/AWSInstanceCheck/..%2finventory/Services/List": admin,
+		"/v1/AWSInstanceCheck/..%2f..%2flogs.zip":           admin,
+
 		"/v1/readyz": none,
 		"/ping":      none,
 
@@ -377,4 +381,32 @@ func TestAuthServerAddVMGatewayToken(t *testing.T) {
 		require.Equal(t, parsed[0], "filter A")
 		require.Equal(t, parsed[1], "filter B")
 	})
+}
+
+func Test_cleanPath(t *testing.T) {
+    t.Parallel()
+	tests := []struct {
+		path     string
+		expected string
+	}{
+		{
+			"/v1/AWSInstanceCheck/..%2finventory/Services/List",
+			"/v1/inventory/Services/List",
+		}, {
+			"/v1/AWSInstanceCheck/..%2f..%2fmanaged/logs.zip",
+			"/managed/logs.zip",
+		}, {
+			"/v1/AWSInstanceCheck/..%2f..%2f/logs.zip",
+			"/logs.zip",
+		},
+	}
+	for _, tt := range tests {
+	tt := tt
+		t.Run(tt.path, func(t *testing.T) {
+		t.Parallel()
+			cleanedPath, err := cleanPath(tt.path)
+			require.NoError(t, err)
+			assert.Equalf(t, tt.expected, cleanedPath, "cleanPath(%v)", tt.path)
+		})
+	}
 }
