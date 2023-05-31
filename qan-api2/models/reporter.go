eabdbe8191e67acbc08a7f4ac1cadbe75db2dda6
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2023-05-31 09:06:38 +0200

    PMM-2852 AND between labels, but OR for same key.

--- qan-api2/models/reporter.go
+++ qan-api2/models/reporter.go
@@ -94,8 +94,8 @@ WHERE period_start >= :period_start_from AND period_start <= :period_start_to
     {{ end }}
 {{ end }}
 {{ if .Labels }}{{$i := 0}}
-	AND ({{range $key, $vals := .Labels }} {{ $i = inc $i}}
-		{{ if gt $i 1}} AND {{ end }} hasAll(labels.value, ['{{ StringsJoin $vals "', '" }}']) AND hasAll(labels.key, ['{{ $key }}'])
+AND ({{range $key, $vals := .Labels }}{{ $i = inc $i}}
+	{{ if gt $i 1}} AND {{ end }} has(['{{ StringsJoin $vals "', '" }}'], labels.value[indexOf(labels.key, '{{ $key }}')])
     {{ end }})
 {{ end }}
 GROUP BY {{ .Group }}
@@ -254,8 +254,8 @@ WHERE period_start >= :period_start_from AND period_start <= :period_start_to
 {{ if not .IsTotal }} AND {{ .Group }} = '{{ .DimensionVal }}' {{ end }}
     {{range $key, $vals := .Dimensions }} AND {{ $key }} IN ( '{{ StringsJoin $vals "', '" }}' ){{ end }}
 {{ if .Labels }}{{$i := 0}}
-	AND ({{range $key, $vals := .Labels }}{{ $i = inc $i}}
-		{{ if gt $i 1}} OR {{ end }} has(['{{ StringsJoin $vals "', '" }}'], labels.value[indexOf(labels.key, '{{ $key }}')])
+AND ({{range $key, $vals := .Labels }}{{ $i = inc $i}}
+	{{ if gt $i 1}} OR {{ end }} has(['{{ StringsJoin $vals "', '" }}'], labels.value[indexOf(labels.key, '{{ $key }}')])
     {{ end }})
 {{ end }}
 GROUP BY point
