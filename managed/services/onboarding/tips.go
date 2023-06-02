b15fb16ab89537b68d927109e3c1a51a081dfc57
Dmytro Liakhov <dim@sikk.me>
2023-06-02 14:47:17 +0300

    Update managed/services/onboarding/tips.go
    Co-authored-by: Alex Tymchuk <alexander.tymchuk@percona.com>

--- managed/services/onboarding/tips.go
+++ managed/services/onboarding/tips.go
@@ -247,7 +247,7 @@ func (t *TipsService) CompleteUserTip(ctx context.Context, userTipRequest *onboa
 	if t.isSystemTip(userTipRequest.TipId) {
 		return nil, errors.New("Tip ID is not correct, it's a system tip")
 	}
-	if ok := t.isUserTip(userTipRequest.TipId); !ok {
+	if t.isUserTip(userTipRequest.TipId) {
 		return nil, errors.New("Tip ID is not correct, it's not a user tip")
 	}
 
