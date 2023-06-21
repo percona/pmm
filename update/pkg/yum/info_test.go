fed65cc53b128007434a31ac7a3e4cfefd1ddbc6
Alex Tymchuk <alexander.tymchuk@percona.com>
2023-06-21 06:57:26 +0000

    PMM-12237 fix the upgrade check failure

--- update/pkg/yum/info_test.go
+++ update/pkg/yum/info_test.go
@@ -337,3 +337,12 @@ func TestParseInfo(t *testing.T) {
 		assert.Equal(t, time.Date(2021, 6, 1, 15, 30, 45, 0, time.UTC), releasetime)
 	})
 }
+
+func TestGetRHELVersion(t *testing.T) {
+	t.Run("getRHELVersion", func(t *testing.T) {
+		expected := "9"
+		actual, err := getRHELVersion()
+		require.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	})
+}
