d32960c8b6221973e039b35f9f963b41507e4048
Nurlan Moldomurov <bupychuk1989@gmail.com>
2023-07-07 23:22:15 +0300

    PMM-12078 fix tests

--- managed/services/supervisord/supervisord_test.go
+++ managed/services/supervisord/supervisord_test.go
@@ -48,7 +48,7 @@ func TestConfig(t *testing.T) {
 		SSLKeyPath:  "path-to-key",
 		SSLCertPath: "path-to-cert",
 	}
-	s := New(configDir, pmmUpdateCheck, &models.Params{VMParams: vmParams, PGParams: pgParams}, gRPCMessageMaxSize)
+	s := New(configDir, pmmUpdateCheck, &models.Params{VMParams: vmParams, PGParams: pgParams, HAParams: &models.HAParams{}}, gRPCMessageMaxSize)
 	settings := &models.Settings{
 		DataRetention:   30 * 24 * time.Hour,
 		AlertManagerURL: "https://external-user:passw!,ord@external-alertmanager:6443/alerts",
@@ -81,7 +81,7 @@ func TestDBaaSController(t *testing.T) {
 	configDir := filepath.Join("..", "..", "testdata", "supervisord.d")
 	vmParams, err := models.NewVictoriaMetricsParams(models.BasePrometheusConfigPath, models.VMBaseURL)
 	require.NoError(t, err)
-	s := New(configDir, pmmUpdateCheck, &models.Params{VMParams: vmParams, PGParams: &models.PGParams{}}, gRPCMessageMaxSize)
+	s := New(configDir, pmmUpdateCheck, &models.Params{VMParams: vmParams, PGParams: &models.PGParams{}, HAParams: &models.HAParams{}}, gRPCMessageMaxSize)
 
 	var tp *template.Template
 	for _, tmpl := range templates.Templates() {
