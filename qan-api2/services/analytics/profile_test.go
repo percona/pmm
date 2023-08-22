24b6a6f8b758d4bbe5b45fb815b912445b74d5c9
vishwas-sharma2480 <vishwas.sharma2480@gmail.com>
2023-08-23 02:10:33 +0530

    made changes to profile_test.go file located in qan-api2/services/analytics/profile_test.go

--- qan-api2/services/analytics/profile_test.go
+++ qan-api2/services/analytics/profile_test.go
@@ -23,7 +23,7 @@ import (
 	"testing"
 	"time"
 
-	// TODO replace with 'google.golang.org/protobuf/encoding/protojson' since this one is deprecated
+	// TODO replace with 'google.golang.org/protobuf/encoding/protojson' since this one is deprecated.
 	"github.com/golang/protobuf/jsonpb" //nolint:staticcheck
 	// TODO replace with 'google.golang.org/protobuf/proto' since this one is deprecated.
 	"github.com/golang/protobuf/proto" //nolint:staticcheck
