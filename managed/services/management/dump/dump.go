41fd1ea1a9d70185477a117d1f6b141491f1dbc9
Artem Gavrilov <artem.gavrilov@percona.com>
2023-09-26 18:36:35 +0200

    PMM-12460 Fix

--- managed/services/management/dump/dump.go
+++ managed/services/management/dump/dump.go
@@ -1,15 +1,30 @@
+// Copyright (C) 2023 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 package dump
 
 import (
 	"context"
 
-	"github.com/percona/pmm/managed/models"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"gopkg.in/reform.v1"
 
 	dumpv1beta1 "github.com/percona/pmm/api/managementpb/dump"
+	"github.com/percona/pmm/managed/models"
 )
 
 type Service struct {
