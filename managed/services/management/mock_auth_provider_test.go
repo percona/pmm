d0abd62930caa3ed6185f5cb26d29b5df9db8911
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2023-10-10 09:23:50 +0200

    PMM-12251 Mock gen.

--- managed/services/management/mock_auth_provider_test.go
+++ managed/services/management/mock_auth_provider_test.go
@@ -44,30 +44,54 @@ func (_m *mockAuthProvider) CreateAdminAPIKey(ctx context.Context, name string) 
 	return r0, r1, r2
 }
 
-// CreateServiceAccountAndToken provides a mock function with given fields: ctx, name
-func (_m *mockAuthProvider) CreateServiceAccountAndToken(ctx context.Context, name string) (int64, string, error) {
-	ret := _m.Called(ctx, name)
+// CreateServiceAccount provides a mock function with given fields: ctx
+func (_m *mockAuthProvider) CreateServiceAccount(ctx context.Context) (int64, error) {
+	ret := _m.Called(ctx)
 
 	var r0 int64
-	var r1 string
-	var r2 error
-	if rf, ok := ret.Get(0).(func(context.Context, string) (int64, string, error)); ok {
-		return rf(ctx, name)
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) (int64, error)); ok {
+		return rf(ctx)
 	}
-	if rf, ok := ret.Get(0).(func(context.Context, string) int64); ok {
-		r0 = rf(ctx, name)
+	if rf, ok := ret.Get(0).(func(context.Context) int64); ok {
+		r0 = rf(ctx)
 	} else {
 		r0 = ret.Get(0).(int64)
 	}
 
-	if rf, ok := ret.Get(1).(func(context.Context, string) string); ok {
-		r1 = rf(ctx, name)
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// CreateServiceToken provides a mock function with given fields: ctx, serviceAccountID
+func (_m *mockAuthProvider) CreateServiceToken(ctx context.Context, serviceAccountID int64) (int64, string, error) {
+	ret := _m.Called(ctx, serviceAccountID)
+
+	var r0 int64
+	var r1 string
+	var r2 error
+	if rf, ok := ret.Get(0).(func(context.Context, int64) (int64, string, error)); ok {
+		return rf(ctx, serviceAccountID)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, int64) int64); ok {
+		r0 = rf(ctx, serviceAccountID)
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, int64) string); ok {
+		r1 = rf(ctx, serviceAccountID)
 	} else {
 		r1 = ret.Get(1).(string)
 	}
 
-	if rf, ok := ret.Get(2).(func(context.Context, string) error); ok {
-		r2 = rf(ctx, name)
+	if rf, ok := ret.Get(2).(func(context.Context, int64) error); ok {
+		r2 = rf(ctx, serviceAccountID)
 	} else {
 		r2 = ret.Error(2)
 	}
@@ -80,8 +104,7 @@ func (_m *mockAuthProvider) CreateServiceAccountAndToken(ctx context.Context, na
 func newMockAuthProvider(t interface {
 	mock.TestingT
 	Cleanup(func())
-},
-) *mockAuthProvider {
+}) *mockAuthProvider {
 	mock := &mockAuthProvider{}
 	mock.Mock.Test(t)
 
