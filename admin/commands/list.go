7dd7d2e4356d8eaf278bb7f135b220c5a1772ecc
Alex Demidoff <alexander.demidoff@percona.com>
2023-11-10 17:46:30 +0000

    PMM-12643 consistent types in list output of pmm-admin

--- admin/commands/list.go
+++ admin/commands/list.go
@@ -222,7 +222,7 @@ func (cmd *ListCommand) RunCmd() (Result, error) { //nolint:cyclop,maintidx
 		return "pull"
 	}
 	pmmAgentIDs := make(map[string]struct{})
-	var agentsList []listResultAgent
+	agentsList := make([]listResultAgent, 0, len(agentsRes.Payload.PMMAgent))
 	for _, a := range agentsRes.Payload.PMMAgent {
 		if a.RunsOnNodeID == cmd.NodeID {
 			pmmAgentIDs[a.AgentID] = struct{}{}
