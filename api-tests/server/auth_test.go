5249ff18c2277c57db5f6e5cf58d4a4ad0e48fe6
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2023-11-21 11:12:32 +0100

    PMM-12251 Basic auth tests.

--- api-tests/server/auth_test.go
+++ api-tests/server/auth_test.go
@@ -23,6 +23,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -252,6 +253,159 @@ func doRequest(tb testing.TB, client *http.Client, req *http.Request) (*http.Res
 	return resp, b
 }
 
+func TestBasicAuthPermissions(t *testing.T) {
+	ts := strconv.FormatInt(time.Now().Unix(), 10)
+	none := "none-" + ts
+	viewer := "viewer-" + ts
+	editor := "editor-" + ts
+	admin := "admin-" + ts
+
+	noneID := createUser(t, none)
+	defer deleteUser(t, noneID)
+
+	viewerID := createUserWithRole(t, viewer, "Viewer")
+	defer deleteUser(t, viewerID)
+
+	editorID := createUserWithRole(t, editor, "Editor")
+	defer deleteUser(t, editorID)
+
+	adminID := createUserWithRole(t, admin, "Admin")
+	defer deleteUser(t, adminID)
+
+	type userCase struct {
+		userType   string
+		login      string
+		statusCode int
+	}
+
+	tests := []struct {
+		name     string
+		url      string
+		method   string
+		userCase []userCase
+	}{
+		{name: "settings", url: "/v1/Settings/Get", method: "POST", userCase: []userCase{
+			{userType: "default", login: none, statusCode: 401},
+			{userType: "viewer", login: viewer, statusCode: 401},
+			{userType: "editor", login: editor, statusCode: 401},
+			{userType: "admin", login: admin, statusCode: 200},
+		}},
+		{name: "alerts-default", url: "/alertmanager/api/v2/alerts", method: http.MethodGet, userCase: []userCase{
+			{userType: "default", login: none, statusCode: 401},
+			{userType: "viewer", login: viewer, statusCode: 401},
+			{userType: "editor", login: editor, statusCode: 401},
+			{userType: "admin", login: admin, statusCode: 200},
+		}},
+		{name: "platform-connect", url: "/v1/Platform/Connect", method: "POST", userCase: []userCase{
+			{userType: "default", login: none, statusCode: 401},
+			{userType: "viewer", login: viewer, statusCode: 401},
+			{userType: "editor", login: editor, statusCode: 401},
+			{userType: "admin", login: admin, statusCode: 400}, // We send bad request, but have access to endpoint
+		}},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			for _, user := range test.userCase {
+				user := user
+				t.Run(fmt.Sprintf("Basic auth %s", user.userType), func(t *testing.T) {
+					// make a BaseURL without authentication
+					u, err := url.Parse(pmmapitests.BaseURL.String())
+					require.NoError(t, err)
+					u.User = url.UserPassword(user.login, user.login)
+					u.Path = test.url
+
+					req, err := http.NewRequestWithContext(pmmapitests.Context, test.method, u.String(), nil)
+					require.NoError(t, err)
+
+					resp, err := http.DefaultClient.Do(req)
+					require.NoError(t, err)
+					defer resp.Body.Close() //nolint:gosec,errcheck
+
+					assert.Equal(t, user.statusCode, resp.StatusCode)
+				})
+			}
+		})
+	}
+}
+
+func createUserWithRole(t *testing.T, login, role string) int {
+	t.Helper()
+	userID := createUser(t, login)
+	setRole(t, userID, role)
+
+	return userID
+}
+
+func deleteUser(t *testing.T, userID int) {
+	t.Helper()
+	u, err := url.Parse(pmmapitests.BaseURL.String())
+	require.NoError(t, err)
+	u.Path = "/graph/api/admin/users/" + strconv.Itoa(userID)
+
+	req, err := http.NewRequestWithContext(pmmapitests.Context, http.MethodDelete, u.String(), nil)
+	require.NoError(t, err)
+
+	resp, b := doRequest(t, http.DefaultClient, req)
+	defer resp.Body.Close() //nolint:gosec,errcheck
+
+	require.Equalf(t, http.StatusOK, resp.StatusCode, "failed to delete user, status code: %d, response: %s", resp.StatusCode, b)
+}
+
+func createUser(t *testing.T, login string) int {
+	t.Helper()
+	u, err := url.Parse(pmmapitests.BaseURL.String())
+	require.NoError(t, err)
+	u.Path = "/graph/api/admin/users"
+
+	// https://grafana.com/docs/http_api/admin/#global-users
+	data, err := json.Marshal(map[string]string{
+		"name":     login,
+		"email":    login + "@percona.invalid",
+		"login":    login,
+		"password": login,
+	})
+	require.NoError(t, err)
+
+	req, err := http.NewRequestWithContext(pmmapitests.Context, http.MethodPost, u.String(), bytes.NewReader(data))
+	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	resp, b := doRequest(t, http.DefaultClient, req)
+	defer resp.Body.Close() //nolint:gosec,errcheck
+	require.Equalf(t, http.StatusOK, resp.StatusCode, "failed to create user, status code: %d, response: %s", resp.StatusCode, b)
+
+	var m map[string]interface{}
+	err = json.Unmarshal(b, &m)
+	require.NoError(t, err)
+
+	return int(m["id"].(float64))
+}
+
+func setRole(t *testing.T, userID int, role string) {
+	t.Helper()
+	u, err := url.Parse(pmmapitests.BaseURL.String())
+	require.NoError(t, err)
+	u.Path = "/graph/api/org/users/" + strconv.Itoa(userID)
+
+	// https://grafana.com/docs/http_api/org/#updates-the-given-user
+	data, err := json.Marshal(map[string]string{
+		"role": role,
+	})
+	require.NoError(t, err)
+
+	req, err := http.NewRequestWithContext(pmmapitests.Context, http.MethodPatch, u.String(), bytes.NewReader(data))
+	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	resp, b := doRequest(t, http.DefaultClient, req)
+	defer resp.Body.Close() //nolint:gosec,errcheck
+
+	require.Equalf(t, http.StatusOK, resp.StatusCode, "failed to set role for user, response: %s", b)
+}
+
 func TestServiceAccountPermissions(t *testing.T) {
 	// service account role options: viewer, editor, admin
 	// service token role options: editor, admin
@@ -259,7 +413,9 @@ func TestServiceAccountPermissions(t *testing.T) {
 	// service_token:token could be used in pmm-agent and pmm-admin (its transformed into Bearer authorization)
 
 	viewerAccountID := createServiceAccountWithRole(t, "Viewer")
+	viewerTokenID, viewerToken := createServiceToken(t, viewerAccountID)
 	defer deleteServiceAccount(t, viewerAccountID)
+	defer deleteServiceToken(t, viewerAccountID, viewerTokenID)
 
 	editorAccountID := createServiceAccountWithRole(t, "Editor")
 	editorTokenID, editorToken := createServiceToken(t, editorAccountID)
@@ -285,19 +441,19 @@ func TestServiceAccountPermissions(t *testing.T) {
 	}{
 		{name: "settings", url: "/v1/Settings/Get", method: "POST", userCase: []userCase{
 			{userType: "default", statusCode: 401},
-			{userType: "viewer", serviceToken: "", statusCode: 401},
+			{userType: "viewer", serviceToken: viewerToken, statusCode: 401},
 			{userType: "editor", serviceToken: editorToken, statusCode: 401},
 			{userType: "admin", serviceToken: adminToken, statusCode: 200},
 		}},
 		{name: "alerts-default", url: "/alertmanager/api/v2/alerts", method: http.MethodGet, userCase: []userCase{
 			{userType: "default", statusCode: 401},
-			{userType: "viewer", serviceToken: "", statusCode: 401},
+			{userType: "viewer", serviceToken: viewerToken, statusCode: 401},
 			{userType: "editor", serviceToken: editorToken, statusCode: 401},
 			{userType: "admin", serviceToken: adminToken, statusCode: 200},
 		}},
 		{name: "platform-connect", url: "/v1/Platform/Connect", method: "POST", userCase: []userCase{
 			{userType: "default", statusCode: 401},
-			{userType: "viewer", serviceToken: "", statusCode: 401},
+			{userType: "viewer", serviceToken: viewerToken, statusCode: 401},
 			{userType: "editor", serviceToken: editorToken, statusCode: 401},
 			{userType: "admin", serviceToken: adminToken, statusCode: 400}, // We send bad request, but have access to endpoint
 		}},
@@ -319,6 +475,22 @@ func TestServiceAccountPermissions(t *testing.T) {
 					require.NoError(t, err)
 
 					req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", user.serviceToken))
+
+					resp, err := http.DefaultClient.Do(req)
+					require.NoError(t, err)
+					defer resp.Body.Close() //nolint:gosec,errcheck
+
+					assert.Equal(t, user.statusCode, resp.StatusCode)
+				})
+
+				t.Run(fmt.Sprintf("Basic auth with Service Token %s", user.userType), func(t *testing.T) {
+					u, err := url.Parse(pmmapitests.BaseURL.String())
+					require.NoError(t, err)
+					u.User = url.UserPassword("service_token", user.serviceToken)
+					u.Path = test.url
+
+					req, err := http.NewRequestWithContext(pmmapitests.Context, test.method, u.String(), nil)
+					require.NoError(t, err)
 
 					resp, err := http.DefaultClient.Do(req)
 					require.NoError(t, err)
