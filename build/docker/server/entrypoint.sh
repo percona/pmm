76aab276ec54933a4de004e30ac99ce02b7f1090
Alex Demidoff <alexander.demidoff@percona.com>
2023-12-06 23:21:37 +0000

    PMM-12692 update the entrypoint

--- build/docker/server/entrypoint.sh
+++ build/docker/server/entrypoint.sh
@@ -16,11 +16,13 @@ if [ ! -f $DIST_FILE ]; then
     echo "Generating self-signed certificates for nginx"
     bash /var/lib/cloud/scripts/per-boot/generate-ssl-certificate
     echo "Initializing Postgres"
-    /usr/pgsql-14/bin/initdb -D /srv/postgres14 --auth=trust --username=postgres --pwfile=<(echo -n)
+    su pmm -c "/usr/pgsql-14/bin/initdb -D /srv/postgres14 --auth=trust --username=postgres"
     echo "Enable pg_stat_statements extension"
-    /usr/pgsql-14/bin/pg_ctl start -D /srv/postgres14 -o "-c logging_collector=off"
-    /usr/bin/psql postgres postgres -c 'CREATE EXTENSION pg_stat_statements SCHEMA public'
-    /usr/pgsql-14/bin/pg_ctl stop -D /srv/postgres14
+    su pmm -c "/usr/pgsql-14/bin/pg_ctl start -D /srv/postgres14 -o '-c logging_collector=off'"
+    # We create the postgres user with superuser privileges to not break the code that connects pmm-managed to postgres.
+    su pmm -c "/usr/pgsql-14/bin/createuser --echo --superuser --host=/run/postgresql --no-password postgres"
+    su pmm -c "/usr/bin/psql postgres postgres -c 'CREATE EXTENSION pg_stat_statements SCHEMA public'"
+    su pmm -c "/usr/pgsql-14/bin/pg_ctl stop -D /srv/postgres14"
 fi
 
 # pmm-managed-init validates environment variables.
