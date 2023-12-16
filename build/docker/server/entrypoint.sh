7767b103ce8e3821e8a53dd84f67a168cc180a01
Alex Demidoff <alexander.demidoff@percona.com>
2023-12-16 19:50:17 +0000

    PMM-12530 optimize entrypoint and dir creation tasks

--- build/docker/server/entrypoint.sh
+++ build/docker/server/entrypoint.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 if [ ! -w /srv ]; then
-    echo "Error: /srv is not writable by $(whoami)." >&2
+    echo "Error: /srv is not writable by $(whoami) user." >&2
     echo "Please make sure that /srv is owned by uid $(id -u) and gid $(id -g) and try again." >&2
     echo "You can change ownership by running: sudo chown -R $(id -u):$(id -g) /srv" >&2
     exit 1
@@ -12,27 +12,24 @@ fi
 DIST_FILE=/srv/pmm-distribution
 if [ ! -f $DIST_FILE ]; then
     echo "File $DIST_FILE doesn't exist. Initializing /srv..."
-    su pmm
-      # Run the commands below as pmm user
-      echo docker > $DIST_FILE
-      mkdir -p /srv/{backup,clickhouse,grafana,logs,nginx,postgres14,prometheus,victoriametrics}
-      echo "Copying plugins and VERSION file"
-      cp /usr/share/percona-dashboards/VERSION /srv/grafana/PERCONA_DASHBOARDS_VERSION
-      cp -r /usr/share/percona-dashboards/panels/* /srv/grafana/plugins
-      
-      echo "Generating self-signed certificates for nginx..."
-      bash /var/lib/cloud/scripts/per-boot/generate-ssl-certificate
-      
-      echo "Initializing Postgres..."
-      /usr/pgsql-14/bin/initdb -D /srv/postgres14 --auth=trust --username=postgres
-      
-      echo "Enabling pg_stat_statements extension for PostgreSQL..."
-      /usr/pgsql-14/bin/pg_ctl start -D /srv/postgres14 -o '-c logging_collector=off'
-      # We create the postgres user with superuser privileges to not break the code that connects pmm-managed to postgres.
-      /usr/pgsql-14/bin/createuser --echo --superuser --host=/run/postgresql --no-password postgres
-      /usr/bin/psql postgres postgres -c 'CREATE EXTENSION pg_stat_statements SCHEMA public'
-      /usr/pgsql-14/bin/pg_ctl stop -D /srv/postgres14
-    exit
+    echo docker > $DIST_FILE
+    mkdir -p /srv/{backup,clickhouse,grafana,logs,nginx,postgres14,prometheus,victoriametrics}
+    echo "Copying grafana plugins and the VERSION file..."
+    cp -r /usr/share/percona-dashboards/panels/* /srv/grafana/plugins
+    cp /usr/share/percona-dashboards/VERSION /srv/grafana/PERCONA_DASHBOARDS_VERSION
+    
+    echo "Generating self-signed certificates for nginx..."
+    bash /var/lib/cloud/scripts/per-boot/generate-ssl-certificate
+    
+    echo "Initializing Postgres..."
+    /usr/pgsql-14/bin/initdb -D /srv/postgres14 --auth=trust --username=postgres
+    
+    echo "Enabling pg_stat_statements extension for PostgreSQL..."
+    /usr/pgsql-14/bin/pg_ctl start -D /srv/postgres14 -o '-c logging_collector=off'
+    # We create the postgres user with superuser privileges to not break the code that connects pmm-managed to postgres.
+    /usr/pgsql-14/bin/createuser --echo --superuser --host=/run/postgresql --no-password postgres
+    /usr/bin/psql postgres postgres -c 'CREATE EXTENSION pg_stat_statements SCHEMA public'
+    /usr/pgsql-14/bin/pg_ctl stop -D /srv/postgres14
 fi
 
 # pmm-managed-init validates environment variables.
