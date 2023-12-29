b6637c8406db0a41d63d0b5c2917d56a5a3283cb
Artem Gavrilov <artem.gavrilov@percona.com>
2023-12-29 19:02:26 +0200

    PMM-12678 Mark some fileds as reserved

--- api/management/v1/json/client/mongo_db_service/add_mongo_db_responses.go
+++ api/management/v1/json/client/mongo_db_service/add_mongo_db_responses.go
@@ -173,9 +173,6 @@ type AddMongoDBBody struct {
 	CustomLabels map[string]string `json:"custom_labels,omitempty"`
 
 	// Skip connection check.
-	//
-	// Disable query examples.
-	//  bool disable_query_examples = 16; TODO https://jira.percona.com/browse/PMM-4650
 	SkipConnectionCheck bool `json:"skip_connection_check,omitempty"`
 
 	// Use TLS for database connections.
@@ -998,9 +995,6 @@ type AddMongoDBOKBodyQANMongodbProfiler struct {
 	TLSSkipVerify bool `json:"tls_skip_verify,omitempty"`
 
 	// Limit query length in QAN (default: server-defined; -1: no limit).
-	//
-	// True if query examples are disabled.
-	//  bool query_examples_disabled = 8; TODO https://jira.percona.com/browse/PMM-4650
 	MaxQueryLength int32 `json:"max_query_length,omitempty"`
 
 	// Custom user-assigned labels.
