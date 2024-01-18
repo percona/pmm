0da2acca77a4b0b45bad09fca84cccaf19d60c62
vishwas-sharma2480 <vishwas.sharma2480@gmail.com>
2024-01-18 06:28:37 +0530

    corrected the make format error ./agent/agents/mysql/slowlog/slowlog.go:723:30: missing ',' in argument list

--- agent/agents/mysql/slowlog/slowlog.go
+++ agent/agents/mysql/slowlog/slowlog.go
@@ -715,12 +715,12 @@ func errListsToMap(k, v []uint64) map[uint64]uint64 {
 }
 
 // Describe implements prometheus.Collector.
-func (s *SlowLog) Describe(ch chan<- *prometheus.Desc) {{//nolint:revive
+func (s *SlowLog) Describe(ch chan<- *prometheus.Desc) { //nolint:revive
 	// This method is needed to satisfy interface.
 }
 
 // Collect implement prometheus.Collector.
-func (s *SlowLog) Collect(ch chan<- prometheus.Metric) {//nolint:revive
+func (s *SlowLog) Collect(ch chan<- prometheus.Metric) { //nolint:revive
 	// This method is needed to satisfy interface.
 }
 
