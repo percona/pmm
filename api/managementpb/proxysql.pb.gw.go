d0c40a218c5671a4ed3c2fce44d5b831662ee1a7
Alex Demidoff <alexander.demidoff@percona.com>
2024-01-25 16:42:42 +0300

    chore: run `make gen`

--- api/managementpb/proxysql.pb.gw.go
+++ api/managementpb/proxysql.pb.gw.go
@@ -37,11 +37,7 @@ func request_ProxySQL_AddProxySQL_0(ctx context.Context, marshaler runtime.Marsh
 	var protoReq AddProxySQLRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -53,11 +49,7 @@ func local_request_ProxySQL_AddProxySQL_0(ctx context.Context, marshaler runtime
 	var protoReq AddProxySQLRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
