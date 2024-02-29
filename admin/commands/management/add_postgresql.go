c8f9a7c86996117355549a3d9cd4a3620c17a463
Artem Gavrilov <artem.gavrilov@percona.com>
2024-02-29 18:24:42 +0200

    PMM-12894 Fix parameter description

--- admin/commands/management/add_postgresql.go
+++ admin/commands/management/add_postgresql.go
@@ -79,7 +79,7 @@ type AddPostgreSQLCommand struct {
 	DisableCollectors      []string          `help:"Comma-separated list of collector names to exclude from exporter"`
 	ExposeExporter         bool              `name:"expose-exporter" help:"Optionally expose the address of the exporter publicly on 0.0.0.0"`
 	AutoDiscoveryLimit     int32             `placeholder:"NUMBER" help:"Auto-discovery will be disabled if there are more than that number of databases (default: server-defined, -1: always disabled)"`
-	MaxExporterConnections int32             `placeholder:"NUMBER" help:"Maximum number of connections to the exporter (default: server-defined)"`
+	MaxExporterConnections int32             `placeholder:"NUMBER" help:"Maximum number of connections to PostgreSQL instance that exporter can use (default: server-defined)"`
 
 	AddCommonFlags
 	AddLogLevelNoFatalFlags
