e940c130d74da1140d255542f4c2810415157b05
Artem Gavrilov <artem.gavrilov@percona.com>
2024-03-07 21:00:05 +0200

    PMM-12895 Fix linter

--- managed/services/victoriametrics/prometheus.go
+++ managed/services/victoriametrics/prometheus.go
@@ -28,7 +28,7 @@ import (
 
 // AddScrapeConfigs - adds agents scrape configuration to given scrape config,
 // pmm_agent_id and push_metrics used for filtering.
-func AddScrapeConfigs(l *logrus.Entry, cfg *config.Config, q *reform.Querier, //nolint:cyclop
+func AddScrapeConfigs(l *logrus.Entry, cfg *config.Config, q *reform.Querier, //nolint:cyclop,maintidx
 	globalResolutions *models.MetricsResolutions, pmmAgentID *string, pushMetrics bool,
 ) error {
 	agents, err := models.FindAgentsForScrapeConfig(q, pmmAgentID, pushMetrics)
