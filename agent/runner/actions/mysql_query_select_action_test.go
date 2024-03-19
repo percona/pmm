477790bf9e361fb3287e62c59872ad999fd5e154
Artem Gavrilov <artem.gavrilov@percona.com>
2024-03-19 19:08:57 +0200

    PMM-12896 Fix tests

--- agent/runner/actions/mysql_query_select_action_test.go
+++ agent/runner/actions/mysql_query_select_action_test.go
@@ -93,7 +93,7 @@ func TestMySQLQuerySelect(t *testing.T) {
 		defer cancel()
 
 		b, err := a.Run(ctx)
-		expected := "Error 1064 \\(42000\\): You have an error in your SQL syntax; check the manual that corresponds " +
+		expected := "Error 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds " +
 			"to your (MySQL|MariaDB) server version for the right syntax to use near 'DROP TABLE city; --' at line 1"
 		require.Error(t, err)
 		assert.ErrorContains(t, err, expected)
