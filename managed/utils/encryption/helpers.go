00dae9c25a34960bd3f02ef9b61d63acf55c34ae
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2024-06-24 11:05:48 +0200

    PMM-13129 Correct mode for cert file.

--- managed/utils/encryption/helpers.go
+++ managed/utils/encryption/helpers.go
@@ -69,5 +69,5 @@ func (e *Encryption) generateKey() error {
 }
 
 func (e *Encryption) saveKeyToFile() error {
-	return os.WriteFile(e.Path, []byte(e.Key), 0o777)
+	return os.WriteFile(e.Path, []byte(e.Key), 0o644)
 }
