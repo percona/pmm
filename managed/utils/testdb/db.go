d0a454ce07c3579433124744e83a03eb03807a7f
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2024-07-02 18:28:58 +0200

    PMM-13129 Encrypt only basic fields in tests (migration).

--- managed/utils/testdb/db.go
+++ managed/utils/testdb/db.go
@@ -90,13 +90,6 @@ func SetupDB(tb testing.TB, db *sql.DB, setupFixtures models.SetupFixturesMode, 
 			Columns: []encryption.Column{
 				{Name: "username"},
 				{Name: "password"},
-				{Name: "agent_password"},
-				{Name: "aws_access_key"},
-				{Name: "aws_secret_key "},
-				{Name: "mysql_options", CustomHandler: models.EncryptMySQLOptionsHandler},
-				{Name: "postgresql_options", CustomHandler: models.EncryptPostgreSQLOptionsHandler},
-				{Name: "mongo_db_tls_options", CustomHandler: models.EncryptMongoDBOptionsHandler},
-				{Name: "azure_options", CustomHandler: models.EncryptAzureOptionsHandler},
 			},
 		},
 	}
