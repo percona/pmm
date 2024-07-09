9fd89821fbef0d233674b20dbcde913f98027c8c
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2024-07-09 09:32:10 +0200

    Revert "PMM-13129 Custom labels."
    This reverts commit 903b4ef4aa0ff84edde087ebf60d80b42ee62b1a.

--- managed/models/agent_helpers.go
+++ managed/models/agent_helpers.go
@@ -645,7 +645,7 @@ func CreateNodeExporter(q *reform.Querier,
 	}
 	encryptedAgent := EncryptAgent(*row)
 
-	if err := row.SetCustomLabels(customLabels); err != nil {
+	if err := encryptedAgent.SetCustomLabels(customLabels); err != nil {
 		return nil, err
 	}
 	if err := q.Insert(&encryptedAgent); err != nil {
@@ -731,7 +731,7 @@ func CreateExternalExporter(q *reform.Querier, params *CreateExternalExporterPar
 	}
 	encryptedAgent := EncryptAgent(*row)
 
-	if err := row.SetCustomLabels(params.CustomLabels); err != nil {
+	if err := encryptedAgent.SetCustomLabels(params.CustomLabels); err != nil {
 		return nil, err
 	}
 	if err := q.Insert(&encryptedAgent); err != nil {
@@ -923,7 +923,7 @@ func CreateAgent(q *reform.Querier, agentType AgentType, params *CreateAgentPara
 	}
 	encryptedAgent := EncryptAgent(*row)
 
-	if err := row.SetCustomLabels(params.CustomLabels); err != nil {
+	if err := encryptedAgent.SetCustomLabels(params.CustomLabels); err != nil {
 		return nil, err
 	}
 	if err := q.Insert(&encryptedAgent); err != nil {
