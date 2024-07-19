be6ce222a9b5ecd51318663be4f9bc6363d06b5d
Alex Demidoff <alexander.demidoff@percona.com>
2024-07-19 18:38:24 +0000

    PMM-13248 regenerate the code

--- api/managementpb/dbaas/psmdb_clusters.pb.gw.go
+++ api/managementpb/dbaas/psmdb_clusters.pb.gw.go
@@ -236,21 +236,21 @@ func RegisterPSMDBClustersHandlerServer(ctx context.Context, mux *runtime.ServeM
 // RegisterPSMDBClustersHandlerFromEndpoint is same as RegisterPSMDBClustersHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterPSMDBClustersHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.DialContext(ctx, endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
