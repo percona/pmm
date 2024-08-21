f8241f70c7aee6f4bd6ce943de619072546501f0
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2024-08-21 15:25:02 +0200

    PMM-12930 Fix font loading

--- ui/src/themes/PmmTheme.tsx
+++ ui/src/themes/PmmTheme.tsx
@@ -3,6 +3,15 @@ import { ThemeOptions } from '@mui/material/styles';
 import { deepmerge } from '@mui/utils';
 import { baseThemeOptions } from '@percona/design';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+
 import '@fontsource/roboto-mono';
 
 const perconaThemeOptions = (mode: PaletteMode): ThemeOptions => {
