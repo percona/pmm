b178526bfbfdbd51f2735fd6ffec047e347baf36
Artem Gavrilov <artem.gavrilov@percona.com>
2024-08-22 17:30:12 +0200

    PMM-13154 Update PG tests

--- agent/runner/actions/postgresql_query_show_action_test.go
+++ agent/runner/actions/postgresql_query_show_action_test.go
@@ -49,13 +49,13 @@ func TestPostgreSQLQueryShow(t *testing.T) {
 		b, err := a.Run(ctx)
 		require.NoError(t, err)
 		assert.LessOrEqual(t, 22000, len(b))
-		assert.LessOrEqual(t, len(b), 33668)
+		assert.LessOrEqual(t, len(b), 36379)
 
 		data, err := agentpb.UnmarshalActionQueryResult(b)
 		require.NoError(t, err)
 		t.Log(spew.Sdump(data))
 		assert.LessOrEqual(t, 200, len(data))
-		assert.LessOrEqual(t, len(data), 369)
+		assert.LessOrEqual(t, len(data), 384)
 
 		var found int
 		for _, m := range data {
