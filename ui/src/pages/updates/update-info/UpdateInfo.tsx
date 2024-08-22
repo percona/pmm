5b43f9c22b36f5d7ed3aef99f421916d01f344d6
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2024-08-22 06:29:35 +0200

    PMM-12930 Adjust section spacing

--- ui/src/pages/updates/update-info/UpdateInfo.tsx
+++ ui/src/pages/updates/update-info/UpdateInfo.tsx
@@ -5,26 +5,30 @@ import { PMM_DOCS_UPDATES_URL } from 'constants';
 
 export const UpdateInfo: FC = () => (
   <Stack
-    spacing={1}
+    spacing={3}
     sx={{
       mt: 3,
     }}
   >
-    <Typography variant="h5">{Messages.title}</Typography>
-    <Typography>
-      {Messages.upgrading}
-      <br />
-      <Link
-        href={PMM_DOCS_UPDATES_URL}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {Messages.readMore}
-      </Link>
-    </Typography>
-    <Typography variant="sectionHeading" textTransform="uppercase">
-      {Messages.whatsNext}
-    </Typography>
-    <Typography>{Messages.afterCompleting}</Typography>
+    <Stack spacing={1}>
+      <Typography variant="h5">{Messages.title}</Typography>
+      <Typography>
+        {Messages.upgrading}
+        <br />
+        <Link
+          href={PMM_DOCS_UPDATES_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {Messages.readMore}
+        </Link>
+      </Typography>
+    </Stack>
+    <Stack spacing={1}>
+      <Typography variant="sectionHeading" textTransform="uppercase">
+        {Messages.whatsNext}
+      </Typography>
+      <Typography>{Messages.afterCompleting}</Typography>
+    </Stack>
   </Stack>
 );
