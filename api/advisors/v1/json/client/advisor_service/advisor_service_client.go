8e92466796eff1c904e179265e715eea846ed7a5
Alex Demidoff <alexander.demidoff@percona.com>
2024-11-05 11:10:45 +0300

    PMM-12985 Apply v3 API infra modifications and update docs (#3207)
    * PMM-12985 Apply API infra modifications
    
    * PMM-12985 Apply API infra modifications
    
    * PMM-12985 add --version flag to spec sync
    
    * PMM-12985 change the slug name
    
    * PMM-12985 migrate from IDs to slugs
    
    * PMM-12985 update the intro with links
    
    * PMM-12985 update inventory overview
    
    * Update docs/api/inventory/overview.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update docs/api/inventory/overview.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update api/server/v1/server.proto
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update docs/api/access-control/overview.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update docs/api/advisor-api/advisors-and-advisor-checks.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update docs/api/advisor-api/overview.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update docs/api/advisor-api/overview.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update docs/api/inventory/overview.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * Update docs/api/advisor-api/overview.md
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    
    * PMM-12985 Regenerate.
    
    ---------
    
    Co-authored-by: Catalina A <94133018+catalinaadam@users.noreply.github.com>
    Co-authored-by: Nurlan Moldomurov <bupychuk1989@gmail.com>

--- api/advisors/v1/json/client/advisor_service/advisor_service_client.go
+++ api/advisors/v1/json/client/advisor_service/advisor_service_client.go
@@ -109,7 +109,7 @@ func (a *Client) ChangeAdvisorChecks(params *ChangeAdvisorChecksParams, opts ...
 }
 
 /*
-GetFailedChecks gets failed checks
+GetFailedChecks gets failed advisor checks
 
 Returns the latest check results for a given service.
 */
