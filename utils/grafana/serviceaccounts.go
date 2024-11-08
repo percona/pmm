84c8b60faa42d0b01ee44e178fcc938eed4c7cef
Jiří Čtvrtka <62988319+JiriCtvrtka@users.noreply.github.com>
2024-11-08 10:51:48 +0100

    PMM-13315 Service account max length. (#3221)
    * PMM-13315 Remove comment about SA max length.
    
    * PMM-13315 Long postfix for tests.
    
    * trigger
    
    * Revert "PMM-13315 Remove comment about SA max length."
    
    This reverts commit d812c2a4326eb8967f46338e99d4401b13b210d6.
    
    * PMM-13315 Long SA names hashing.
    
    * PMM-13315 Lint.
    
    * PMM-11315 Changes, refactor.
    
    * PMM-13315 Format.
    
    * PMM-13315 Remove forgotten prints.
    
    * PMM-13315 Remove another print.
    
    * PMM-13315 Fix problem with big orgIDs.
    
    * PMM-13315 Move SanitizeSAName to global utils.
    
    * PMM-13315 Headers.
    
    * PMM-13315 Licence year.
    
    * PMM-13315 Lint.

---
+++ utils/grafana/serviceaccounts.go
@@ -0,0 +1,37 @@
+// Copyright (C) 2023 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// Package grafana provides util functions related to grafana functionality.
+package grafana
+
+import (
+	"crypto/md5" //nolint:gosec
+	"fmt"
+)
+
+// SanitizeSAName is used for sanitize name and it's length for service accounts.
+// Max length of service account name is 190 chars (limit in Grafana Postgres DB).
+// However, prefix added by grafana is counted too. Prefix is sa-{orgID}-.
+// Bare minimum is 5 chars reserved (orgID is <10, like sa-1-) and could be more depends
+// on orgID number. Let's reserve 10 chars. It will cover almost one million orgIDs.
+// Sanitizing, ensure its length by hashing postfix when length is exceeded.
+// MD5 is used because it has fixed length 32 chars.
+func SanitizeSAName(name string) string {
+	if len(name) <= 180 {
+		return name
+	}
+
+	return fmt.Sprintf("%s%x", name[:148], md5.Sum([]byte(name[148:]))) //nolint:gosec
+}
