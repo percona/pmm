68d7b9fae01c49db5a1c6da74e489d1cda82b305
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2024-12-04 19:48:45 +0100

    PMM-5086-12634 Another MySQL fix.

--- managed/models/agent_helpers.go
+++ managed/models/agent_helpers.go
@@ -52,7 +52,7 @@ func MySQLOptionsFromRequest(params MySQLOptionsParams) *MySQLOptions {
 			TLSKey:  params.GetTlsKey(),
 		}
 	}
-	return nil
+	return &MySQLOptions{}
 }
 
 // PostgreSQLOptionsParams contains methods to create PostgreSQLOptions object.
