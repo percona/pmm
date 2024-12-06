581341ce8dd71471dde772e4fd7cc5f7d538a5ce
Alex Demidoff <alexander.demidoff@percona.com>
2024-12-06 18:58:21 +0300

    PMM-13487 Fix indentation

--- build/local/build.sh
+++ build/local/build.sh
@@ -203,28 +203,28 @@ update() {
   fi
 
   cat <<-'EOF' > entrypoint.sh
-    #!/bin/bash -e
+  #!/bin/bash -e
 
-    git config --global --add safe.directory /app
+  git config --global --add safe.directory /app
 
-    # Join the dependencies from gitmodules.yml and ci.yml
-    rm -f gitmodules.yml
-    python ci.py --convert
+  # Join the dependencies from gitmodules.yml and ci.yml
+  rm -f gitmodules.yml
+  python ci.py --convert
 
-    DEPS=$(yq -o=json eval-all '. as $item ireduce ({}; . *d $item )' gitmodules.yml ci.yml | jq '.deps')
-    DEPS=$(echo "$DEPS" | jq -r '[.[] | {name: .name, branch: .branch, path: .path, url: .url}]')
-    rm -f gitmodules.yml
-    echo "$DEPS" > /app/build/build.json
-    echo -n > /tmp/deps.txt
+  DEPS=$(yq -o=json eval-all '. as $item ireduce ({}; . *d $item )' gitmodules.yml ci.yml | jq '.deps')
+  DEPS=$(echo "$DEPS" | jq -r '[.[] | {name: .name, branch: .branch, path: .path, url: .url}]')
+  rm -f gitmodules.yml
+  echo "$DEPS" > /app/build/build.json
+  echo -n > /tmp/deps.txt
 
-    echo "$DEPS" | jq -c '.[]' | while read -r item; do
-      branch=$(echo "$item" | jq -r '.branch')
-      path=$(echo "$item" | jq -r '.path')
-      name=$(echo "$item" | jq -r '.name')
-      echo "name=${name}:path=${path}:branch=${branch}" >> /tmp/deps.txt
-    done
+  echo "$DEPS" | jq -c '.[]' | while read -r item; do
+    branch=$(echo "$item" | jq -r '.branch')
+    path=$(echo "$item" | jq -r '.path')
+    name=$(echo "$item" | jq -r '.name')
+    echo "name=${name}:path=${path}:branch=${branch}" >> /tmp/deps.txt
+  done
 
-    mv -f /tmp/deps.txt /app/build/deps.txt
+  mv -f /tmp/deps.txt /app/build/deps.txt
 EOF
 
   chmod +x "$CURDIR/entrypoint.sh"
