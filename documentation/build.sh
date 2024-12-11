6d33cf4ed40911790de6de0996a33b93810fd69a
Alex Demidoff <alexander.demidoff@percona.com>
2024-12-11 23:57:46 +0300

    PMM-13597 Remove unused images

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,5 +1,5 @@
 # This script is used for building a themed site to preview on render.com
-# Preview URL: https://pmm-doc.onrender.com
+# Preview URL: https://pmm-doc-v3.onrender.com
 
 # python -m pip install --upgrade pip
 # pip install wheel
