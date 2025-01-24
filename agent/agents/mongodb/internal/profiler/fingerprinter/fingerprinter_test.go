9707bb08198604fd9cb045eb4f906bf549ba0d5d
Nurlan Moldomurov <bupychuk1989@gmail.com>
2025-01-24 18:06:07 +0300

    PMM-13374 fix linter and tests.

--- agent/agents/mongodb/internal/profiler/fingerprinter/fingerprinter_test.go
+++ agent/agents/mongodb/internal/profiler/fingerprinter/fingerprinter_test.go
@@ -1,3 +1,17 @@
+// Copyright (C) 2023 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package fingerprinter
 
 import (
@@ -125,17 +139,17 @@ func TestProfilerFingerprinter(t *testing.T) {
 			bson.D{
 				{
 					Key: "$collStats", Value: bson.M{
-						// TODO: PMM-9568 : Add support to handle histogram metrics
-						"latencyStats": bson.M{"histograms": false},
-						"storageStats": bson.M{"scale": 1},
-					},
+					// TODO: PMM-9568 : Add support to handle histogram metrics
+					"latencyStats": bson.M{"histograms": false},
+					"storageStats": bson.M{"scale": 1},
+				},
 				},
 			}, bson.D{
 				{
 					Key: "$project", Value: bson.M{
-						"storageStats.wiredTiger":   0,
-						"storageStats.indexDetails": 0,
-					},
+					"storageStats.wiredTiger":   0,
+					"storageStats.indexDetails": 0,
+				},
 				},
 			},
 		})
@@ -180,11 +194,17 @@ func TestProfilerFingerprinter(t *testing.T) {
 			`db.runCommand({"findAndModify":"test","query":{"id":"?"},"remove":true})`,
 			`db.secondcollection.find({"name":"?"}).sort({"id":-1}).limit(?)`,
 			`db.test.aggregate([{"$match":{"id":"?","time":{"$gt":"?"}}}, {"$group":{"_id":"$id","count":{"$sum":1}}}, {"$sort":{"_id":1}}])`,
+			`db.test.aggregate([{"$match":{"id":"?","time":{"$gt":"?"}}}, {"$group":{"count":{"$sum":1},"_id":"$id"}}, {"$sort":{"_id":1}}])`,
 			`db.secondcollection.aggregate([{"$collStats":{"latencyStats":{"histograms":false},"storageStats":{"scale":1}}}, {"$project":{"storageStats.wiredTiger":0,"storageStats.indexDetails":0}}])`,
+			`db.secondcollection.aggregate([{"$collStats":{"latencyStats":{"histograms":false},"storageStats":{"scale":1}}}, {"$project":{"storageStats.indexDetails":0,"storageStats.wiredTiger":0}}])`,
+			`db.secondcollection.aggregate([{"$collStats":{"storageStats":{"scale":1},"latencyStats":{"histograms":false}}}, {"$project":{"storageStats.wiredTiger":0,"storageStats.indexDetails":0}}])`,
+			`db.secondcollection.aggregate([{"$collStats":{"storageStats":{"scale":1},"latencyStats":{"histograms":false}}}, {"$project":{"storageStats.indexDetails":0,"storageStats.wiredTiger":0}}])`,
 			`db.secondcollection.deleteOne({"id":"?"})`,
 			`db.test.deleteMany({"name":"?"})`,
 		}
-		assert.EqualValues(t, expectedFingerprints, fingerprints)
+		for i, fingerprint := range fingerprints {
+			assert.Contains(t, expectedFingerprints, fingerprint, "fingerprint %d: %s", i, fingerprint)
+		}
 	})
 
 	type testCase struct {
