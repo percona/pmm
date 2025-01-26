c70d09fca5951ee47d97cf3aac1506822b51e1ba
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-01-26 22:46:39 +0100

    PMM-12468 Format.

--- agent/agents/mongodb/internal/profiler/aggregator/extended.go
+++ agent/agents/mongodb/internal/profiler/aggregator/extended.go
@@ -22,10 +22,10 @@ import (
 	"sync"
 
 	mathStats "github.com/montanaflynn/stats"
-	"go.mongodb.org/mongo-driver/bson"
-
 	"github.com/percona/percona-toolkit/src/go/mongolib/proto"
 	"github.com/percona/percona-toolkit/src/go/mongolib/stats"
+	"go.mongodb.org/mongo-driver/bson"
+
 	"github.com/percona/pmm/agent/agents/mongodb/internal/profiler/collector"
 )
 
