bae521bcc9a8d7db8ed4b04ce2dabe32cbc8327c
Alex Demidoff <alexander.demidoff@percona.com>
2025-03-12 22:54:45 +0300

    PMM-12153 bump up the version for testing

--- managed/services/agents/mysql.go
+++ managed/services/agents/mysql.go
@@ -35,7 +35,7 @@ import (
 var (
 	mysqlExporterVersionWithPluginCollector = version.MustParse("2.36.0-0")
 	// TODO: put back 3.2.0 when 3.1.0 is released.
-	v3_2_0 = version.MustParse("3.0.0-0")
+	v3_2_0 = version.MustParse("3.1.0-0")
 )
 
 // mysqldExporterConfig returns desired configuration of mysqld_exporter process.
