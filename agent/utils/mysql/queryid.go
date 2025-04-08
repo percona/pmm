d41ad51100db1f7aef55a28ca7129b3e895d280e
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-04-08 16:38:32 +0200

    PMM-12413 Package comment.

--- agent/utils/mysql/queryid.go
+++ agent/utils/mysql/queryid.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package mysql contains shared and helpers functions for mysql agent.
 package mysql
 
 import "fmt"
