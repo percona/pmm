418d39148cb9cf566f3654849d090a51b8278214
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-04-24 15:29:43 +0200

    PMM-13722 Improve setup for turborepo

--- ui/apps/pmm-compat/webpack.config.ts
+++ ui/apps/pmm-compat/webpack.config.ts
@@ -1,11 +1,19 @@
 import type { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
 import grafanaConfig from './.config/webpack/webpack.config';
+import path from 'path';
 
 const config = async (env): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
 
-  return merge(baseConfig, {});
+  return merge(baseConfig, {
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        '@pmm/shared': path.resolve(__dirname, '../../packages/shared/src'),
+      },
+    },
+  });
 };
 
 export default config;
