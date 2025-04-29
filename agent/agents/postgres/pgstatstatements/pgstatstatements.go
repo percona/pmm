396fa95efa68a73a883cfe8e70275ac3a5e68661
Michael Okoko <okokomichaels@outlook.com>
2025-04-29 11:29:29 +0100

    include test for pgss.max value

--- agent/agents/postgres/pgstatstatements/pgstatstatements.go
+++ agent/agents/postgres/pgstatstatements/pgstatstatements.go
@@ -44,7 +44,7 @@ import (
 
 const (
 	retainStatStatements = 25 * time.Hour // make it work for daily queries
-	minPgSSCacheSize     = 5000           // cache size rows limit
+	defaultPgssCacheSize = 5000           // cache size rows limit
 	queryStatStatements  = time.Minute
 )
 
@@ -94,19 +94,22 @@ func New(params *Params, l *logrus.Entry) (*PGStatStatementsQAN, error) {
 	reformL := sqlmetrics.NewReform("postgres", params.AgentID, l.Tracef)
 	// TODO register reformL metrics https://jira.percona.com/browse/PMM-4087
 	q := reform.NewDB(sqlDB, postgresql.Dialect, reformL).WithTag(queryTag)
-	// read pg_stat_statements.max parameter
-	var cacheSize uint
-	err = q.QueryRow(pgssMaxQuery).Scan(&cacheSize)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get pg_stat_statements.max")
-	}
-	if cacheSize < minPgSSCacheSize {
-		cacheSize = minPgSSCacheSize
-	}
-	return newPgStatStatementsQAN(q, sqlDB, params.AgentID, cacheSize, params.MaxQueryLength, params.DisableCommentsParsing, l)
+	return newPgStatStatementsQAN(q, sqlDB, params.AgentID, params.MaxQueryLength, params.DisableCommentsParsing, l)
 }
 
-func newPgStatStatementsQAN(q *reform.Querier, dbCloser io.Closer, agentID string, cacheSize uint, maxQueryLength int32, disableCommentsParsing bool, l *logrus.Entry) (*PGStatStatementsQAN, error) { //nolint:lll
+func getPgStatStatementsCacheSize(q *reform.Querier, l *logrus.Entry) uint {
+	var pgSSCacheSize int
+	err := q.QueryRow(pgssMaxQuery).Scan(&pgSSCacheSize)
+	if err != nil {
+		l.WithError(err).Error("failed to get pg_stat_statements.max")
+		return defaultPgssCacheSize
+	}
+
+	return uint(pgSSCacheSize)
+}
+
+func newPgStatStatementsQAN(q *reform.Querier, dbCloser io.Closer, agentID string, maxQueryLength int32, disableCommentsParsing bool, l *logrus.Entry) (*PGStatStatementsQAN, error) { //nolint:lll
+	cacheSize := getPgStatStatementsCacheSize(q, l)
 	statementCache, err := newStatementsCache(statementsMap{}, retainStatStatements, cacheSize, l)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot create cache")
--- agent/agents/postgres/pgstatstatements/pgstatstatements_test.go
+++ agent/agents/postgres/pgstatstatements/pgstatstatements_test.go
@@ -36,7 +36,7 @@ import (
 	inventoryv1 "github.com/percona/pmm/api/inventory/v1"
 )
 
-func setup(t *testing.T, db *reform.DB, cacheSize uint) *PGStatStatementsQAN {
+func setup(t *testing.T, db *reform.DB) *PGStatStatementsQAN {
 	t.Helper()
 
 	selectQuery := fmt.Sprintf("SELECT /* %s */ ", queryTag)
@@ -44,7 +44,7 @@ func setup(t *testing.T, db *reform.DB, cacheSize uint) *PGStatStatementsQAN {
 	_, err := db.Exec(selectQuery + "pg_stat_statements_reset()")
 	require.NoError(t, err)
 
-	p, err := newPgStatStatementsQAN(db.WithTag(queryTag), nil, "agent_id", cacheSize, truncate.GetDefaultMaxQueryLength(), false, logrus.WithField("test", t.Name()))
+	p, err := newPgStatStatementsQAN(db.WithTag(queryTag), nil, "agent_id", truncate.GetDefaultMaxQueryLength(), false, logrus.WithField("test", t.Name()))
 	require.NoError(t, err)
 
 	return p
@@ -169,7 +169,7 @@ func TestPGStatStatementsQAN(t *testing.T) {
 	}
 
 	t.Run("AllCities", func(t *testing.T) {
-		m := setup(t, db, minPgSSCacheSize)
+		m := setup(t, db)
 
 		_, err := db.Exec(selectAllCities)
 		require.NoError(t, err)
@@ -259,7 +259,7 @@ func TestPGStatStatementsQAN(t *testing.T) {
 	})
 
 	t.Run("AllCitiesTruncated", func(t *testing.T) {
-		m := setup(t, db, minPgSSCacheSize)
+		m := setup(t, db)
 
 		const n = 500
 		placeholders := db.Placeholders(1, n)
@@ -374,7 +374,7 @@ func TestPGStatStatementsQAN(t *testing.T) {
 			_, err := db.Exec(fmt.Sprintf(`DROP TABLE %s`, tableName))
 			require.NoError(t, err)
 		}()
-		m := setup(t, db, minPgSSCacheSize)
+		m := setup(t, db)
 
 		var waitGroup sync.WaitGroup
 		n := 1000
@@ -468,65 +468,23 @@ func TestPGStatStatementsQPS(t *testing.T) {
 		return res
 	}
 
-	t.Run("check query count for low cache size", func(t *testing.T) {
-		p := setup(t, db, minPgSSCacheSize)
-		runTimes := 5000
-
-		defer func() {
-			for i := 0; i < runTimes; i++ {
-				_, err = db.Exec(fmt.Sprintf("drop table if exists t%d", i))
-				require.NoError(t, err)
-			}
-		}()
-		for i := 0; i < runTimes; i++ {
-			_, err = db.Exec(fmt.Sprintf("create /* controller='test' */ table t%d (id int);", i))
-			require.NoError(t, err)
-			_, err = db.Exec(fmt.Sprintf("insert /* controller='test' */ into t%d values(1);", i))
-			require.NoError(t, err)
-		}
-
-		buckets, err := p.getNewBuckets(context.Background(), time.Date(2019, 4, 1, 10, 59, 0, 0, time.UTC), 60)
-		require.NoError(t, err)
-		insertBuckets := filterInsertQueries(buckets)
-		mismatchedCount := 0
-		for _, b := range insertBuckets {
-			assert.Equal(t, float32(1), b.Common.NumQueries)
-			if b.Common.NumQueries != 1 {
-				mismatchedCount++
-			}
-		}
-		assert.Zero(t, mismatchedCount)
-
-		// re-run insert queries and check that there are now mismatches since the cache can't hold all the previous bucket.
-		for i := 0; i < runTimes; i++ {
-			_, err = db.Exec(fmt.Sprintf("insert /* controller='test' */ into t%d values(1);", i))
-			require.NoError(t, err)
-		}
-		buckets, err = p.getNewBuckets(context.Background(), time.Date(2019, 4, 1, 10, 59, 0, 0, time.UTC), 60)
-		require.NoError(t, err)
-		insertBuckets = filterInsertQueries(buckets)
-		mismatchedCount = 0
-		for _, b := range insertBuckets {
-			if b.Common.NumQueries != 1 {
-				mismatchedCount++
-			}
-		}
-		assert.Greater(t, mismatchedCount, 0) //nolint:testifylint // this makes what we're testing more explicit
+	t.Run("uses pgss.max value", func(t *testing.T) {
+		p := setup(t, db)
+		assert.Equal(t, uint(10000), p.statementsCache.cache.Capacity())
 	})
 
 	t.Run("check query count when cache size equals pgss.max", func(t *testing.T) {
 		var cacheSize uint
 		err = db.Querier.QueryRow(pgssMaxQuery).Scan(&cacheSize)
 		require.NoError(t, err)
-		p := setup(t, db, cacheSize)
+		p := setup(t, db)
 
 		runTimes := 5000
-		defer func() {
+		t.Cleanup(func() {
 			for i := 0; i < runTimes; i++ {
-				_, err = db.Exec(fmt.Sprintf("drop table if exists t%d", i))
-				require.NoError(t, err)
+				_, _ = db.Exec(fmt.Sprintf("drop table if exists t%d", i))
 			}
-		}()
+		})
 
 		for i := 0; i < runTimes; i++ {
 			_, err = db.Exec(fmt.Sprintf("create /* controller='test' */ table t%d (id int);", i))
