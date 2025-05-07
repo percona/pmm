5cbe58e062c4272f1598a588401e7733597137da
Michael Okoko <okokomichaels@outlook.com>
2025-05-07 16:51:27 +0100

    update license copyright year

--- managed/services/management/valkey.go
+++ managed/services/management/valkey.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Percona LLC
+// Copyright (C) 2023 Percona LLC
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
