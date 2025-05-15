2736334b4d6ccd566c7939c405588a3c7805dc63
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-05-15 15:36:35 +0200

    PMM-13722 Adjust styles after grafana upgrade

--- ui/apps/pmm-compat/src/styles.ts
+++ ui/apps/pmm-compat/src/styles.ts
@@ -3,13 +3,28 @@ export const applyCustomStyles = () => {
 
   const style = document.createElement('style');
   style.innerText = `
-                  #mega-menu-toggle,
-              header > div:first-child,
-              header > div:nth-child(2) > div:first-of-type,
-              header div[class*=NavToolbar-actions] > div:last-of-type,
-              button[title="Toggle top search bar"]
-               {
+              #mega-menu-toggle,
+              header > div:first-child > div:nth-child(2) {
                 display: none;
-              }`;
+              }
+
+              header {
+                padding: 8px;
+                flex-direction: row !important;
+                border-bottom: 1px solid rgba(36, 41, 46, 0.12)
+              }
+
+              header > div:first-child {
+                flex: 1;
+              }
+
+              header > div {
+                border-bottom: 0 !important;
+              }
+
+              [class*="canvas-wrapper"] > div {
+                top: 57px;
+              }
+              `;
   document.head.appendChild(style);
 };
