05a37b9a7d9cb762ef699f0f5b5a3459006ca563
Nurlan Moldomurov <bupychuk1989@gmail.com>
2025-05-28 14:43:09 +0300

    PMM-7 Remove unnecessary lint directive from agentsList function

--- admin/commands/list.go
+++ admin/commands/list.go
@@ -272,7 +272,7 @@ func getMetricsMode(s bool) string {
 	return "pull"
 }
 
-func agentsList(agentsRes *agents.ListAgentsOK, nodeID string) []listResultAgent { //nolint:maintidx
+func agentsList(agentsRes *agents.ListAgentsOK, nodeID string) []listResultAgent {
 	pmmAgentIDs := make(map[string]struct{})
 	agentsList := []listResultAgent{}
 
