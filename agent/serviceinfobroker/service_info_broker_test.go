afa1b4852e7b6c21c6c7ad660d4c55da95a398fe
Nurlan Moldomurov <bupychuk1989@gmail.com>
2025-06-02 13:43:33 +0300

    PMM-12575 Update copyright headers in test files to include the correct format and year for consistency across the codebase.

--- agent/serviceinfobroker/service_info_broker_test.go
+++ agent/serviceinfobroker/service_info_broker_test.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Percona LLC
+// Copyright (C) 2023 Percona LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
