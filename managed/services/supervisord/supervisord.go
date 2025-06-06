d637fd9734132349f4e9548420278a6a7d4b6a99
Nurlan Moldomurov <bupychuk1989@gmail.com>
2025-06-06 22:51:06 +0300

    PMM-14063: Remove 'user' directive from supervisord configs
    Removed the 'user' directive from multiple supervisord configuration files to streamline process settings. Adjusted file permission settings in supervisord.go for consistency and compatibility.

--- managed/services/supervisord/supervisord.go
+++ managed/services/supervisord/supervisord.go
@@ -401,7 +401,7 @@ func (s *Service) saveConfigAndReload(name string, cfg []byte) (bool, error) {
 	restore := true
 	defer func() {
 		if restore {
-			if err = os.WriteFile(path, oldCfg, 0o644); err != nil { //nolint:gosec
+			if err = os.WriteFile(path, oldCfg, 0o666); err != nil { //nolint:gosec
 				s.l.Errorf("Failed to restore: %s.", err)
 			}
 			if err = s.reload(name); err != nil {
@@ -411,7 +411,7 @@ func (s *Service) saveConfigAndReload(name string, cfg []byte) (bool, error) {
 	}()
 
 	// write and reload
-	if err = os.WriteFile(path, cfg, 0o644); err != nil { //nolint:gosec
+	if err = os.WriteFile(path, cfg, 0o666); err != nil { //nolint:gosec
 		return false, errors.WithStack(err)
 	}
 	if err = s.reload(name); err != nil {
