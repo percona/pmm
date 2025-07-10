b70b919c19e0c14d15ed74be261082329694befc
Nurlan Moldomurov <bupychuk1989@gmail.com>
2025-07-10 08:28:02 +0300

    PMM-14132 Refactor not relevant tests from dir_test.go

--- managed/utils/dir/dir_test.go
+++ managed/utils/dir/dir_test.go
@@ -27,40 +27,20 @@ func TestCreateDataDir(t *testing.T) {
 	t.Parallel()
 
 	testcases := []struct {
-		name      string
-		path      string
-		username  string
-		groupname string
-		perm      os.FileMode
-		err       string
+		name string
+		path string
+		perm os.FileMode
+		err  string
 	}{{
-		name:      "valid params",
-		path:      "/tmp/testdir_valid",
-		username:  "pmm",
-		groupname: "pmm",
-		perm:      os.FileMode(0o775),
-		err:       ``,
+		name: "valid params",
+		path: "/tmp/testdir_valid",
+		perm: os.FileMode(0o775),
+		err:  ``,
 	}, {
-		name:      "invalid path",
-		path:      "",
-		username:  "pmm",
-		groupname: "pmm",
-		perm:      os.FileMode(0o775),
-		err:       `cannot create path "": mkdir : no such file or directory`,
-	}, {
-		name:      "unknown user",
-		path:      "/tmp/testdir_user",
-		username:  "$",
-		groupname: "pmm",
-		perm:      os.FileMode(0o775),
-		err:       `user: unknown user $`,
-	}, {
-		name:      "unknown group",
-		path:      "/tmp/testdir_group",
-		username:  "pmm",
-		groupname: "$",
-		perm:      os.FileMode(0o775),
-		err:       `group: unknown group $`,
+		name: "invalid path",
+		path: "",
+		perm: os.FileMode(0o775),
+		err:  `cannot create path "": mkdir : no such file or directory`,
 	}}
 
 	for _, tc := range testcases {
