060871bf058fc3ddd337820cf2f222694664a231
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-07-24 08:59:05 +0200

    PMM-13699 Fix keyboard shortcuts triggering

--- ui/apps/pmm-compat/src/lib/utils/shortcuts.ts
+++ ui/apps/pmm-compat/src/lib/utils/shortcuts.ts
@@ -4,6 +4,7 @@ export const triggerShortcut = (shortcut: 'view-shortcuts' | 'toggle-theme' | 's
       key: 'k',
       code: 'KeyK',
       keyCode: 75,
+      which: 75,
       metaKey: true,
     });
   } else if (shortcut === 'view-shortcuts') {
@@ -11,6 +12,7 @@ export const triggerShortcut = (shortcut: 'view-shortcuts' | 'toggle-theme' | 's
       key: '?',
       code: 'Slash',
       keyCode: 191,
+      which: 191,
       shiftKey: true,
     });
   } else if (shortcut === 'toggle-theme') {
@@ -18,11 +20,13 @@ export const triggerShortcut = (shortcut: 'view-shortcuts' | 'toggle-theme' | 's
       key: 'c',
       code: 'KeyC',
       keyCode: 67,
+      which: 67,
     });
     triggerKeypress({
       key: 't',
       code: 'KeyT',
       keyCode: 84,
+      which: 84,
     });
   }
 };
