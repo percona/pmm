1c3eefccb2ad718c262287b4a8d6b3c3d6a12054
Alex Demidoff <alex@demidoff.me>
2025-08-13 00:10:10 +0300

    PMM-13157 fix nodes unit test

--- managed/services/management/rds_test.go
+++ managed/services/management/rds_test.go
@@ -383,12 +383,13 @@ func TestRDSService(t *testing.T) {
 			Service: &managementv1.AddServiceResponse_Rds{
 				Rds: &managementv1.RDSServiceResult{
 					Node: &inventoryv1.RemoteRDSNode{
-						NodeId:    "00000000-0000-4000-8000-00000000000a",
-						NodeName:  "rds-postgresql",
-						Address:   "rds-postgresql-renaming.xyzzy.us-east-1.rds.amazonaws.com",
-						NodeModel: "db.t3.micro",
-						Region:    "us-east-1",
-						Az:        "us-east-1b",
+						NodeId:     "00000000-0000-4000-8000-00000000000a",
+						NodeName:   "rds-postgresql",
+						Address:    "rds-postgresql-renaming.xyzzy.us-east-1.rds.amazonaws.com",
+						InstanceId: "rds-postgresql",
+						NodeModel:  "db.t3.micro",
+						Region:     "us-east-1",
+						Az:         "us-east-1b",
 						CustomLabels: map[string]string{
 							"foo": "bar",
 						},
