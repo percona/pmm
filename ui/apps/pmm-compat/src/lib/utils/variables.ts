0f767daf2f2df84f5d16acea6fccca4aa071108e
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2025-10-10 09:40:00 +0200

    PMM-13652 Match navitem variables with v3

--- ui/apps/pmm-compat/src/lib/utils/variables.ts
+++ ui/apps/pmm-compat/src/lib/utils/variables.ts
@@ -21,12 +21,20 @@ const getLinkUrl = (link: Partial<DashboardLink>) => {
 
 export const getLinkWithVariables = (url?: string): string => {
   if (url && isDashboardUrl(url) && isDashboardUrl(window.location.pathname)) {
-    return getLinkUrl({
+    const urlWithLinks = getLinkUrl({
       url: url,
       keepTime: true,
       // Check if the DB type matches the current one used
       includeVars: checkDbType(url),
+      asDropdown: false,
+      icon: '',
+      tags: [],
+      targetBlank: false,
+      title: '',
+      tooltip: '',
+      type: 'link',
     });
+    return cleanupVariables(urlWithLinks);
   } else {
     return url ? url : '#';
   }
@@ -36,7 +44,24 @@ const isDashboardUrl = (url?: string) => url?.includes('/d/');
 
 const checkDbType = (url: string): boolean => {
   const currentDB = window.location.pathname?.split('/')[3]?.split('-')[0];
-  const urlDB = url?.split('/')[2]?.split('-')[0];
+  const targetDB = url?.split('/')[3]?.split('-')[0];
 
-  return currentDB !== undefined && currentDB === urlDB;
+  // enable variable sharing between same db types and db type -> os/node
+  return (currentDB !== undefined && currentDB === targetDB) || targetDB === 'node';
+};
+
+const cleanupVariables = (urlWithLinks: string) => {
+  const [base, params] = urlWithLinks.split('?');
+
+  if (params) {
+    // remove variables which have the All value or the value is empty
+    const variables = params
+      .split('&')
+      .filter((param) => !(param.includes('All') || param.endsWith('=')))
+      .join('&');
+
+    return base + '?' + variables;
+  }
+
+  return base;
 };
