b875e7c50f39a04a132375f6c87cd133b970e2aa
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-10-30 10:47:24 +0100

    PMM-13971 Format.

--- managed/services/qan/client.go
+++ managed/services/qan/client.go
@@ -320,7 +320,7 @@ func (c *Client) Collect(ctx context.Context, metricsBuckets []*agentv1.MetricsB
 		c.l.Debugf("%+v", qanReq)
 		res, err := c.c.Collect(ctx, qanReq)
 		if err != nil {
-			   return fmt.Errorf("failed to send CollectRequest to QAN: %w", err)
+			return fmt.Errorf("failed to send CollectRequest to QAN: %w", err)
 		}
 		c.l.Debugf("%+v", res)
 
