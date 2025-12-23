2d4cad82b8332e26c0937fd0dfeb28f1ca543b34
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-12-23 13:06:27 +0100

    PMM-14577 Lint.

--- managed/utils/interceptors/grpc_extension.go
+++ managed/utils/interceptors/grpc_extension.go
@@ -1,4 +1,4 @@
-//nolint:all
+//nolint:file
 // Copyright (C) 2023 Percona LLC
 //
 // This program is free software: you can redistribute it and/or modify
--- qan-api2/main.go
+++ qan-api2/main.go
@@ -106,7 +106,6 @@ func runGRPCServer(ctx context.Context, db *sqlx.DB, mbm *models.MetricsBucket, 
 		l.Debug("RPC response latency histogram enabled.")
 		grpc_prometheus.EnableHandlingTimeHistogram()
 	}
-	// grpc_prometheus.Register(grpcServer) // Not needed, handled by custom extension
 
 	// run server until it is stopped gracefully or not
 	go func() {
