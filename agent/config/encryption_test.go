d951db28f2e575ce0b9a1f7ed51c3d5019afbfbe
Alex Demidoff <a@demidoff.me>
2026-01-08 22:24:43 +0300

    PMM-14643 Remove hard-coded secrets

--- agent/config/encryption_test.go
+++ agent/config/encryption_test.go
@@ -15,65 +15,64 @@
 package config
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/youmark/pkcs8"
 )
 
-const rsaKey = `
------BEGIN PRIVATE KEY-----
-MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAL7XQZoBKba6RYXp
-WsmX8kaeX4OBsNyqUrho3eh85jdEpfAYwzlKWNy3aboDre85ugwMEt5yMFOXL96r
-9KnFrp4KpEacfJJdcoAPncAIslb0anwhhXOoc9ZIgz7AyJMKUN7PQSC4VpSP/86w
-CShq5cEyRPbs4CwnQ5yYtlqCWQi1AgMBAAECgYA3EuXSrN0953mi0JorrVb0vEWy
-LN4+gETJBTJtIoZJkt0UcgD86pDEeYXgcaljbVRcn6teWLPLm8jryNIdoHfoknIB
-crf6vemmlP80Lpw2cdg46Q9lcleGTwJGOd+R2QSWJLV7kPrhhR+wIw6m7TDHvhGU
-yx9AY4GrAMSrf4wTbQJBAN+3KuNssRBRZXKBDMuCAwgr8hSTNmjivRGmmATv3MGF
-cCey/PbvQQ1jPRViSUysmFumOTE59GlcmB6TQQeUXF8CQQDaYZWIuGuAenI4pz+w
-BR8JyJs5N52/YmsxTe2XShqksiiyvaHxNJ1mvSVEorPqEwfMwwFLI+cq0rh7YwZd
-QLNrAkAV5QlPhL23iR/SmwqziB/f1t00Ykv66+XxKkrKgOcsEXEukXfsevH063d4
-9kuSM3odziDezns7LJK+u06r/TslAkEAjowiStNuwLestVRe2ywMnZtHz2qBWvsI
-U2+1xgqGJ7lvnXTxL3yTvgt7NzkpTYLMlZk4z+6Ip8hSyZ/S+K4SLwJAPO8sd5U8
-mfvGLl15b1BI4o86X4r+HC4Nwb33i4eVBM06YBYbxSKUu9E9lWYiU1wCgPA7+T75
-TF1yxL9OKxrMpQ==
------END PRIVATE KEY-----
-`
+func generateRSAKey(t *testing.T) []byte {
+	t.Helper()
 
-//nolint:gosec
-const rsaPasswordKey = `
------BEGIN ENCRYPTED PRIVATE KEY-----
-MIIC5TBfBgkqhkiG9w0BBQ0wUjAxBgkqhkiG9w0BBQwwJAQQ1vE1Ke5aqPfS+DEA
-U0f79QICCAAwDAYIKoZIhvcNAgkFADAdBglghkgBZQMEASoEEKoYunBmlc2MEbT2
-aaTRrNcEggKAKAmW5WL80flLmh+llCu4claeq5PT3DfrhUvL0UkdXDVV70aIfOMr
-C7d28usIcfbguEtyPA72oWraNpot0u8z6SxVEZ0lFz4tk4eG8II1vLuzSXsswM0q
-JKBGwbkwPVFIq21pK1vmfvWpA6gTTd7QU4YKInq1eXuoxHkdDC37ryXEUx/tEd+s
-Gd54Zs1QXh2k0jSzTaOLlUaez9ADT1Nk9pS9Fj2aUSns7xXVXKYMYxpEYkUfd7xK
-mowC0q0L/av9Xoj+EI5H0f9CdbufMpCe9GPAPShEEkV2feZsuigneAAYiIyAOMUh
-T954t9M/rsSN5jAAZ/syaC6bDblx+nL6hDUcQjLyUJah7GRGwt5xexgMtfZccS8n
-dp0DNG1gJjhK1QaP0BNBslMGlrbEoIkMn3MbmKVwnmKK/AM9kqalJV45RAogXpTG
-lMhJ2bTpgx0CfIzalVEWc3eo7E0tf+Q//OHOptCx/u/sjeO5ZScyYt4SCiRsPYbJ
-MjRQd3w3rQj5dW8f7ewDOf1xjueJ6q9sNL0f1OXgmCwq6MUbA2EpOMorETuZaATy
-pLhDt8QNTp75NfENGbR+Uk+CoezmxS6KKGEG6P7SAvXnBHY4TGPNQq+pxaWxu6PB
-+AS+K3t64XElJT99IB6rGjjpODQRQhk3IiX7hsJpG3KsTi2/sG/ZUkHJ5p7SDiK8
-/q7EECYgQoqkQ4CFXbF1+Nz0fVIkX3FfRgJtKeuxNnjsY/CXnb1PERendg+6U0sp
-e60uBuuy8TnXdkC3MIZyuc0iuZkw+XXdgZctv3bcAmkMDIcm0wNX5248N6tWcBUb
-9j2emDUrENFV+8s8F+Mkj0mpoB+lre7Tww==
------END ENCRYPTED PRIVATE KEY-----
-`
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
 
-func writeKey(t *testing.T, keyname, key string) string {
+	privateKeyBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	require.NoError(t, err)
+
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	})
+
+	return privateKeyPEM
+}
+
+func generateEncryptedRSAKey(t *testing.T, password string) []byte {
+	t.Helper()
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+
+	privateKeyDER, err := pkcs8.MarshalPrivateKey(privateKey, []byte(password), nil)
+	require.NoError(t, err)
+
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "ENCRYPTED PRIVATE KEY",
+		Bytes: privateKeyDER,
+	})
+
+	return privateKeyPEM
+}
+
+func writeKey(t *testing.T, keyname string, key []byte) string {
 	t.Helper()
 	path := filepath.Join(t.TempDir(), keyname)
-	require.NoError(t, os.WriteFile(path, []byte(key), 0o600))
+	require.NoError(t, os.WriteFile(path, key, 0o600))
 	return path
 }
 
 func TestEncryption(t *testing.T) {
 	t.Run("Encrypted", func(t *testing.T) {
-		key := writeKey(t, "key", rsaKey)
+		keyPEM := generateRSAKey(t)
+		key := writeKey(t, "key", keyPEM)
 		enc := Encryption{
 			KeyFile: key,
 		}
@@ -84,10 +83,12 @@ func TestEncryption(t *testing.T) {
 	})
 
 	t.Run("EncryptedPassword", func(t *testing.T) {
-		key := writeKey(t, "key", rsaPasswordKey)
+		password := "abcdefgh"
+		keyPEM := generateEncryptedRSAKey(t, password)
+		key := writeKey(t, "key", keyPEM)
 		enc := Encryption{
 			KeyFile:         key,
-			KeyFilePassword: "abcdefgh",
+			KeyFilePassword: password,
 		}
 		configfilef := writeConfig(t, &Config{ID: "agent-id", Encryption: enc})
 		cfg, err := loadFromFile(configfilef, &enc)
@@ -96,10 +97,12 @@ func TestEncryption(t *testing.T) {
 	})
 
 	t.Run("EncryptedWrongPassword", func(t *testing.T) {
-		key := writeKey(t, "key", rsaPasswordKey)
+		password := "abcdefgh"
+		keyPEM := generateEncryptedRSAKey(t, password)
+		key := writeKey(t, "key", keyPEM)
 		configfilef := writeConfig(t, &Config{ID: "agent-id", Encryption: Encryption{
 			KeyFile:         key,
-			KeyFilePassword: "abcdefgh",
+			KeyFilePassword: password,
 		}})
 
 		cfg, err := loadFromFile(configfilef, &Encryption{
@@ -111,15 +114,18 @@ func TestEncryption(t *testing.T) {
 	})
 
 	t.Run("EncryptedWrongKey", func(t *testing.T) {
-		key1 := writeKey(t, "key1", rsaPasswordKey)
-		key2 := writeKey(t, "key2", rsaKey)
+		password := "abcdefgh"
+		key1PEM := generateEncryptedRSAKey(t, password)
+		key2PEM := generateRSAKey(t)
+		key1 := writeKey(t, "key1", key1PEM)
+		key2 := writeKey(t, "key2", key2PEM)
 
 		configfilef := writeConfig(t, &Config{ID: "agent-id", Encryption: Encryption{
 			KeyFile: key2,
 		}})
 		cfg, err := loadFromFile(configfilef, &Encryption{
 			KeyFile:         key1,
-			KeyFilePassword: "abcdefgh",
+			KeyFilePassword: password,
 		})
 		require.EqualError(t, err, "unable to RSA-unwrap AES key: crypto/rsa: decryption error")
 		assert.Nil(t, cfg)
