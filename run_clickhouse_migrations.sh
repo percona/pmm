f674a13c7d713a3e610d0a51d200527695785a50
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2026-01-23 11:14:37 +0100

    PMM-14737 Ability to apply remaining migrations after restore.

---
+++ run_clickhouse_migrations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Usage: ./run_clickhouse_migrations.sh <start_number>
+# Runs all ClickHouse .up.sql migrations with a number greater than <start_number>
+
+set -euo pipefail
+
+start=${1:-0}
+dir="qan-api2/migrations/sql"
+
+for file in $(ls $dir | grep -E '^[0-9]+_.*\.up\.sql$' | sort); do
+    num=$(echo $file | cut -d'_' -f1)
+    if [ "$num" -gt "$start" ]; then
+        echo "Running migration: $file"
+        sql=$(sed 's/ALTER TABLE metrics/ALTER TABLE pmm.metrics/g' "$dir/$file")
+        docker exec pmm-server clickhouse-client --password=clickhouse --query="$sql"
+    fi
+done
