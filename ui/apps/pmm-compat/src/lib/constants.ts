5d334b62f88473363bb8debe3d2fb1499edb0fc8
Fabio Silva <ffjs1993@gmail.com>
2026-01-23 00:38:59 +0000

    fix: styles

--- ui/apps/pmm-compat/src/lib/constants.ts
+++ ui/apps/pmm-compat/src/lib/constants.ts
@@ -2,7 +2,7 @@ export const LOCATORS = {
   toolbar: 'header > div:first-child > div:nth-child(2)',
   menuToggle: 'header #mega-menu-toggle',
   helpButton: 'header button[aria-label="Help"]',
-  searchButton: 'header button[aria-label="Search or jump to..."]',
+  searchButton: 'header button[aria-label="Search..."]',
   profileButton: 'header button[aria-label="Profile"]',
   commandPaletteTrigger: 'header div[data-testid="data-testid Command palette trigger"]',
   toolbarSignIn: 'header > div:first-child > div:nth-child(2) > a[target="_self"]',
