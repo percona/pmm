ff5a75795a0f19d131e4cb759e200fdb97e2ce5b
Alex Demidoff <a@demidoff.me>
2026-01-25 14:26:09 +0300

    PMM-14779 Readd the comments wherever possible

--- qan-api2/migrations/sql/17_shared_blk_columns.up.sql
+++ qan-api2/migrations/sql/17_shared_blk_columns.up.sql
@@ -4,6 +4,6 @@ ALTER TABLE metrics
   RENAME COLUMN m_blk_write_time_cnt to m_shared_blk_write_time_cnt,
   RENAME COLUMN m_blk_write_time_sum to m_shared_blk_write_time_sum,
   ADD COLUMN m_local_blk_read_time_cnt Float32,
-  ADD COLUMN m_local_blk_read_time_sum Float32,
+  ADD COLUMN m_local_blk_read_time_sum Float32 COMMENT 'Total time the statement spent reading local blocks, in milliseconds (if track_io_timing is enabled, otherwise zero).',
   ADD COLUMN m_local_blk_write_time_cnt Float32,
-  ADD COLUMN m_local_blk_write_time_sum Float32;
+  ADD COLUMN m_local_blk_write_time_sum Float32 COMMENT 'Total time the statement spent writing local blocks, in milliseconds (if track_io_timing is enabled, otherwise zero).';
