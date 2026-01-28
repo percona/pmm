99a86dd361db90b7950c82d8469209a8477b6c6b
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2026-01-28 13:53:53 +0100

    PMM-14589 Add PR link

--- ui/apps/pmm/src/api/rta.fallback.ts
+++ ui/apps/pmm/src/api/rta.fallback.ts
@@ -2,7 +2,7 @@
  * @fileoverview
  * This file contains the fallback API for the RTA API.
  * It is used to get the running real-time agents and change the real-time agent status.
- * It is used to fallback to the old API until the new API is merged.
+ * It is used to fallback to the old API until the new API is merged (https://github.com/percona/pmm/pull/4956).
  */
 
 import { AgentStatus } from 'types/agent.types';
