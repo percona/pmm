3aee20151dd65026cefd4755eeef2b79813d1cc7
Fabio Silva <ffjs1993@gmail.com>
2026-01-28 18:25:46 +0000

    chore: optimize vite config

--- ui/apps/pmm/vite.config.ts
+++ ui/apps/pmm/vite.config.ts
@@ -2,62 +2,21 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vitest/config';
 import svgr from 'vite-plugin-svgr';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths({ root: '.' }), react(), svgr()],
   base: '/pmm-ui',
   resolve: {
-    preserveSymlinks: true,
     dedupe: [
       'react',
       'react-dom',
-      '@mui/material',
-      '@mui/utils',
-      '@mui/system',
-      '@mui/icons-material',
     ],
-    alias: {
-      react: path.resolve(__dirname, '../../node_modules/react'),
-      'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
-      '@emotion/react': path.resolve(
-        __dirname,
-        '../../node_modules/@emotion/react'
-      ),
-      '@emotion/styled': path.resolve(
-        __dirname,
-        '../../node_modules/@emotion/styled'
-      ),
-      '@mui/system': path.resolve(__dirname, '../../node_modules/@mui/system'),
-      '@mui/utils': path.resolve(__dirname, '../../node_modules/@mui/utils'),
-      '@mui/material': path.resolve(
-        __dirname,
-        '../../node_modules/@mui/material'
-      ),
-      '@mui/icons-material': path.resolve(
-        __dirname,
-        '../../node_modules/@mui/icons-material'
-      ),
-    },
   },
   optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      '@emotion/react',
-      '@emotion/styled',
-      'prop-types',
-      'react-is',
-      '@mui/system',
-      '@mui/material',
-    ],
-    exclude: ['@percona/percona-ui'],
-    esbuildOptions: {
-      // Handle CommonJS modules properly
-      mainFields: ['module', 'main'],
-    },
-    force: true, // Force re-optimization on startup
+    // Uncomment when using yarn link for @percona/percona-ui locally
+    // exclude: ['@percona/percona-ui'],
+    force: true
   },
   server: {
     watch: {
