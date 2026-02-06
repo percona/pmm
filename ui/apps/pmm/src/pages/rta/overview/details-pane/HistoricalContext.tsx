9a900f8bc9e44464591f4d66cde0625d048f1e6f
Fabio Silva <ffjs1993@gmail.com>
2026-02-06 16:23:59 +0000

    chore: fonts

--- ui/apps/pmm/src/pages/rta/overview/details-pane/HistoricalContext.tsx
+++ ui/apps/pmm/src/pages/rta/overview/details-pane/HistoricalContext.tsx
@@ -2,30 +2,23 @@ import { FC } from "react";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import DetailsMetric from "./DetailsMetric";
-
-const Metric = ({ mainText, subText, tertiaryText }: { mainText: string, subText?: string, tertiaryText?: string }) => (
-  <span style={{ display: 'inline-flex', alignItems: 'baseline' }}>
-    <Typography variant="h5" fontWeight="600" fontFamily='Roboto Mono, monospace'>{mainText}</Typography>
-    {subText && <Typography variant="body2" fontWeight="400" fontFamily='Roboto Mono, monospace' ml={0.5}>{subText}</Typography>}
-    {tertiaryText && <Typography variant="body2" color='text.disabled' fontWeight="400" fontFamily='Roboto Mono, monospace' ml={2}>{tertiaryText}</Typography>}
-  </span>
-)
+import BigNumberMetric from "./BigNumberMetric";
 
 const HistoricalContext: FC = () => {
   return <Stack>
     <Typography variant="h5" mb={3}>Historical Context</Typography>
     <Stack direction="row" gap={3} mb={1} sx={{ '& > *': { flexBasis: 0, flex: 1 } }}>
       <DetailsMetric title="Max. exec. time">
-        <Metric mainText="2421.83" subText="ms" tertiaryText="5.01%" />
+        <BigNumberMetric mainText="2421.83" subText="ms" tertiaryText="5.01%" />
       </DetailsMetric>
       <DetailsMetric title="Average exec. time">
-        <Metric mainText="638.19" subText="ms" tertiaryText="0.98%" />
+        <BigNumberMetric mainText="638.19" subText="ms" tertiaryText="0.98%" />
       </DetailsMetric>
       <DetailsMetric title="Exec. count">
-        <Metric mainText="178" subText="x" />
+        <BigNumberMetric mainText="178" subText="x" />
       </DetailsMetric>
       <DetailsMetric title="Total exec. time">
-        <Metric mainText="2" subText="min" tertiaryText="12.72%" />
+        <BigNumberMetric mainText="2" subText="min" tertiaryText="12.72%" />
       </DetailsMetric>
     </Stack>
   </Stack>
--- ui/apps/pmm/src/pages/rta/overview/details-pane/QueryAndDetails.tsx
+++ ui/apps/pmm/src/pages/rta/overview/details-pane/QueryAndDetails.tsx
@@ -6,6 +6,7 @@ import { QueryData } from "types/rta.types";
 import DetailsMetric from "./DetailsMetric";
 import Typography from "@mui/material/Typography";
 import { StateCell } from "pages/rta/components/state-cell";
+import BigNumberMetric from "./BigNumberMetric";
 
 type Props = {
   query: QueryData;
@@ -33,33 +34,33 @@ const QueryAndDetails: FC<Props> = ({ query: { queryText, state, serviceName } }
           </DetailsMetric>
         </GridItem>
         <GridItem>
-          <DetailsMetric title="Elapsed exec. time" subtitle="secs_running">
-            <Typography>20s</Typography>
+          <DetailsMetric title="Elapsed exec. time">
+            <BigNumberMetric mainText="20" subText="ms" />
           </DetailsMetric>
         </GridItem>
         <GridItem>
           <DetailsMetric title="Plan summary">
-            <Typography>Full collection scan (COLLSCAN)</Typography>
+            <Typography variant="body2">Full collection scan (COLLSCAN)</Typography>
           </DetailsMetric>
         </GridItem>
         <GridItem>
           <DetailsMetric title="Docs examined/sent">
-            <Typography>84,291/1</Typography>
+            <BigNumberMetric mainText="84,291/1" />
           </DetailsMetric>
         </GridItem>
         <GridItem>
           <DetailsMetric title="Snapshot time">
-            <Typography>2025-10-17 11:18:29</Typography>
+            <BigNumberMetric mainText="2025-10-17 11:18:29" size="small" />
           </DetailsMetric>
         </GridItem>
         <GridItem>
           <DetailsMetric title="Operation ID" subtitle="opid">
-            <Typography>1238912</Typography>
+            <BigNumberMetric mainText="1238912" size="small" />
           </DetailsMetric>
         </GridItem>
         <GridItem>
           <DetailsMetric title="Service">
-            <Typography>{serviceName}</Typography>
+            <BigNumberMetric mainText={serviceName} size="small" />
           </DetailsMetric>
         </GridItem>
       </Grid>
