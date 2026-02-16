9d088b9e5b6b75c1e5c3d60f7fc9119ca1e4d976
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2026-02-16 11:12:28 +0100

    PMM-7 Fix.

--- agent/utils/version/postgresql_test.go
+++ agent/utils/version/postgresql_test.go
@@ -24,13 +24,18 @@ import (
 func TestParsePostgreSQLVersion(t *testing.T) {
 	for v, expected := range map[string]string{
 		"PostgreSQL 12beta2 (Debian 12~beta2-1.pgdg100+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 8.3.0-6) 8.3.0, 64-bit":              "12",
-		"PostgreSQL 10.9 (Debian 10.9-1.pgdg90+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516, 64-bit":     "10",
+		"PostgreSQL 10.9 (Debian 10.9-1.pgdg90+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516, 64-bit":     "10.9",
 		"PostgreSQL 9.4.23 on x86_64-pc-linux-gnu (Debian 9.4.23-1.pgdg90+1), compiled by gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516, 64-bit": "9.4",
 	} {
 		t.Run(v, func(t *testing.T) {
 			major, minor := ParsePostgreSQLVersion(v)
-			actual := fmt.Sprintf("%s.%s", major, minor)
-			assert.Equal(t, expected, actual, "%s", v)
+			switch minor {
+			case "":
+				assert.Equal(t, expected, fmt.Sprintf("%s.%s", major, minor), "%s", v)
+			default:
+				assert.Equal(t, expected, major, "%s", v)
+			}
+
 		})
 	}
 }
