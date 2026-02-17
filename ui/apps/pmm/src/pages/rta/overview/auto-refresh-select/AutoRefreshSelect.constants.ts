1b18eb609146acffb038c99895bc3079d3883d47
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2026-02-17 16:02:40 +0100

    PMM-14840 Provide auto refresh interval and manual refresh

---
+++ ui/apps/pmm/src/pages/rta/overview/auto-refresh-select/AutoRefreshSelect.constants.ts
@@ -0,0 +1,24 @@
+import { TextSelectOption } from 'components/text-select/TextSelect.types';
+
+export const REFRESH_INTERVAL_OPTIONS: TextSelectOption<number>[] = [
+  {
+    label: '1s',
+    value: 1000,
+  },
+  {
+    label: '2s',
+    value: 2000,
+  },
+  {
+    label: '3s',
+    value: 3000,
+  },
+  {
+    label: '4s',
+    value: 4000,
+  },
+  {
+    label: '5s',
+    value: 5000,
+  },
+];
--- ui/apps/pmm/src/pages/rta/overview/auto-refresh-select/AutoRefreshSelect.messages.ts
+++ ui/apps/pmm/src/pages/rta/overview/auto-refresh-select/AutoRefreshSelect.messages.ts
@@ -1,3 +1,5 @@
 export const Messages = {
+  off: 'OFF',
   fetching: 'Fetching',
+  refreshInterval: 'Auto-refresh',
 };
