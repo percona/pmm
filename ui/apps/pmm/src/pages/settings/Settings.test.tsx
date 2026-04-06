67499d2c8517813a9d52ece22a92b4d184fe8e13
freenandes <uuuuurso@gmail.com>
2026-04-06 10:04:02 +0100

    Refactor Settings test to remove MemoryRouter

--- ui/apps/pmm/src/pages/settings/Settings.test.tsx
+++ ui/apps/pmm/src/pages/settings/Settings.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import { Settings } from './Settings';
 import { TestWrapper } from 'utils/testWrapper';
 import { wrapWithQueryProvider } from 'utils/testUtils';
-import { MemoryRouter } from 'react-router-dom';
 import * as settingsApi from 'api/settings';
 
 vi.mock('api/settings');
@@ -18,9 +17,7 @@ describe('Settings', () => {
     render(
       <TestWrapper>
         {wrapWithQueryProvider(
-          <MemoryRouter>
-            <Settings />
-          </MemoryRouter>
+          <Settings />
         )}
       </TestWrapper>
     );
