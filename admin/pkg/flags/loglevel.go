6be99d41bdf92cc279b6dbdbe4bf602f6789bbfe
Alex Demidoff <a@demidoff.me>
2026-04-19 20:31:20 +0300

    PMM-14069 Address linter errors

--- admin/pkg/flags/loglevel.go
+++ admin/pkg/flags/loglevel.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package flags provides common flags and helpers for it.
 package flags
 
 import (
