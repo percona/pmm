de7b965e5f68fb379675c5bdcab0389e101bdf8f
Fabio Silva <ffjs1993@gmail.com>
2026-04-23 10:40:38 +0100

    fix: tests

--- dashboards/pmm-app/jest.config.js
+++ dashboards/pmm-app/jest.config.js
@@ -1,7 +1,6 @@
 // force timezone to UTC to allow tests to work regardless of local timezone
 // generally used by snapshots, but can affect specific tests
 const baseConfig = require('./.config/jest.config');
-const path = require('path');
 
 process.env.TZ = 'GMT';
 
@@ -18,13 +17,9 @@ module.exports = {
     '!**/*constants.{ts,tsx}',
     '!**/*module.{ts,tsx}',
     '!**/*types.ts',
-    '!webpack.config.ts',
-    '!**/.config/**',
   ],
   moduleNameMapper: {
     ...baseConfig.moduleNameMapper,
-    '^d3$': path.resolve(__dirname, '../../ui/node_modules/d3/dist/d3.min.js'),
-    '^react$': require.resolve('react'),
-    '^react-dom$': require.resolve('react-dom'),
+    d3: '<rootDir>/node_modules/d3/dist/d3.min.js',
   },
 };
