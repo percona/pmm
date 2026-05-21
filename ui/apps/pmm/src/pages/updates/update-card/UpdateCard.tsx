90c46073dfc5821afa446457d4c534944de805c5
Fabio Silva <ffjs1993@gmail.com>
2026-05-21 10:40:42 +0100

    chore: remove "Upgrade Now" button

--- ui/apps/pmm/src/pages/updates/update-card/UpdateCard.tsx
+++ ui/apps/pmm/src/pages/updates/update-card/UpdateCard.tsx
@@ -9,49 +9,22 @@ import {
   Skeleton,
   Alert,
 } from '@mui/material';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { PMM_HOME_URL } from 'lib/constants';
 import { Messages } from './UpdateCard.messages';
 import { FetchingIcon } from 'components/fetching-icon';
-import { useCheckUpdates, useStartUpdate } from 'hooks/api/useUpdates';
+import { useCheckUpdates } from 'hooks/api/useUpdates';
 import { formatVersion } from './UpdateCard.utils';
-import { enqueueSnackbar } from 'notistack';
 import { UpdateStatus } from 'types/updates.types';
-import KeyboardDoubleArrowUp from '@mui/icons-material/KeyboardDoubleArrowUp';
+import CallMadeIcon from '@mui/icons-material/CallMade';
 import { UpdateInfo } from '../update-info';
-import { UpdateInProgressCard } from '../update-in-progress-card';
 import { useUpdates } from 'contexts/updates';
 import { ChangeLog } from '../change-log';
-import { capitalize } from 'utils/text.utils';
-import { DEPRECATION_DOCKER_UPGRADE_HREF, DEPRECATION_HELM_UPGRADE_HREF, DEPRECATION_PODMAN_UPGRADE_HREF } from './UpdateCard.constants';
+import { UPGRADE_DOCS_HREF } from './UpdateCard.constants';
 
 export const UpdateCard: FC = () => {
-  const { inProgress, status, setStatus } = useUpdates();
+  const { status } = useUpdates();
   const { isLoading, data, error, isRefetching, refetch } = useCheckUpdates();
-  const { mutate: startUpdate } = useStartUpdate();
-  const [authToken, setAuthToken] = useState<string>();
-
-  const handleStartUpdate = async () => {
-    setStatus(UpdateStatus.Updating);
-    startUpdate(
-      {},
-      {
-        onSuccess: async (response) => {
-          if (response) {
-            setStatus(UpdateStatus.Restarting);
-            setAuthToken(response.authToken);
-          }
-        },
-        onError: (e) => {
-          const message = e.isAxiosError ? e.response?.data.message : e.message;
-          setStatus(UpdateStatus.Error);
-          enqueueSnackbar(message ? capitalize(message) : Messages.error, {
-            variant: 'error',
-          });
-        },
-      }
-    );
-  };
 
   if (isLoading)
     return (
@@ -72,16 +45,6 @@ export const UpdateCard: FC = () => {
           <Alert severity="error">{Messages.fetchError}</Alert>
         </CardContent>
       </Card>
-    );
-  }
-
-  if (inProgress && data.latest) {
-    return (
-      <UpdateInProgressCard
-        versionInfo={data.latest}
-        status={status}
-        authToken={authToken}
-      />
     );
   }
 
@@ -122,42 +85,17 @@ export const UpdateCard: FC = () => {
         {data.updateAvailable && <UpdateInfo />}
       </CardContent>
       {data.updateAvailable ? (
-        <>
-          {/* TODO temporary solution for link color */}
-          <Alert severity="warning" sx={{ mb: 2, '& a': { color: 'inherit', textDecorationColor: 'inherit' } }}>
-            <Typography variant="body1">
-              <strong>{Messages.deprecation.heading}</strong>
-              {Messages.deprecation.paragraph1BeforeUpdateNow}
-              <strong>{Messages.updateNow}</strong>
-              {Messages.deprecation.paragraph1AfterUpdateNow}
-            </Typography>
-            <Typography>
-              {Messages.deprecation.viaIntro}
-              <Link href={DEPRECATION_DOCKER_UPGRADE_HREF} target="_blank" rel="noopener noreferrer">
-                {Messages.deprecation.docker}
-              </Link>
-              {Messages.deprecation.afterDocker}
-              <Link href={DEPRECATION_PODMAN_UPGRADE_HREF} target="_blank" rel="noopener noreferrer">
-                {Messages.deprecation.podman}
-              </Link>
-              {Messages.deprecation.afterPodman}
-              <Link href={DEPRECATION_HELM_UPGRADE_HREF} target="_blank" rel="noopener noreferrer">
-                {Messages.deprecation.helm}
-              </Link>
-              {Messages.deprecation.afterHelm}
-            </Typography>
-            <Typography>{Messages.deprecation.reminder}</Typography>
-          </Alert>
           <CardActions>
             <Button
-              endIcon={<KeyboardDoubleArrowUp />}
+              endIcon={<CallMadeIcon />}
               variant="contained"
-              onClick={handleStartUpdate}
+              href={UPGRADE_DOCS_HREF}
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              {Messages.updateNow}
+              {Messages.howToUpdateDocs}
             </Button>
           </CardActions>
-        </>
       ) : (
         <CardActions>
           <Button
