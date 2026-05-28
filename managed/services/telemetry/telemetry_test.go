bdb7301a1d0645c8e773097adedd8af0ed464821
Ante Gulin <ante.gulin@percona.com>
2026-05-29 00:15:01 +0200

    PMM-15039 Enable telemetry in TestRunTelemetryService

--- managed/services/telemetry/telemetry_test.go
+++ managed/services/telemetry/telemetry_test.go
@@ -154,6 +154,11 @@ func TestRunTelemetryService(t *testing.T) {
 	})
 	db := reform.NewDB(sqlDB, postgresql.Dialect, nil)
 
+	_, err := models.UpdateSettings(db, &models.ChangeSettingsParams{
+		EnableTelemetry: new(true),
+	})
+	require.NoError(t, err)
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
