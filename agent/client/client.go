430b0cdef0729a604dce6557ec2480a228681bf6
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2026-06-22 17:31:08 +0200

    WIP on PMM-7-5483

--- agent/client/client.go
+++ agent/client/client.go
@@ -526,13 +526,13 @@ LOOP:
 
 func (c *Client) handleStartActionRequest(p *agentv1.StartActionRequest) error {
 	timeout := p.Timeout.AsDuration()
-	err := p.Timeout.CheckValid()
-	if err != nil {
+	if err := p.Timeout.CheckValid(); err != nil {
 		timeout = 0
 	}
 
 	cfg := c.cfg.Get()
 	var action actions.Action
+	var err error
 	switch params := p.Params.(type) {
 	case *agentv1.StartActionRequest_MysqlExplainParams:
 		action, err = actions.NewMySQLExplainAction(p.ActionId, timeout, params.MysqlExplainParams)
