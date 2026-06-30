5d5f475e9afbea9b9ab59a590af0f5b4f2780360
Matej Kubinec <matej.kubinec@3pillarglobal.com>
2026-06-30 12:42:09 +0200

    PMM-7 Setup format check on UI

--- ui/apps/pmm/src/components/main/header/Header.tsx
+++ ui/apps/pmm/src/components/main/header/Header.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { useHeader } from 'hooks/useHeader';
 import Stack from '@mui/material/Stack';
 
-
 const Header: FC = () => {
   const { visible, Component } = useHeader();
 
