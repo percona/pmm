742d8f0f37f3f907f047da997e4288ba5d845632
Ante Gulin <ante.gulin@percona.com>
2026-07-10 12:35:48 +0200

    PMM-15130 Remove unused `lll` nolint directive

--- managed/services/inventory/agents.go
+++ managed/services/inventory/agents.go
@@ -2011,7 +2011,7 @@ func convertMetricsResolutions(mrs *common.MetricsResolutions) *models.ChangeMet
 }
 
 // Helper function to execute agent change and build response.
-func (as *AgentsService) executeAgentChange(ctx context.Context, agentID string, params *models.ChangeAgentParams) (inventoryv1.Agent, error) { //nolint:ireturn,lll
+func (as *AgentsService) executeAgentChange(ctx context.Context, agentID string, params *models.ChangeAgentParams) (inventoryv1.Agent, error) { //nolint:ireturn
 	var agent inventoryv1.Agent
 
 	err := as.db.InTransactionContext(ctx, nil, func(tx *reform.TX) error {
