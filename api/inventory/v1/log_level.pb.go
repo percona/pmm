d4cd1d14a8a981f74b6445faeda9bd61c10057e2
Alex Demidoff <a@demidoff.me>
2026-07-24 11:16:47 +0300

    chore: format the code

--- api/inventory/v1/log_level.pb.go
+++ api/inventory/v1/log_level.pb.go
@@ -114,6 +114,7 @@ var (
 		LogLevel(0), // 0: inventory.v1.LogLevel
 	}
 )
+
 var file_inventory_v1_log_level_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
