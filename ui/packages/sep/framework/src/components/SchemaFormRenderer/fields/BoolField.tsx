27688a3d8b6f9046ce445065613681b09b1df8cf
peter-o-addo <peter.addo@percona.com>
2026-09-09 09:54:42 +0300

    Show warning alert for destructive bool fields

--- ui/packages/sep/framework/src/components/SchemaFormRenderer/fields/BoolField.tsx
+++ ui/packages/sep/framework/src/components/SchemaFormRenderer/fields/BoolField.tsx
@@ -15,7 +15,8 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { useFormContext } from 'react-hook-form';
+import { useFormContext, useWatch } from 'react-hook-form';
+import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import { SwitchInput } from '@percona/peak-ui';
 import { FieldHelpIcon } from '../FieldLabelWithHelp';
@@ -27,19 +28,28 @@ interface BoolFieldProps {
 
 export function BoolField({ field }: BoolFieldProps) {
   const { control } = useFormContext();
+  const value = useWatch({ control, name: field.name });
+
   return (
-    <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: 0.5 }}>
-      <Box sx={{ minWidth: 0 }}>
-        <SwitchInput
-          name={field.name}
-          label={field.label}
-          labelCaption={field.description}
-          control={control}
-        />
+    <Box>
+      <Box sx={{ display: 'flex', alignItems: 'flex-start', gap: 0.5 }}>
+        <Box sx={{ minWidth: 0 }}>
+          <SwitchInput
+            name={field.name}
+            label={field.label}
+            labelCaption={field.description}
+            control={control}
+          />
+        </Box>
+        {field.description ? (
+          <FieldHelpIcon description={field.description} label={field.label} />
+        ) : null}
       </Box>
-      {field.description ? (
-        <FieldHelpIcon description={field.description} label={field.label} />
-      ) : null}
+      {field.destructive && value && (
+        <Alert severity="warning" sx={{ mt: 1 }} data-testid="destructive-warning">
+          {field.destructive}
+        </Alert>
+      )}
     </Box>
   );
 }
